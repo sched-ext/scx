280796c4bd9b341ecbff939ce2f6705d627e0992
Andrea Righi <andrea.righi@canonical.com>
2024-01-01 17:52:56 +0100

    scx_rustland: small code refactoring
    No functional change, make the user-space scheduler code a bit more
    readable and more Rust idiomatic.
    
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- scheds/rust/scx_rustland/src/main.rs
+++ scheds/rust/scx_rustland/src/main.rs
@@ -159,22 +159,6 @@ impl TaskInfoMap {
         }
     }
 
-    // Get an item (as mutable) from the HashMap (by pid).
-    fn get_mut(&mut self, pid: i32) -> Option<&mut TaskInfo> {
-        self.tasks.get_mut(&pid)
-    }
-
-    // Add or update an item in the HashMap (by pid), if the pid is already present the item will
-    // be replaced (updated).
-    fn insert(&mut self, pid: i32, task: TaskInfo) {
-        self.tasks.insert(pid, task);
-    }
-
-    // Return the amount of tasks stored in the TaskInfoMap.
-    fn len(&self) -> usize {
-        self.tasks.len()
-    }
-
     // Clean up old entries (pids that don't exist anymore).
     fn gc(&mut self) {
         fn is_pid_running(pid: i32) -> bool {
@@ -376,36 +360,36 @@ impl<'a> Scheduler<'a> {
         loop {
             match queued.lookup_and_delete(&[]) {
                 Ok(Some(msg)) => {
-                    // Schedule the task and update task information.
+                    // Extract the task object from the message.
                     let task = EnqueuedMessage::from_bytes(msg.as_slice()).as_queued_task_ctx();
-                    if let Some(task_info) = self.task_map.get_mut(task.pid) {
-                        // Taks is already mapped in self.task_map: update its information.
-                        Self::update_enqueued(
-                            task_info,
-                            task.sum_exec_runtime,
-                            task.weight,
-                            // Make sure the global vruntime is always progressing during each
-                            // scheduler run.
-                            self.min_vruntime + 1,
-                            self.skel.rodata().slice_ns,
-                        );
-                        self.task_pool.push(task.pid, task.cpu, task_info.vruntime);
-                    } else {
-                        // A new task has been scheduled.
-                        //
-                        // Initialize task information in self.task_map and set an initial vruntime
-                        // of (self.min_vruntime + 1): this ensures a progressing global vruntime
+
+                    // Get task information if the task is already stored in the task map,
+                    // otherwise create a new entry for it.
+                    let task_info =
+                        self.task_map
+                            .tasks
+                            .entry(task.pid)
+                            .or_insert_with_key(|&_pid| TaskInfo {
+                                sum_exec_runtime: task.sum_exec_runtime,
+                                vruntime: self.min_vruntime,
+                            });
+
+                    // Update task information.
+                    Self::update_enqueued(
+                        task_info,
+                        task.sum_exec_runtime,
+                        task.weight,
+                        // Make sure the global vruntime is always progressing (at least by +1)
                         // during each scheduler run, providing a priority boost to newer tasks
                         // (that is still beneficial for potential short-lived tasks), while also
                         // preventing excessive starvation of the other tasks sitting in the
                         // self.task_pool tree, waiting to be dispatched.
-                        let task_info = TaskInfo {
-                            sum_exec_runtime: task.sum_exec_runtime,
-                            vruntime: self.min_vruntime + 1,
-                        };
-                        self.task_map.insert(task.pid, task_info);
-                        self.task_pool.push(task.pid, task.cpu, self.min_vruntime + 1);
-                    }
+                        self.min_vruntime + 1,
+                        self.skel.rodata().slice_ns,
+                    );
+
+                    // Insert task in the task pool (ordered by vruntime).
+                    self.task_pool.push(task.pid, task.cpu, task_info.vruntime);
                 }
                 Ok(None) => {
                     // Reset nr_queued and update nr_scheduled, to notify the dispatcher that
@@ -521,7 +505,11 @@ impl<'a> Scheduler<'a> {
     // Print internal scheduler statistics (fetched from the BPF part).
     fn print_stats(&mut self) {
         // Show minimum vruntime (this should be constantly incrementing).
-        info!("vruntime={} tasks={}", self.min_vruntime, self.task_map.len());
+        info!(
+            "vruntime={} tasks={}",
+            self.min_vruntime,
+            self.task_map.tasks.len()
+        );
 
         // Show general statistics.
         let nr_user_dispatches = self.skel.bss().nr_user_dispatches as u64;
