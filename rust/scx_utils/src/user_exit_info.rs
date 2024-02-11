sched-ext/scx

commit d57a23f481d59fca853d6534009206411cd9aca5

rust/scx_utils: Add user_exit_info support
So that rust scheds can use the same unified exit handling as C scheds.

Signed-off-by: Tejun Heo <tj@kernel.org>

rust/scx_utils/src/user_exit_info.rs
@@ -1,0 +1,140 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+//
+// This software may be used and distributed according to the terms of the
+// GNU General Public License version 2.
+use crate::bindings;
+use anyhow::bail;
+use anyhow::Result;
+use std::ffi::CStr;
+
+pub enum ScxExitKind {
+    None = bindings::scx_exit_kind_SCX_EXIT_NONE as isize,
+    Done = bindings::scx_exit_kind_SCX_EXIT_DONE as isize,
+    Unreg = bindings::scx_exit_kind_SCX_EXIT_UNREG as isize,
+    SysRq = bindings::scx_exit_kind_SCX_EXIT_SYSRQ as isize,
+    Error = bindings::scx_exit_kind_SCX_EXIT_ERROR as isize,
+    ErrorBPF = bindings::scx_exit_kind_SCX_EXIT_ERROR_BPF as isize,
+    ErrorStall = bindings::scx_exit_kind_SCX_EXIT_ERROR_STALL as isize,
+}
+
+/// Takes a reference to C struct user_exit_info and reads it into
+/// UserExitInfo. See UserExitInfo.
+#[macro_export]
+macro_rules! uei_read {
+    ($bpf_uei:expr) => {{
+        {
+            let bpf_uei = $bpf_uei;
+            scx_utils::UserExitInfo::new(
+                &bpf_uei.kind as *const _,
+                bpf_uei.reason.as_ptr() as *const _,
+                bpf_uei.msg.as_ptr() as *const _,
+                bpf_uei.dump.as_ptr() as *const _,
+            )
+        }
+    }};
+}
+
+/// Takes a reference to C struct user_exit_info and test whether the BPF
+/// scheduler has exited. See UserExitInfo.
+#[macro_export]
+macro_rules! uei_exited {
+    ($bpf_uei:expr) => {{
+        (unsafe { std::ptr::read_volatile(&$bpf_uei.kind as *const _) } != 0)
+    }};
+}
+
+/// Takes a reference to C struct user_exit_info, reads it and invokes
+/// UserExitInfo::report() on it. See UserExitInfo.
+#[macro_export]
+macro_rules! uei_report {
+    ($bpf_uei:expr) => {{
+        scx_utils::uei_read!($bpf_uei).report()
+    }};
+}
+
+/// Rust counterpart of C struct user_exit_info.
+#[derive(Debug, Default)]
+pub struct UserExitInfo {
+    /// The C enum scx_exit_kind value. Test against ScxExitKind. None-zero
+    /// value indicates that the BPF scheduler has exited.
+    kind: i32,
+    reason: Option<String>,
+    msg: Option<String>,
+    dump: Option<String>,
+}
+
+impl UserExitInfo {
+    /// Create UserExitInfo from C struct user_exit_info. Each scheduler
+    /// implementation creates its own Rust binding for the C struct
+    /// user_exit_info, so we can't take the type directly. Instead, this
+    /// method takes each member field. Use the macro uei_read!() on the C
+    /// type which then calls this method with the individual fields.
+    pub fn new(
+        kind_ptr: *const i32,
+        reason_ptr: *const i8,
+        msg_ptr: *const i8,
+        dump_ptr: *const i8,
+    ) -> Self {
+        let kind = unsafe { std::ptr::read_volatile(kind_ptr) };
+
+        let (reason, msg, dump) = (
+            Some(
+                unsafe { CStr::from_ptr(reason_ptr) }
+                    .to_str()
+                    .expect("Failed to convert reason to string")
+                    .to_string(),
+            )
+            .filter(|s| !s.is_empty()),
+            Some(
+                unsafe { CStr::from_ptr(msg_ptr) }
+                    .to_str()
+                    .expect("Failed to convert msg to string")
+                    .to_string(),
+            )
+            .filter(|s| !s.is_empty()),
+            Some(
+                unsafe { CStr::from_ptr(dump_ptr) }
+                    .to_str()
+                    .expect("Failed to convert msg to string")
+                    .to_string(),
+            )
+            .filter(|s| !s.is_empty()),
+        );
+
+        Self {
+            kind,
+            reason,
+            msg,
+            dump,
+        }
+    }
+
+    /// Print out the exit message to stderr if the exit was normal. After
+    /// an error exit, it throws an error containing the exit message
+    /// instead. If debug dump exists, it's always printed to stderr.
+    pub fn report(&self) -> Result<()> {
+        if self.kind == 0 {
+            return Ok(());
+        }
+
+	if let Some(dump) = &self.dump {
+	    eprintln!("\nDEBUG DUMP");
+	    eprintln!("================================================================================\n");
+	    eprintln!("{}", dump);
+	    eprintln!("================================================================================\n");
+	}
+
+        let why = match (&self.reason, &self.msg) {
+            (Some(reason), None) => format!("EXIT: {}", reason),
+            (Some(reason), Some(msg)) => format!("EXIT: {} ({})", reason, msg),
+            _ => "<UNKNOWN>".into(),
+        };
+
+        if self.kind <= ScxExitKind::Unreg as i32 {
+            eprintln!("{}", why);
+            Ok(())
+        } else {
+            bail!("{}", why)
+        }
+    }
+}

rust/scx_utils/Cargo.toml
@@ -21,5 +21,6 @@ tar = "0.4"
 version-compare = "0.1"
 
 [build-dependencies]
+bindgen = ">=0.68, <0.70"
 tar = "0.4"
 walkdir = "2.4"

rust/scx_utils/bindings.h
@@ -1,0 +1,1 @@
+#include "bpf_h/vmlinux/vmlinux.h"

rust/scx_utils/build.rs
@@ -9,7 +9,7 @@ use std::path::PathBuf;
 
 const BPF_H: &str = "bpf_h";
 
-fn main() {
+fn gen_bpf_h() {
     let file =
         File::create(PathBuf::from(env::var("OUT_DIR").unwrap()).join(format!("{}.tar", BPF_H)))
             .unwrap();
@@ -25,4 +25,29 @@ fn main() {
             println!("cargo:rerun-if-changed={}", ent.path().to_string_lossy());
         }
     }
+}
+
+fn gen_bindings() {
+    // FIXME - bindgen's API changed between 0.68 and 0.69 so that
+    // `bindgen::CargoCallbacks::new()` should be used instead of
+    // `bindgen::CargoCallbacks`. Unfortunately, as of Dec 2023, fedora is
+    // shipping 0.68. To accommodate fedora, allow both 0.68 and 0.69 of
+    // bindgen and suppress deprecation warning. Remove the following once
+    // fedora can be updated to bindgen >= 0.69.
+    #[allow(deprecated)]
+    let bindings = bindgen::Builder::default()
+        .header("bindings.h")
+	.allowlist_type("scx_exit_kind")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    bindings
+        .write_to_file(PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs"))
+        .expect("Couldn't write bindings");
+}
+
+fn main() {
+    gen_bpf_h();
+    gen_bindings();
 }

rust/scx_utils/src/bindings.rs
@@ -1,0 +1,6 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));

rust/scx_utils/src/lib.rs
@@ -30,7 +30,13 @@
 //! Utility modules which can be useful for userspace component of sched_ext
 //! schedulers.
 
+mod bindings;
+
 mod bpf_builder;
 pub use bpf_builder::BpfBuilder;
 
 pub mod ravg;
+
+mod user_exit_info;
+pub use user_exit_info::UserExitInfo;
+pub use user_exit_info::ScxExitKind;

commit f5a21198ad51cfb5342725e7ac4fbe45345e5eb7

scx_utils: use c_char to prevent build failures
Use c_char to convert C strings, that is more portable across different
architectures.

This prevents a build failure on arm64 and ppc64el.

Fixes: d57a23f ("rust/scx_utils: Add user_exit_info support")
Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

rust/scx_utils/src/user_exit_info.rs
@@ -6,6 +6,7 @@ use crate::bindings;
 use anyhow::bail;
 use anyhow::Result;
 use std::ffi::CStr;
+use std::os::raw::c_char;
 
 pub enum ScxExitKind {
     None = bindings::scx_exit_kind_SCX_EXIT_NONE as isize,
@@ -71,9 +72,9 @@ impl UserExitInfo {
     /// type which then calls this method with the individual fields.
     pub fn new(
         kind_ptr: *const i32,
-        reason_ptr: *const i8,
-        msg_ptr: *const i8,
-        dump_ptr: *const i8,
+        reason_ptr: *const c_char,
+        msg_ptr: *const c_char,
+        dump_ptr: *const c_char,
     ) -> Self {
         let kind = unsafe { std::ptr::read_volatile(kind_ptr) };
 