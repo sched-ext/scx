871a6c10f9986a52aade860b02093900d91bc779
Andrea Righi <andrea.righi@canonical.com>
2024-02-24 22:02:47 +0100

    scx_rustland_core: include scx_rustland backend
    Move the BPF component of scx_rustland to scx_rustland_core and make it
    available to other user-space schedulers.
    
    NOTE: main.bpf.c and bpf.rs are not pre-compiled in the
    scx_rustland_core crate, they need to be included in the user-space
    scheduler's source code in order to be compiled/linked properly.
    
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- scheds/rust/scx_rustland/build.rs
+++ scheds/rust/scx_rustland/build.rs
@@ -4,8 +4,8 @@
 fn main() {
     scx_utils::BpfBuilder::new()
         .unwrap()
-        .enable_intf("src/bpf/intf.h", "bpf_intf.rs")
-        .enable_skel("src/bpf/main.bpf.c", "bpf")
+        .enable_intf("../../../rust/scx_rustland_core/src/bpf/intf.h", "bpf_intf.rs")
+        .enable_skel("../../../rust/scx_rustland_core/src/bpf/main.bpf.c", "bpf")
         .build()
         .unwrap();
 }
