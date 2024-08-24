sched-ext/scx

commit 6a2285398dd3821e1a6c37ef02185555bca22499

scx_bpfland: introduce --lowlatency option
Introduce the new `--lowlatency` option, which enables switching between
the default pure vruntime-based scheduling (more optimized for server
workloads) and a deadline-based scheduling (better suited for
low-latency workloads).

When the low-latency mode is activated, a task's deadline is calculated
as its vruntime, adjusted by a bonus proportional to the task's average
number of voluntary context switches (the more voluntary context
switches, the shorter the deadline).

This feature enhances the prioritization of interactive tasks even more,
proportionally to their average voluntary context switches, also within
the two main global queues (priority / shared) and it helps to maintain
interactive workloads always responsive, even in presence of heavy
non-interactive background work.

Low-latency mode allows to prevent audio cracking even in presence of a
large amount of short-lived tasks with pseudo-interactive behavior (i.e,
hackbench) and it enables achieving approximately a +33% average
frames-per-second (FPS) in the typical "gaming while building the
kernel" benchmark.

However, it can also amplify the de-prioritization of CPU-intensive
tasks, making this option more suitable for specific low-latency
scenarios. Therefore the low-latency mode is disabled by default and it
can only be enabled via the `--lowlatency` option.

Tested-by: Piotr Gorski (piotrgorski@cachyos.org)
Signed-off-by: Andrea Righi <andrea.righi@linux.dev>

scheds/rust/scx_bpfland/src/bpf/main.bpf.c
@@ -59,6 +59,27 @@ const volatile s64 slice_ns_lag;
 const volatile bool local_kthreads;
 
 /*
+ * Boost interactive tasks, by shortening their deadline as a function of their
+ * average amount of voluntary context switches.
+ *
+ * Tasks are already classified as interactive if their average amount of
+ * context switches exceeds nvcsw_avg_thresh, which grants them higher
+ * priority.
+ *
+ * When this option is enabled, tasks will receive a deadline boost in addition
+ * to their interactive vs. regular classification, with the boost being
+ * proportional to their average number of context switches.
+ *
+ * This ensures that within the main scheduling classes (interactive and
+ * regular), tasks that more frequently voluntarily yield the CPU receive an
+ * even higher priority.
+ *
+ * This option is particularly useful in soft real-time scenarios, such as
+ * audio processing, multimedia, etc.
+ */
+const volatile bool lowlatency;
+
+/*
  * Maximum threshold of voluntary context switches.
  *
  * This limits the range of nvcsw_avg_thresh (see below).
@@ -341,10 +362,26 @@ static inline bool vtime_before(u64 a, u64 b)
 }
 
 /*
- * Return task's evaluated vruntime.
+ * Return task's average amount of context switches per second.
  */
-static inline u64 task_vtime(struct task_struct *p)
+static bool task_avg_nvcsw(struct task_struct *p)
 {
+	struct task_ctx *tctx;
+
+	tctx = try_lookup_task_ctx(p);
+	if (!tctx)
+		return 0;
+	return tctx->avg_nvcsw;
+}
+
+/*
+ * Return task's evaluated deadline.
+ */
+static inline u64 task_deadline(struct task_struct *p)
+{
+	u64 dl_boost = lowlatency ?
+		MIN(task_avg_nvcsw(p), nvcsw_max_thresh) * slice_ns : 0;
+
 	/*
 	 * Limit the vruntime to (vtime_now - slice_ns_lag) to avoid
 	 * excessively penalizing tasks.
@@ -360,7 +397,12 @@ static inline u64 task_vtime(struct task_struct *p)
 	if (vtime_before(p->scx.dsq_vtime, vtime_now - slice_ns_lag))
 		p->scx.dsq_vtime = vtime_now - slice_ns_lag;
 
-	return p->scx.dsq_vtime;
+	/*
+	 * Return the task's deadline as its vruntime, with a bonus that is
+	 * proportional to the task's average number of voluntary context
+	 * switches.
+	 */
+	return p->scx.dsq_vtime - dl_boost;
 }
 
 /*
@@ -413,7 +455,7 @@ static u64 cpu_to_dsq(s32 cpu)
 static int dispatch_direct_cpu(struct task_struct *p, s32 cpu, u64 enq_flags)
 {
 	struct bpf_cpumask *offline;
-	u64 vtime = task_vtime(p);
+	u64 deadline = task_deadline(p);
 	u64 dsq_id = cpu_to_dsq(cpu);
 
 	/*
@@ -423,7 +465,7 @@ static int dispatch_direct_cpu(struct task_struct *p, s32 cpu, u64 enq_flags)
 	if (!bpf_cpumask_test_cpu(cpu, p->cpus_ptr))
 		return -EINVAL;
 
-	scx_bpf_dispatch_vtime(p, dsq_id, SCX_SLICE_DFL, vtime, enq_flags);
+	scx_bpf_dispatch_vtime(p, dsq_id, SCX_SLICE_DFL, deadline, enq_flags);
 
 	/*
 	 * If the CPU has gone offline notify that the task needs to be
@@ -723,7 +765,7 @@ static void handle_sync_wakeup(struct task_struct *p)
  */
 void BPF_STRUCT_OPS(bpfland_enqueue, struct task_struct *p, u64 enq_flags)
 {
-	u64 vtime = task_vtime(p);
+	u64 deadline = task_deadline(p);
 
 	/*
 	 * If the system is saturated and we couldn't dispatch directly in
@@ -756,11 +798,11 @@ void BPF_STRUCT_OPS(bpfland_enqueue, struct task_struct *p, u64 enq_flags)
 	 */
 	if (is_task_interactive(p)) {
 		scx_bpf_dispatch_vtime(p, prio_dsq_id, SCX_SLICE_DFL,
-				       vtime, enq_flags);
+				       deadline, enq_flags);
 		__sync_fetch_and_add(&nr_prio_dispatches, 1);
 	} else {
 		scx_bpf_dispatch_vtime(p, shared_dsq_id, SCX_SLICE_DFL,
-				       vtime, enq_flags);
+				       deadline, enq_flags);
 		__sync_fetch_and_add(&nr_shared_dispatches, 1);
 	}
 }

scheds/rust/scx_bpfland/src/main.rs
@@ -172,6 +172,14 @@ struct Opts {
     #[clap(short = 'l', long, allow_hyphen_values = true, default_value = "0")]
     slice_us_lag: i64,
 
+    /// Shorten interactive tasks deadline based on their average amount of voluntary context
+    /// switches.
+    ///
+    /// Enabling this option can be beneficial in soft real-time scenarios, such as audio
+    /// processing, multimedia, etc.
+    #[clap(short = 'L', long, action = clap::ArgAction::SetTrue)]
+    lowlatency: bool,
+
     /// Enable per-CPU kthreads prioritization.
     ///
     /// Enabling this can enhance the performance of interrupt-driven workloads (e.g., networking
@@ -314,6 +322,7 @@ impl<'a> Scheduler<'a> {
         // Override default BPF scheduling parameters.
         skel.maps.rodata_data.debug = opts.debug;
         skel.maps.rodata_data.smt_enabled = smt_enabled;
+        skel.maps.rodata_data.lowlatency = opts.lowlatency;
         skel.maps.rodata_data.local_kthreads = opts.local_kthreads;
         skel.maps.rodata_data.slice_ns = opts.slice_us * 1000;
         skel.maps.rodata_data.slice_ns_min = opts.slice_us_min * 1000;

commit 5a08855a8634c3aa042b872b89ea59fe5873df88

scx_bpfland: always honor average nvcsw in lowlatency mode
Keep evaluating the average number of voluntary context switches for
each task when lowlatency mode is enabled, even when interactive tasks
classification is disabled (via `-c 0`).

The average nvcsw is also used in lowlatency mode to evaluate the
proportional bonus to the tasks' deadline and it shouldn't be ignored
when interactive tasks classification is disabled. Moreover, make sure
that such bonus never exceeds the starvation threshold.

Keep in mind that it is still possible to disable the periodic average
nvcsw evaluation with `-c 0`, without specifying `--lowlatency`.

Fixes: 6a22853 ("scx_bpfland: introduce --lowlatency option")
Signed-off-by: Andrea Righi <andrea.righi@linux.dev>

scheds/rust/scx_bpfland/src/bpf/main.bpf.c
@@ -384,8 +384,7 @@ static bool task_avg_nvcsw(struct task_struct *p)
  */
 static inline u64 task_deadline(struct task_struct *p)
 {
-	u64 dl_boost = lowlatency ?
-		MIN(task_avg_nvcsw(p), nvcsw_max_thresh) * slice_ns : 0;
+	u64 dl_boost = lowlatency ? task_avg_nvcsw(p) * slice_ns : 0;
 
 	/*
 	 * Limit the vruntime to (vtime_now - slice_ns_lag) to avoid
@@ -406,8 +405,11 @@ static inline u64 task_deadline(struct task_struct *p)
 	 * Return the task's deadline as its vruntime, with a bonus that is
 	 * proportional to the task's average number of voluntary context
 	 * switches.
+	 *
+	 * Also make sure the bonus is limited to the starvation threshold (to
+	 * prevent starvation).
 	 */
-	return p->scx.dsq_vtime - dl_boost;
+	return p->scx.dsq_vtime - MIN(dl_boost, starvation_thresh_ns);
 }
 
 /*
@@ -1062,18 +1064,19 @@ void BPF_STRUCT_OPS(bpfland_stopping, struct task_struct *p, bool runnable)
 	 * Evaluate the average number of voluntary context switches per second
 	 * using an exponentially weighted moving average, see calc_avg().
 	 */
+	if (!lowlatency && !is_nvcsw_enabled())
+		return;
 	delta_t = (s64)(now - tctx->nvcsw_ts);
-	if (is_nvcsw_enabled() && delta_t > NSEC_PER_SEC) {
+	if (delta_t > NSEC_PER_SEC) {
 		u64 delta_nvcsw = p->nvcsw - tctx->nvcsw;
 		u64 avg_nvcsw = delta_nvcsw * NSEC_PER_SEC / delta_t;
 
 		/*
 		 * Evaluate the average nvcsw for the task, limited to the
-		 * range [0 .. nvcsw_max_thresh * 8] to prevent excessive
-		 * spikes.
+		 * range [0 .. 1000] to prevent excessive spikes.
 		 */
 		tctx->avg_nvcsw = calc_avg_clamp(tctx->avg_nvcsw, avg_nvcsw,
-						 0, nvcsw_max_thresh << 3);
+						 0, MAX(nvcsw_max_thresh, 1000));
 		tctx->nvcsw = p->nvcsw;
 		tctx->nvcsw_ts = now;
 
@@ -1098,10 +1101,6 @@ void BPF_STRUCT_OPS(bpfland_stopping, struct task_struct *p, bool runnable)
 		 * Reresh task status: interactive or regular.
 		 */
 		update_task_interactive(tctx);
-
-		dbg_msg("%d (%s) avg_nvcsw = %llu [%s]",
-			p->pid, p->comm, tctx->avg_nvcsw,
-			tctx->avg_nvcsw < nvcsw_avg_thresh ? "regular" : "interactive");
 	}
 }
 