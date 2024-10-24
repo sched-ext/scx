1e0e0d2f5042ab98568b3d1c42c6094f74e46353
Pat Somaru <patso@likewhatevs.io>
2024-10-24 10:44:30 -0400

    make timerlib work the best it can with tooling

--- scheds/rust/scx_layered/src/bpf/timer.bpf.h
+++ scheds/rust/scx_layered/src/bpf/timer.bpf.h
@@ -1,6 +1,12 @@
 /* Copyright (c) Meta Platforms, Inc. and affiliates. */
 #ifndef __LAYERED_TIMER_H
 #define __LAYERED_TIMER_H
+#ifdef LSP
+#define __bpf__
+#ifndef LSP_INC
+#include "../../../../include/scx/common.bpf.h"
+#endif
+#endif
 #include <bpf/bpf_core_read.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
