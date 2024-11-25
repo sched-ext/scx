sched-ext/scx

commit e3b623dd4b600ee311cbb55424d94402b029da63

topology: Build uniform skip indicies
Build skip indicies prefixed with all_ at every layer.

rust/scx_utils/src/topology.rs
@@ -137,6 +137,8 @@ pub struct Llc {
     pub id: usize,
     pub cores: BTreeMap<usize, Arc<Core>>,
     pub span: Cpumask,
+
+    pub all_cpus: BTreeMap<usize, Arc<Cpu>>,
 }
 
 impl Llc {
@@ -154,9 +156,13 @@ impl Llc {
 pub struct Node {
     pub id: usize,
     pub llcs: BTreeMap<usize, Arc<Llc>>,
+    pub span: Cpumask,
+
+    pub all_cores: BTreeMap<usize, Arc<Core>>,
+    pub all_cpus: BTreeMap<usize, Arc<Cpu>>,
+
     #[cfg(feature = "gpu-topology")]
     pub gpus: BTreeMap<GpuIndex, Gpu>,
-    pub span: Cpumask,
 }
 
 impl Node {
@@ -186,41 +192,71 @@ impl Node {
 #[derive(Debug)]
 pub struct Topology {
     pub nodes: Vec<Node>,
-    pub cores: Vec<Arc<Core>>,
-    pub cpus: BTreeMap<usize, Arc<Cpu>>,
     pub span: Cpumask,
     pub nr_cpus_online: usize,
+
+    pub all_llcs: BTreeMap<usize, Arc<Llc>>,
+    pub all_cores: BTreeMap<usize, Arc<Core>>,
+    pub all_cpus: BTreeMap<usize, Arc<Cpu>>,
 }
 
 impl Topology {
-    fn instantiate(span: Cpumask, nodes: Vec<Node>) -> Result<Self> {
-        // For convenient and efficient lookup from the root topology object,
-        // create two BTreeMaps to the full set of Core and Cpu objects on the
-        // system. We clone the objects that are located further down in the
-        // hierarchy rather than dealing with references, as the entire
-        // Topology is read-only anyways.
-        let mut cores = Vec::new();
-        let mut cpus = BTreeMap::new();
-        for node in nodes.iter() {
-            for llc in node.llcs.values() {
-                for core in llc.cores.values() {
-                    cores.push(core.clone());
-                    for (cpu_id, cpu) in core.cpus.iter() {
-                        if let Some(_) = cpus.insert(*cpu_id, cpu.clone()) {
-                            bail!("Found duplicate CPU ID {}", cpu_id);
+    fn instantiate(span: Cpumask, mut nodes: Vec<Node>) -> Result<Self> {
+        // Build skip indices prefixed with all_ for easy lookups. As Arc
+        // objects can only be modified while there's only one reference,
+        // skip indices must be built from bottom to top.
+        let mut topo_llcs = BTreeMap::new();
+        let mut topo_cores = BTreeMap::new();
+        let mut topo_cpus = BTreeMap::new();
+
+        for node in nodes.iter_mut() {
+            let mut node_cores = BTreeMap::new();
+            let mut node_cpus = BTreeMap::new();
+
+            for (&llc_id, llc) in node.llcs.iter_mut() {
+                let llc_mut = Arc::get_mut(llc).unwrap();
+                let mut llc_cpus = BTreeMap::new();
+
+                for (&core_id, core) in llc_mut.cores.iter_mut() {
+                    for (&cpu_id, cpu) in core.cpus.iter() {
+                        if topo_cpus
+                            .insert(cpu_id, cpu.clone())
+                            .or(node_cpus.insert(cpu_id, cpu.clone()))
+                            .or(llc_cpus.insert(cpu_id, cpu.clone()))
+                            .is_some()
+                        {
+                            bail!("Duplicate CPU ID {}", cpu_id);
                         }
                     }
+
+                    if topo_cores
+                        .insert(core_id, core.clone())
+                        .or(node_cores.insert(core_id, core.clone()))
+                        .is_some()
+                    {
+                        bail!("Duplicate CORE ID {}", core_id);
+                    }
+                }
+
+                llc_mut.all_cpus = llc_cpus;
+
+                if topo_llcs.insert(llc_id, llc.clone()).is_some() {
+                    bail!("Duplicate LLC ID {}", llc_id);
                 }
             }
+
+            node.all_cores = node_cores;
+            node.all_cpus = node_cpus;
         }
 
         let nr_cpus_online = span.weight();
         Ok(Topology {
             nodes,
-            cores,
-            cpus,
             span,
             nr_cpus_online,
+            all_llcs: topo_llcs,
+            all_cores: topo_cores,
+            all_cpus: topo_cpus,
         })
     }
 
@@ -259,13 +295,15 @@ impl Topology {
 
     /// Returns whether the Topology has a hybrid architecture of big and little cores.
     pub fn has_little_cores(&self) -> bool {
-        self.cores.iter().any(|c| c.core_type == CoreType::Little)
+        self.all_cores
+            .values()
+            .any(|c| c.core_type == CoreType::Little)
     }
 
     /// Returns a BitVec of online CPUs.
     pub fn cpus_bitvec(&self) -> BitVec {
         let mut cpus = bitvec![0; *NR_CPUS_POSSIBLE];
-        for (id, _) in self.cpus.iter() {
+        for id in self.all_cpus.keys() {
             cpus.set(*id, true);
         }
         cpus
@@ -279,7 +317,7 @@ impl Topology {
     /// Assuming each core holds exactly at most two cpus.
     pub fn sibling_cpus(&self) -> Vec<i32> {
         let mut sibling_cpu = vec![-1i32; *NR_CPUS_POSSIBLE];
-        for core in &self.cores {
+        for core in self.all_cores.values() {
             let mut first = -1i32;
             for (cpu_id, _) in &core.cpus {
                 let cpu = *cpu_id;
@@ -323,7 +361,7 @@ impl TopologyMap {
     pub fn new(topo: &Topology) -> Result<TopologyMap> {
         let mut map: Vec<Vec<usize>> = Vec::new();
 
-        for core in &topo.cores {
+        for core in topo.all_cores.values() {
             let mut cpu_ids: Vec<usize> = Vec::new();
             for cpu_id in core.span.clone().into_iter() {
                 cpu_ids.push(cpu_id);
@@ -443,6 +481,7 @@ fn create_insert_cpu(
         id: llc_id,
         cores: BTreeMap::new(),
         span: Cpumask::new()?,
+        all_cpus: BTreeMap::new(),
     }));
     let llc_mut = Arc::get_mut(llc).unwrap();
 
@@ -545,6 +584,8 @@ fn create_default_node(online_mask: &Cpumask, flatten_llc: bool) -> Result<Vec<N
         span: Cpumask::new()?,
         #[cfg(feature = "gpu-topology")]
         gpus: BTreeMap::new(),
+        all_cores: BTreeMap::new(),
+        all_cpus: BTreeMap::new(),
     };
 
     #[cfg(feature = "gpu-topology")]
@@ -595,6 +636,10 @@ fn create_numa_nodes(online_mask: &Cpumask) -> Result<Vec<Node>> {
             id: node_id,
             llcs: BTreeMap::new(),
             span: Cpumask::new()?,
+
+            all_cores: BTreeMap::new(),
+            all_cpus: BTreeMap::new(),
+
             #[cfg(feature = "gpu-topology")]
             gpus: BTreeMap::new(),
         };

rust/scx_rustland_core/assets/bpf.rs
@@ -345,7 +345,7 @@ impl<'cb> BpfScheduler<'cb> {
     ) -> Result<(), std::io::Error> {
         // Determine the list of CPU IDs associated to each cache node.
         let mut cache_id_map: HashMap<usize, Vec<usize>> = HashMap::new();
-        for core in &topo.cores {
+        for core in topo.all_cores.values() {
             for (cpu_id, cpu) in &core.cpus {
                 let cache_id = match cache_lvl {
                     2 => cpu.l2_id,

scheds/rust/scx_bpfland/src/main.rs
@@ -63,8 +63,8 @@ fn get_primary_cpus(mode: Powermode) -> std::io::Result<Vec<usize>> {
     let topo = Topology::new().unwrap();
 
     let cpus: Vec<usize> = topo
-        .cores
-        .iter()
+        .all_cores
+        .values()
         .flat_map(|core| &core.cpus)
         .filter_map(|(cpu_id, cpu)| match (&mode, &cpu.core_type) {
             // Performance mode: add all the Big CPUs (either Turbo or non-Turbo)
@@ -481,7 +481,7 @@ impl<'a> Scheduler<'a> {
     ) -> Result<(), std::io::Error> {
         // Determine the list of CPU IDs associated to each cache node.
         let mut cache_id_map: HashMap<usize, Vec<usize>> = HashMap::new();
-        for core in topo.cores.iter() {
+        for core in topo.all_cores.values() {
             for (cpu_id, cpu) in &core.cpus {
                 let cache_id = match cache_lvl {
                     2 => cpu.l2_id,

scheds/rust/scx_flash/src/main.rs
@@ -197,7 +197,7 @@ impl<'a> Scheduler<'a> {
     ) -> Result<(), std::io::Error> {
         // Determine the list of CPU IDs associated to each cache node.
         let mut cache_id_map: HashMap<usize, Vec<usize>> = HashMap::new();
-        for core in topo.cores.iter() {
+        for core in topo.all_cores.values() {
             for (cpu_id, cpu) in &core.cpus {
                 let cache_id = match cache_lvl {
                     2 => cpu.l2_id,

scheds/rust/scx_layered/src/layer_core_growth.rs
@@ -122,7 +122,7 @@ impl<'a> LayerCoreOrderGenerator<'a> {
             }
         };
 
-        for i in 0..self.topo.cores.len() {
+        for i in 0..self.topo.all_cores.len() {
             core_order.push(i);
         }
 
@@ -142,7 +142,7 @@ impl<'a> LayerCoreOrderGenerator<'a> {
     }
 
     fn grow_linear(&self) -> Vec<usize> {
-        (0..self.topo.cores.len()).collect()
+        (0..self.topo.all_cores.len()).collect()
     }
 
     fn grow_reverse(&self) -> Vec<usize> {
@@ -189,7 +189,7 @@ impl<'a> LayerCoreOrderGenerator<'a> {
     }
 
     fn grow_big_little(&self) -> Vec<usize> {
-        let mut cores: Vec<&Arc<Core>> = self.topo.cores.iter().collect();
+        let mut cores: Vec<&Arc<Core>> = self.topo.all_cores.values().collect();
         cores.sort_by(|a, b| a.core_type.cmp(&b.core_type));
         cores
             .into_iter()

scheds/rust/scx_layered/src/lib.rs
@@ -98,7 +98,7 @@ impl CpuPool {
         let all_cpus = topo.cpus_bitvec();
         let nr_cpus = topo.nr_cpus_online;
         let core_cpus = topo_map.core_cpus_bitvec();
-        let nr_cores = topo.cores.len();
+        let nr_cores = topo.all_cores.len();
         let sibling_cpu = topo.sibling_cpus();
         let cpu_core = topo_map.cpu_core_mapping();
 

scheds/rust/scx_layered/src/main.rs
@@ -625,7 +625,7 @@ fn initialize_cpu_ctxs(skel: &BpfSkel, topo: &Topology) -> Result<()> {
             &*(cpu_ctxs_vec[cpu].as_slice().as_ptr() as *const bpf_intf::cpu_ctx)
         });
 
-        let topo_cpu = topo.cpus.get(&cpu).unwrap();
+        let topo_cpu = topo.all_cpus.get(&cpu).unwrap();
         let is_big = topo_cpu.core_type == CoreType::Big { turbo: true };
         cpu_ctxs[cpu].is_big = is_big;
     }
@@ -1202,13 +1202,13 @@ impl<'a> Scheduler<'a> {
                 node.id, skel.maps.rodata_data.numa_cpumasks[node.id]
             );
 
-            for (_, llc) in &node.llcs {
+            for llc in node.llcs.values() {
                 debug!("configuring llc {:?} for node {:?}", llc.id, node.id);
                 skel.maps.rodata_data.llc_numa_id_map[llc.id] = node.id as u32;
             }
         }
 
-        for (_, cpu) in &topo.cpus {
+        for cpu in topo.all_cpus.values() {
             skel.maps.rodata_data.cpu_llc_id_map[cpu.id] = cpu.llc_id as u32;
         }
     }

commit f105e75980707650adaa1685203f6169ebfae97d

topology: handle LLCs spanning multiple NUMA nodes
On physical hardware the same LLC should never span across multiple NUMA
nodes. However, this scenario can happen in virtualized environments,
particularly when multiple NUMA nodes are defined without specifying an
exact CPU topology.

Example:

 $ vng --cpu 8 -m 4G --numa 2G,cpus=0-3 --numa 2G,cpus=4-8
 ...
 $ lscpu -e
 CPU NODE SOCKET CORE L1d:L1i:L2:L3 ONLINE
   0    0      0    0 0:0:0:0          yes
   1    0      0    1 1:1:1:0          yes
   2    0      0    2 2:2:2:0          yes
   3    0      0    3 3:3:3:0          yes
   4    1      0    4 4:4:4:0          yes
   5    1      0    5 5:5:5:0          yes
   6    1      0    6 6:6:6:0          yes
   7    1      0    7 7:7:7:0          yes

In this case, an error like the following is reported, causing scx
schedulers relying on topology to fail:

 > sudo scx_bpfland
 ...
 thread 'main' panicked at scheds/rust/scx_bpfland/src/main.rs:261:36:
 called `Result::unwrap()` on an `Err` value: Duplicate LLC ID 0

We can handle this particular configuration by introducing an artificial
LLC for each NUMA node. In the example above, an additional LLC (ID 1)
is created to cover CPU4..CPU7.

Result with this fix applied:

 > sudo scx_bpfland
 ...
 08:09:29 [INFO] L2 cache ID 4: sibling CPUs: [4]
 08:09:29 [INFO] L2 cache ID 0: sibling CPUs: [0]
 08:09:29 [INFO] L2 cache ID 6: sibling CPUs: [6]
 08:09:29 [INFO] L2 cache ID 1: sibling CPUs: [1]
 08:09:29 [INFO] L2 cache ID 5: sibling CPUs: [5]
 08:09:29 [INFO] L2 cache ID 3: sibling CPUs: [3]
 08:09:29 [INFO] L2 cache ID 2: sibling CPUs: [2]
 08:09:29 [INFO] L2 cache ID 7: sibling CPUs: [7]
 08:09:29 [INFO] L3 cache ID 0: sibling CPUs: [0, 1, 2, 3]
 08:09:29 [INFO] L3 cache ID 1: sibling CPUs: [4, 5, 6, 7]

Fixes: e3b623d ("topology: Build uniform skip indicies")
Signed-off-by: Andrea Righi <arighi@nvidia.com>

rust/scx_utils/src/topology.rs
@@ -196,16 +196,27 @@ impl Topology {
             let mut node_cores = BTreeMap::new();
             let mut node_cpus = BTreeMap::new();
 
-            for (&llc_id, llc) in node.llcs.iter_mut() {
+            for (&(mut llc_id), llc) in node.llcs.iter_mut() {
                 let llc_mut = Arc::get_mut(llc).unwrap();
                 let mut llc_cpus = BTreeMap::new();
 
+                // The same LLC ID should not span across multiple NUMA nodes. If it does, create a
+                // new artificial LLC for each node. While this should never happen on physical
+                // hardware, it can occur in virtualized environments.
+                if topo_llcs.contains_key(&llc_id) {
+                    llc_id += 1;
+                }
+
                 for (&core_id, core) in llc_mut.cores.iter_mut() {
-                    for (&cpu_id, cpu) in core.cpus.iter() {
+                    let core_mut = Arc::get_mut(core).unwrap();
+                    for (&cpu_id, cpu) in core_mut.cpus.iter_mut() {
+                        let cpu_mut = Arc::get_mut(cpu).unwrap();
+                        cpu_mut.llc_id = llc_id;
+                        cpu_mut.l3_id = llc_id;
                         if topo_cpus
-                            .insert(cpu_id, cpu.clone())
-                            .or(node_cpus.insert(cpu_id, cpu.clone()))
-                            .or(llc_cpus.insert(cpu_id, cpu.clone()))
+                            .insert(cpu_id, cpu_mut.clone().into())
+                            .or(node_cpus.insert(cpu_id, cpu_mut.clone().into()))
+                            .or(llc_cpus.insert(cpu_id, cpu_mut.clone().into()))
                             .is_some()
                         {
                             bail!("Duplicate CPU ID {}", cpu_id);