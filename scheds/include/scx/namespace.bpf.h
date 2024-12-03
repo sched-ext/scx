6879929100ca769cf0426b9652d7a0d7382f583a
Daniel Hodges <hodges.daniel.scott@gmail.com>
2024-12-02 17:59:28 -0800

    scx_layered: Make layered work in pid namespaces
    Add an additional helper function to translate the scx_layered userspace
    tgid when running in a pid namespace.
    
    Signed-off-by: Daniel Hodges <hodges.daniel.scott@gmail.com>

--- scheds/include/scx/namespace.bpf.h
+++ scheds/include/scx/namespace.bpf.h
@@ -5,11 +5,16 @@
 #ifndef __SCHED_EXT_NAMESPACE_BPF_H
 #define __SCHED_EXT_NAMESPACE_BPF_H
 
+#ifdef LSP
+#define __bpf__
+#include "../vmlinux.h"
+#else
 #include "vmlinux.h"
+#endif
 
-struct pid_namespace* get_task_pid_ns(const struct task_struct* task);
+struct pid_namespace* get_task_pid_ns(const struct task_struct* task, enum pid_type);
 struct pid* get_task_pid_ptr(const struct task_struct* task, enum pid_type type);
-pid_t get_task_ns_pid(const struct task_struct* task, enum pid_type type);
+pid_t get_task_ns_pid(const struct task_struct* task);
 
 pid_t get_pid_nr_ns(struct pid* pid, struct pid_namespace* ns);
 pid_t get_ns_pid(void);
