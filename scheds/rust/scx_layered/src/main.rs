f41645022ded38f4f35d1b0036203193f746f899
Tejun Heo <tj@kernel.org>
2024-12-06 12:27:31 -1000

    scx_layered: Fix up rust layer_specs instead of just the BPF side
    Some config range and default value adjustments were made in
    Scheduler::init_layers() while transferring the configurations to the BPF
    side. This makes the rust side layer_specs deviate from the BPF side and
    leads to misbehaviors when e.g. per-layer weights are not specified as 0
    remains 0 which makes layer sizing malfunction.
    
    Fix up layer_specs in main() right after loading so that the rust and BPF
    sides stay consistent.

--- scheds/rust/scx_layered/src/main.rs
+++ scheds/rust/scx_layered/src/main.rs
@@ -1086,7 +1086,6 @@ impl<'a> Scheduler<'a> {
 
     fn init_layers(
         skel: &mut OpenBpfSkel,
-        opts: &Opts,
         specs: &Vec<LayerSpec>,
         topo: &Topology,
     ) -> Result<()> {
@@ -1171,11 +1170,7 @@ impl<'a> Scheduler<'a> {
                     ..
                 } = spec.kind.common();
 
-                layer.slice_ns = if *slice_us > 0 {
-                    *slice_us * 1000
-                } else {
-                    opts.slice_us * 1000
-                };
+                layer.slice_ns = *slice_us * 1000;
                 layer.min_exec_ns = min_exec_us * 1000;
                 layer.yield_step_ns = if *yield_ignore > 0.999 {
                     0
@@ -1191,11 +1186,7 @@ impl<'a> Scheduler<'a> {
                 layer.preempt_first.write(*preempt_first);
                 layer.exclusive.write(*exclusive);
                 layer.growth_algo = growth_algo.as_bpf_enum();
-                layer.weight = if *weight <= MAX_LAYER_WEIGHT && *weight >= MIN_LAYER_WEIGHT {
-                    *weight
-                } else {
-                    DEFAULT_LAYER_WEIGHT
-                };
+                layer.weight = *weight;
                 layer.xllc_mig_min_ns = (xllc_mig_min_us * 1000.0) as u64;
                 layer.owned_usage_target_ppk =
                     Self::layer_owned_usage_target_ppk(spec.kind.util_range(), 0.0);
@@ -1614,7 +1605,7 @@ impl<'a> Scheduler<'a> {
         }
         skel.maps.bss_data.nr_empty_layer_ids = nr_layers as u32;
 
-        Self::init_layers(&mut skel, opts, &layer_specs, &topo)?;
+        Self::init_layers(&mut skel, &layer_specs, &topo)?;
         Self::init_nodes(&mut skel, opts, &topo);
 
         let mut skel = scx_ops_load!(skel, layered, uei)?;
@@ -2318,8 +2309,19 @@ fn main() -> Result<()> {
         );
     }
 
-    for spec in &layer_config.specs {
-        if spec.kind.common().idle_smt.is_some() {
+    for spec in layer_config.specs.iter_mut() {
+        let common = spec.kind.common_mut();
+
+        if common.slice_us == 0 {
+            common.slice_us = opts.slice_us;
+        }
+
+        if common.weight == 0 {
+            common.weight = DEFAULT_LAYER_WEIGHT;
+        }
+        common.weight = common.weight.max(MIN_LAYER_WEIGHT).min(MAX_LAYER_WEIGHT);
+
+        if common.idle_smt.is_some() {
             warn!("Layer {} has deprecated flag \"idle_smt\"", &spec.name);
         }
     }
