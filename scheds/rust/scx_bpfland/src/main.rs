sched-ext/scx

commit 939d4f516c646c376728ae2db84d19d7b743dd38

scx_utils: move autopower into scx_utils

scheds/rust/scx_bpfland/src/main.rs
@@ -34,6 +34,7 @@ use libbpf_rs::ProgramInput;
 use log::info;
 use log::warn;
 use scx_stats::prelude::*;
+use scx_utils::autopower::fetch_power_profile;
 use scx_utils::build_id;
 use scx_utils::import_enums;
 use scx_utils::scx_enums;
@@ -153,7 +154,8 @@ struct Opts {
     /// tasks may overflow to other available CPUs.
     ///
     /// Special values:
-    ///  - "auto" = automatically detect the CPUs based on the current energy profile
+    ///  - "auto" = automatically detect the CPUs based on the active power profile;
+    ///     require power-profiles-daemon being running.
     ///  - "performance" = automatically detect and prioritize the fastest CPUs
     ///  - "powersave" = automatically detect and prioritize the slowest CPUs
     ///  - "all" = all CPUs assigned to the primary domain
@@ -214,7 +216,7 @@ struct Scheduler<'a> {
     skel: BpfSkel<'a>,
     struct_ops: Option<libbpf_rs::Link>,
     opts: &'a Opts,
-    energy_profile: String,
+    power_profile: String,
     stats_server: StatsServer<(), Metrics>,
     user_restart: bool,
 }
@@ -261,13 +263,12 @@ impl<'a> Scheduler<'a> {
         let topo = Topology::new().unwrap();
 
         // Initialize the primary scheduling domain and the preferred domain.
-        let energy_profile = Self::read_energy_profile();
-        if let Err(err) = Self::init_energy_domain(&mut skel, &opts.primary_domain, &energy_profile)
+        let power_profile = fetch_power_profile();
+        if let Err(err) = Self::init_energy_domain(&mut skel, &opts.primary_domain, &power_profile)
         {
             warn!("failed to initialize primary domain: error {}", err);
         }
-        if let Err(err) = Self::init_cpufreq_perf(&mut skel, &opts.primary_domain, &energy_profile)
-        {
+        if let Err(err) = Self::init_cpufreq_perf(&mut skel, &opts.primary_domain, &power_profile) {
             warn!(
                 "failed to initialize cpufreq performance level: error {}",
                 err
@@ -291,7 +292,7 @@ impl<'a> Scheduler<'a> {
             skel,
             struct_ops,
             opts,
-            energy_profile,
+            power_profile,
             stats_server,
             user_restart: false,
         })
@@ -319,30 +320,6 @@ impl<'a> Scheduler<'a> {
         Ok(())
     }
 
-    fn read_energy_profile() -> String {
-        let energy_pref_path =
-            "/sys/devices/system/cpu/cpufreq/policy0/energy_performance_preference";
-        let scaling_governor_path = "/sys/devices/system/cpu/cpufreq/policy0/scaling_governor";
-
-        let res = File::open(energy_pref_path)
-            .and_then(|mut file| {
-                let mut contents = String::new();
-                file.read_to_string(&mut contents)?;
-                Ok(contents.trim().to_string())
-            })
-            .or_else(|_| {
-                File::open(scaling_governor_path)
-                    .and_then(|mut file| {
-                        let mut contents = String::new();
-                        file.read_to_string(&mut contents)?;
-                        Ok(contents.trim().to_string())
-                    })
-                    .or_else(|_| Ok("none".to_string()))
-            });
-
-        res.unwrap_or_else(|_: String| "none".to_string())
-    }
-
     fn epp_to_cpumask(profile: Powermode) -> Result<Cpumask> {
         let mut cpus = get_primary_cpus(profile).unwrap_or(Vec::new());
         if cpus.is_empty() {
@@ -354,18 +331,14 @@ impl<'a> Scheduler<'a> {
     fn init_energy_domain(
         skel: &mut BpfSkel<'_>,
         primary_domain: &String,
-        energy_profile: &String,
+        power_profile: &String,
     ) -> Result<()> {
         let domain = match primary_domain.as_str() {
             "powersave" => Self::epp_to_cpumask(Powermode::Powersave)?,
             "performance" => Self::epp_to_cpumask(Powermode::Performance)?,
-            "auto" => match energy_profile.as_str() {
-                "power" | "balance_power" | "powersave" => {
-                    Self::epp_to_cpumask(Powermode::Powersave)?
-                }
-                "balance_performance" | "performance" => {
-                    Self::epp_to_cpumask(Powermode::Performance)?
-                }
+            "auto" => match power_profile.as_str() {
+                "power-saver" => Self::epp_to_cpumask(Powermode::Powersave)?,
+                "performance" | "balanced" => Self::epp_to_cpumask(Powermode::Performance)?,
                 &_ => Self::epp_to_cpumask(Powermode::Any)?,
             },
             "all" => Self::epp_to_cpumask(Powermode::Any)?,
@@ -394,12 +367,12 @@ impl<'a> Scheduler<'a> {
     fn init_cpufreq_perf(
         skel: &mut BpfSkel<'_>,
         primary_domain: &String,
-        energy_profile: &String,
+        power_profile: &String,
     ) -> Result<()> {
         let perf_lvl: i64 = match primary_domain.as_str() {
-            "auto" => match energy_profile.as_str() {
+            "auto" => match power_profile.as_str() {
                 "performance" => 1024,
-                "power" | "powersave" => 0,
+                "power-saver" => 0,
                 &_ => -1,
             },
             "performance" => 1024,
@@ -421,10 +394,10 @@ impl<'a> Scheduler<'a> {
     }
 
     fn refresh_sched_domain(&mut self) -> bool {
-        if self.energy_profile != "none" {
-            let energy_profile = Self::read_energy_profile();
-            if energy_profile != self.energy_profile {
-                self.energy_profile = energy_profile.clone();
+        if self.power_profile != "none" {
+            let power_profile = fetch_power_profile();
+            if power_profile != self.power_profile {
+                self.power_profile = power_profile.clone();
 
                 if self.opts.primary_domain == "auto" {
                     return true;
@@ -432,7 +405,7 @@ impl<'a> Scheduler<'a> {
                 if let Err(err) = Self::init_cpufreq_perf(
                     &mut self.skel,
                     &self.opts.primary_domain,
-                    &energy_profile,
+                    &power_profile,
                 ) {
                     warn!("failed to refresh cpufreq performance level: error {}", err);
                 }

scheds/rust/scx_lavd/src/main.rs
@@ -49,6 +49,7 @@ use log::info;
 use log::warn;
 use plain::Plain;
 use scx_stats::prelude::*;
+use scx_utils::autopower::fetch_power_profile;
 use scx_utils::build_id;
 use scx_utils::import_enums;
 use scx_utils::scx_enums;
@@ -66,9 +67,6 @@ use stats::SchedSamples;
 use stats::StatsReq;
 use stats::StatsRes;
 use stats::SysStats;
-use zbus::blocking::Connection;
-use zbus::fdo;
-use zbus::proxy;
 
 /// scx_lavd: Latency-criticality Aware Virtual Deadline (LAVD) scheduler
 ///
@@ -82,7 +80,8 @@ struct Opts {
     #[clap(long = "autopilot", action = clap::ArgAction::SetTrue)]
     autopilot: bool,
 
-    /// Automatically decide the scheduler's power mode based on the system's energy profile.
+    /// Automatically decide the scheduler's power mode based on the system's active power profile.
+    /// Require power-profiles-daemon being running.
     #[clap(long = "autopower", action = clap::ArgAction::SetTrue)]
     autopower: bool,
 
@@ -482,16 +481,6 @@ impl FlatTopology {
     }
 }
 
-#[proxy(
-    interface = "net.hadess.PowerProfiles",
-    default_service = "net.hadess.PowerProfiles",
-    default_path = "/net/hadess/PowerProfiles"
-)]
-trait PowerProfiles {
-    #[zbus(property)]
-    fn active_profile(&self) -> fdo::Result<String>;
-}
-
 struct Scheduler<'a> {
     skel: BpfSkel<'a>,
     struct_ops: Option<libbpf_rs::Link>,
@@ -501,15 +490,10 @@ struct Scheduler<'a> {
     monitor_tid: Option<ThreadId>,
     stats_server: StatsServer<StatsReq, StatsRes>,
     mseq_id: u64,
-    power_profiles_proxy: Option<&'a PowerProfilesProxyBlocking<'a>>,
 }
 
 impl<'a> Scheduler<'a> {
-    fn init(
-        opts: &'a Opts,
-        open_object: &'a mut MaybeUninit<OpenObject>,
-        power_profiles_proxy: Option<&'a PowerProfilesProxyBlocking<'a>>,
-    ) -> Result<Self> {
+    fn init(opts: &'a Opts, open_object: &'a mut MaybeUninit<OpenObject>) -> Result<Self> {
         if *NR_CPU_IDS > LAVD_CPU_ID_MAX as usize {
             panic!(
                 "Num possible CPU IDs ({}) exceeds maximum of ({})",
@@ -558,7 +542,6 @@ impl<'a> Scheduler<'a> {
             monitor_tid: None,
             stats_server,
             mseq_id: 0,
-            power_profiles_proxy,
         })
     }
 
@@ -815,15 +798,8 @@ impl<'a> Scheduler<'a> {
         Ok(())
     }
 
-    fn fetch_power_profile(&self) -> String {
-        self.power_profiles_proxy
-            .map(|power_profiles_proxy| power_profiles_proxy.active_profile().ok())
-            .flatten()
-            .unwrap_or_default()
-    }
-
     fn update_power_profile(&mut self, prev_profile: String) -> (bool, String) {
-        let profile = self.fetch_power_profile();
+        let profile = fetch_power_profile();
         if profile == prev_profile {
             // If the profile is the same, skip updaring the profile for BPF.
             return (true, profile);
@@ -952,15 +928,9 @@ fn main() -> Result<()> {
         }
     }
 
-    let system_bus = Connection::system().ok();
-    let power_profiles_proxy = system_bus
-        .as_ref()
-        .map(|system_bus| PowerProfilesProxyBlocking::new(system_bus).ok())
-        .flatten();
-
     let mut open_object = MaybeUninit::uninit();
     loop {
-        let mut sched = Scheduler::init(&opts, &mut open_object, power_profiles_proxy.as_ref())?;
+        let mut sched = Scheduler::init(&opts, &mut open_object)?;
         info!(
             "scx_lavd scheduler is initialized (build ID: {})",
             *build_id::SCX_FULL_VERSION

rust/scx_utils/Cargo.toml
@@ -29,6 +29,7 @@ version-compare = "0.1"
 metrics = "0.23.0"
 metrics-util = "0.17.0"
 libc = "0.2.137"
+zbus = { version = "5", optional = true }
 
 [build-dependencies]
 anyhow = "1.0.65"
@@ -45,3 +46,4 @@ walkdir = "2.4"
 [features]
 default = []
 gpu-topology = ["dep:nvml-wrapper"]
+autopower = ["dep:zbus"]

rust/scx_utils/src/autopower.rs
@@ -1,0 +1,31 @@
+use std::sync::OnceLock;
+
+use zbus::blocking::Connection;
+use zbus::proxy;
+use zbus::Result;
+
+#[proxy(
+    interface = "net.hadess.PowerProfiles",
+    default_service = "net.hadess.PowerProfiles",
+    default_path = "/net/hadess/PowerProfiles"
+)]
+trait PowerProfiles {
+    #[zbus(property)]
+    fn active_profile(&self) -> Result<String>;
+}
+
+static POWER_PROFILES_PROXY: OnceLock<Option<PowerProfilesProxyBlocking<'static>>> =
+    OnceLock::new();
+
+pub fn fetch_power_profile() -> String {
+    POWER_PROFILES_PROXY
+        .get_or_init(|| {
+            let system_bus = Connection::system().ok()?;
+            let system_bus = Box::leak(Box::new(system_bus));
+            PowerProfilesProxyBlocking::new(system_bus).ok()
+        })
+        .as_ref()
+        .map(|power_profiles_proxy| power_profiles_proxy.active_profile().ok())
+        .flatten()
+        .unwrap_or_default()
+}

rust/scx_utils/src/lib.rs
@@ -94,3 +94,6 @@ pub use netdev::NetDev;
 
 pub mod enums;
 pub use enums::scx_enums;
+
+#[cfg(feature = "autopower")]
+pub mod autopower;

scheds/rust/scx_bpfland/Cargo.toml
@@ -15,7 +15,7 @@ libbpf-rs = "0.24.1"
 log = "0.4.17"
 scx_stats = { path = "../../../rust/scx_stats", version = "1.0.6" }
 scx_stats_derive = { path = "../../../rust/scx_stats/scx_stats_derive", version = "1.0.6" }
-scx_utils = { path = "../../../rust/scx_utils", version = "1.0.7" }
+scx_utils = { path = "../../../rust/scx_utils", version = "1.0.7", features = ["autopower"] }
 serde = { version = "1.0", features = ["derive"] }
 simplelog = "0.12"
 

scheds/rust/scx_lavd/Cargo.toml
@@ -21,13 +21,12 @@ log = "0.4.17"
 ordered-float = "3.4.0"
 scx_stats = { path = "../../../rust/scx_stats", version = "1.0.6" }
 scx_stats_derive = { path = "../../../rust/scx_stats/scx_stats_derive", version = "1.0.6" }
-scx_utils = { path = "../../../rust/scx_utils", version = "1.0.7" }
+scx_utils = { path = "../../../rust/scx_utils", version = "1.0.7", features = ["autopower"] }
 serde = { version = "1.0", features = ["derive"] }
 simplelog = "0.12"
 static_assertions = "1.1.0"
 plain = "0.2.3"
 gpoint = "0.2"
-zbus = "5"
 
 [build-dependencies]
 scx_utils = { path = "../../../rust/scx_utils", version = "1.0.7" }

commit 489c9bf94b9ee1fb1c75db2115ac52be6db4733b

scx_bpfland: do not rely on scx_utils::autopower
Relying on scx_utils::autopower reuires power-profiles-daemon running,
this has introduced a regression in certain systems that don't have this
service available.

Restore the old behavior for now, we will fix scx_utils::autopower
later.

Fixes: 939d4f5 ("scx_utils: move autopower into scx_utils")
Signed-off-by: Andrea Righi <arighi@nvidia.com>

scheds/rust/scx_bpfland/src/main.rs
@@ -34,7 +34,6 @@ use libbpf_rs::ProgramInput;
 use log::info;
 use log::warn;
 use scx_stats::prelude::*;
-use scx_utils::autopower::fetch_power_profile;
 use scx_utils::build_id;
 use scx_utils::import_enums;
 use scx_utils::scx_enums;
@@ -154,8 +153,7 @@ struct Opts {
     /// tasks may overflow to other available CPUs.
     ///
     /// Special values:
-    ///  - "auto" = automatically detect the CPUs based on the active power profile;
-    ///     require power-profiles-daemon being running.
+    ///  - "auto" = automatically detect the CPUs based on the current energy profile
     ///  - "performance" = automatically detect and prioritize the fastest CPUs
     ///  - "powersave" = automatically detect and prioritize the slowest CPUs
     ///  - "all" = all CPUs assigned to the primary domain
@@ -216,7 +214,7 @@ struct Scheduler<'a> {
     skel: BpfSkel<'a>,
     struct_ops: Option<libbpf_rs::Link>,
     opts: &'a Opts,
-    power_profile: String,
+    energy_profile: String,
     stats_server: StatsServer<(), Metrics>,
     user_restart: bool,
 }
@@ -263,12 +261,13 @@ impl<'a> Scheduler<'a> {
         let topo = Topology::new().unwrap();
 
         // Initialize the primary scheduling domain and the preferred domain.
-        let power_profile = fetch_power_profile();
-        if let Err(err) = Self::init_energy_domain(&mut skel, &opts.primary_domain, &power_profile)
+        let energy_profile = Self::read_energy_profile();
+        if let Err(err) = Self::init_energy_domain(&mut skel, &opts.primary_domain, &energy_profile)
         {
             warn!("failed to initialize primary domain: error {}", err);
         }
-        if let Err(err) = Self::init_cpufreq_perf(&mut skel, &opts.primary_domain, &power_profile) {
+        if let Err(err) = Self::init_cpufreq_perf(&mut skel, &opts.primary_domain, &energy_profile)
+        {
             warn!(
                 "failed to initialize cpufreq performance level: error {}",
                 err
@@ -292,7 +291,7 @@ impl<'a> Scheduler<'a> {
             skel,
             struct_ops,
             opts,
-            power_profile,
+            energy_profile,
             stats_server,
             user_restart: false,
         })
@@ -320,6 +319,30 @@ impl<'a> Scheduler<'a> {
         Ok(())
     }
 
+    fn read_energy_profile() -> String {
+        let energy_pref_path =
+            "/sys/devices/system/cpu/cpufreq/policy0/energy_performance_preference";
+        let scaling_governor_path = "/sys/devices/system/cpu/cpufreq/policy0/scaling_governor";
+
+        let res = File::open(energy_pref_path)
+            .and_then(|mut file| {
+                let mut contents = String::new();
+                file.read_to_string(&mut contents)?;
+                Ok(contents.trim().to_string())
+            })
+            .or_else(|_| {
+                File::open(scaling_governor_path)
+                    .and_then(|mut file| {
+                        let mut contents = String::new();
+                        file.read_to_string(&mut contents)?;
+                        Ok(contents.trim().to_string())
+                    })
+                    .or_else(|_| Ok("none".to_string()))
+            });
+
+        res.unwrap_or_else(|_: String| "none".to_string())
+    }
+
     fn epp_to_cpumask(profile: Powermode) -> Result<Cpumask> {
         let mut cpus = get_primary_cpus(profile).unwrap_or(Vec::new());
         if cpus.is_empty() {
@@ -331,14 +354,18 @@ impl<'a> Scheduler<'a> {
     fn init_energy_domain(
         skel: &mut BpfSkel<'_>,
         primary_domain: &String,
-        power_profile: &String,
+        energy_profile: &String,
     ) -> Result<()> {
         let domain = match primary_domain.as_str() {
             "powersave" => Self::epp_to_cpumask(Powermode::Powersave)?,
             "performance" => Self::epp_to_cpumask(Powermode::Performance)?,
-            "auto" => match power_profile.as_str() {
-                "power-saver" => Self::epp_to_cpumask(Powermode::Powersave)?,
-                "performance" | "balanced" => Self::epp_to_cpumask(Powermode::Performance)?,
+            "auto" => match energy_profile.as_str() {
+                "power" | "balance_power" | "powersave" => {
+                    Self::epp_to_cpumask(Powermode::Powersave)?
+                }
+                "balance_performance" | "performance" => {
+                    Self::epp_to_cpumask(Powermode::Performance)?
+                }
                 &_ => Self::epp_to_cpumask(Powermode::Any)?,
             },
             "all" => Self::epp_to_cpumask(Powermode::Any)?,
@@ -367,12 +394,12 @@ impl<'a> Scheduler<'a> {
     fn init_cpufreq_perf(
         skel: &mut BpfSkel<'_>,
         primary_domain: &String,
-        power_profile: &String,
+        energy_profile: &String,
     ) -> Result<()> {
         let perf_lvl: i64 = match primary_domain.as_str() {
-            "auto" => match power_profile.as_str() {
+            "auto" => match energy_profile.as_str() {
                 "performance" => 1024,
-                "power-saver" => 0,
+                "power" | "powersave" => 0,
                 &_ => -1,
             },
             "performance" => 1024,
@@ -394,10 +421,10 @@ impl<'a> Scheduler<'a> {
     }
 
     fn refresh_sched_domain(&mut self) -> bool {
-        if self.power_profile != "none" {
-            let power_profile = fetch_power_profile();
-            if power_profile != self.power_profile {
-                self.power_profile = power_profile.clone();
+        if self.energy_profile != "none" {
+            let energy_profile = Self::read_energy_profile();
+            if energy_profile != self.energy_profile {
+                self.energy_profile = energy_profile.clone();
 
                 if self.opts.primary_domain == "auto" {
                     return true;
@@ -405,7 +432,7 @@ impl<'a> Scheduler<'a> {
                 if let Err(err) = Self::init_cpufreq_perf(
                     &mut self.skel,
                     &self.opts.primary_domain,
-                    &power_profile,
+                    &energy_profile,
                 ) {
                     warn!("failed to refresh cpufreq performance level: error {}", err);
                 }