edfb0411ce90818b70a8a14faf0971868ce36de9
Tejun Heo <tj@kernel.org>
2024-12-27 05:06:33 +0900

    scx_layered: Disable owner protection for preempt layers
    Preempt layers are always preferred and owner protection doesn't do anything
    for them. Explicitly disable owner protection for them for clarity. This
    shouldn't change actual scheduling behaviors.

--- scheds/rust/scx_layered/src/main.rs
+++ scheds/rust/scx_layered/src/main.rs
@@ -1688,29 +1688,26 @@ impl<'a> Scheduler<'a> {
             })
             .count() as u32;
 
-        skel.maps.rodata_data.min_open_layer_disallow_open_after_ns = layer_specs
-            .iter()
-            .filter_map(|spec| match &spec.kind {
-                LayerKind::Open { .. } => Some(spec.kind.common().disallow_open_after_us.unwrap()),
-                _ => None,
-            })
-            .min()
-            .unwrap_or(*DFL_DISALLOW_OPEN_AFTER_US)
-            * 1000;
+        let mut min_open = u64::MAX;
+        let mut min_preempt = u64::MAX;
 
+        for spec in layer_specs.iter() {
+            if let LayerKind::Open { common, .. } = &spec.kind {
+                min_open = min_open.min(common.disallow_open_after_us.unwrap());
+                min_preempt = min_preempt.min(common.disallow_preempt_after_us.unwrap());
+            }
+        }
+
+        skel.maps.rodata_data.min_open_layer_disallow_open_after_ns = match min_open {
+            u64::MAX => *DFL_DISALLOW_OPEN_AFTER_US,
+            v => v,
+        };
         skel.maps
             .rodata_data
-            .min_open_layer_disallow_preempt_after_ns = layer_specs
-            .iter()
-            .filter_map(|spec| match &spec.kind {
-                LayerKind::Open { .. } => {
-                    Some(spec.kind.common().disallow_preempt_after_us.unwrap())
-                }
-                _ => None,
-            })
-            .min()
-            .unwrap_or(*DFL_DISALLOW_PREEMPT_AFTER_US)
-            * 1000;
+            .min_open_layer_disallow_preempt_after_ns = match min_preempt {
+            u64::MAX => *DFL_DISALLOW_PREEMPT_AFTER_US,
+            v => v,
+        };
 
         // Consider all layers empty at the beginning.
         for i in 0..layer_specs.len() {
@@ -2433,12 +2430,29 @@ fn main() -> Result<()> {
         }
         common.weight = common.weight.max(MIN_LAYER_WEIGHT).min(MAX_LAYER_WEIGHT);
 
-        if common.disallow_open_after_us.is_none() {
-            common.disallow_open_after_us = Some(*DFL_DISALLOW_OPEN_AFTER_US);
-        }
+        if common.preempt {
+            if common.disallow_open_after_us.is_some() {
+                warn!(
+                    "Preempt layer {} has non-null disallow_open_after_us, ignored",
+                    &spec.name
+                );
+            }
+            if common.disallow_preempt_after_us.is_some() {
+                warn!(
+                    "Preempt layer {} has non-null disallow_preempt_after_us, ignored",
+                    &spec.name
+                );
+            }
+            common.disallow_open_after_us = Some(u64::MAX);
+            common.disallow_preempt_after_us = Some(u64::MAX);
+        } else {
+            if common.disallow_open_after_us.is_none() {
+                common.disallow_open_after_us = Some(*DFL_DISALLOW_OPEN_AFTER_US);
+            }
 
-        if common.disallow_preempt_after_us.is_none() {
-            common.disallow_preempt_after_us = Some(*DFL_DISALLOW_PREEMPT_AFTER_US);
+            if common.disallow_preempt_after_us.is_none() {
+                common.disallow_preempt_after_us = Some(*DFL_DISALLOW_PREEMPT_AFTER_US);
+            }
         }
 
         if common.idle_smt.is_some() {
