a9e837d0f03ca973b93228d1ac4e84096fe6e155
I Hsin Cheng <richard120310@gmail.com>
2024-12-30 03:10:17 +0800

    scx_flash: Option for userspace lock boosting
    Implement an option for users to choose whether to boost the owner of
    user-space lock or not. As in some scenario it might be better to not
    prioritize user-space lock owner, such as database transaction.
    
    Check #1118 for details.
    
    Signed-off-by: I Hsin Cheng <richard120310@gmail.com>

--- scheds/rust/scx_flash/src/bpf/main.bpf.c
+++ scheds/rust/scx_flash/src/bpf/main.bpf.c
@@ -47,6 +47,12 @@ const volatile u64 slice_lag = 20ULL * NSEC_PER_MSEC;
 const volatile bool local_kthreads;
 
 /*
+ * When enabled, threads who's hold a user-space lock and still able to run
+ * will be dispatch first. See flash_dispatch() for details.
+ */
+const volatile bool user_lock_boost;
+
+/*
  * Scheduling statistics.
  */
 volatile u64 nr_kthread_dispatches, nr_direct_dispatches, nr_shared_dispatches;
@@ -822,7 +828,7 @@ void BPF_STRUCT_OPS(flash_dispatch, s32 cpu, struct task_struct *prev)
 	 * If the task can still run and it's holding a user-space lock, let it
 	 * run for another round.
 	 */
-	if (prev && (prev->scx.flags & SCX_TASK_QUEUED) &&
+	if (user_lock_boost && prev && (prev->scx.flags & SCX_TASK_QUEUED) &&
 	    is_task_locked(prev)) {
 		task_unlock(prev);
 		task_refill_slice(prev);
