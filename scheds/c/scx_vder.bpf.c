c8ed6b08d1437ac00527a2b682f0ede93e1f8fce
Andrea Righi <arighi@nvidia.com>
2025-01-03 19:05:13 +0100

    scx_vder: no task context (use p->scx.dsq_vtime directly)
    Signed-off-by: Andrea Righi <arighi@nvidia.com>

--- scheds/c/scx_vder.bpf.c
+++ scheds/c/scx_vder.bpf.c
@@ -42,35 +42,6 @@ const volatile u64 slice_ns;
 static u64 vtime_now;
 
 /*
- * Per-task context.
- */
-struct task_ctx {
-	/*
-	 * The task's deadline (vruntime).
-	 */
-	u64 deadline;
-};
-
-/*
- * BPF map to store the context of each task.
- */
-struct {
-	__uint(type, BPF_MAP_TYPE_TASK_STORAGE);
-	__uint(map_flags, BPF_F_NO_PREALLOC);
-	__type(key, int);
-	__type(value, struct task_ctx);
-} task_ctx_stor SEC(".maps");
-
-/*
- * Return the context of @p, or NULL if the task doesn't exist.
- */
-struct task_ctx *try_lookup_task_ctx(const struct task_struct *p)
-{
-	return bpf_task_storage_get(&task_ctx_stor,
-					(struct task_struct *)p, 0, 0);
-}
-
-/*
  * Return true if vtime @a is before vtime @b, false otherwise.
  */
 static inline bool vtime_before(u64 a, u64 b)
@@ -110,16 +81,7 @@ s32 BPF_STRUCT_OPS(vder_select_cpu, struct task_struct *p, s32 prev_cpu, u64 wak
  */
 void BPF_STRUCT_OPS(vder_enqueue, struct task_struct *p, u64 enq_flags)
 {
-	struct task_ctx *tctx;
 	u64 vtime_min;
-
-	/*
-	 * Get the task context, if a context doesn't exist we can ignore
-	 * the spurious enqueue event.
-	 */
-	tctx = try_lookup_task_ctx(p);
-	if (!tctx)
-		return;
 
 	/*
 	 * Limit the amount of vtime budget that an idling task can
@@ -131,7 +93,7 @@ void BPF_STRUCT_OPS(vder_enqueue, struct task_struct *p, u64 enq_flags)
 	 */
 	vtime_min = vtime_now - slice_ns;
 	if (vtime_before(p->scx.dsq_vtime, vtime_min))
-		tctx->deadline = vtime_min;
+		p->scx.dsq_vtime = vtime_min;
 
 	/*
 	 * Queue the task to the global shared DSQ.
@@ -139,7 +101,7 @@ void BPF_STRUCT_OPS(vder_enqueue, struct task_struct *p, u64 enq_flags)
 	 * Tasks are ordered by their deadline, the task with the earliest
 	 * deadline will be consumed first in ops.dispatch().
 	 */
-	scx_bpf_dsq_insert_vtime(p, SHARED_DSQ, slice_ns, tctx->deadline, enq_flags);
+	scx_bpf_dsq_insert_vtime(p, SHARED_DSQ, slice_ns, p->scx.dsq_vtime, enq_flags);
 }
 
 /*
@@ -158,18 +120,12 @@ void BPF_STRUCT_OPS(vder_dispatch, s32 cpu, struct task_struct *prev)
  */
 void BPF_STRUCT_OPS(vder_running, struct task_struct *p)
 {
-	struct task_ctx *tctx;
-
-	tctx = try_lookup_task_ctx(p);
-	if (!tctx)
-		return;
-
 	/*
 	 * Update the global vruntime as a new task is starting to use a
 	 * CPU.
 	 */
-	if (vtime_before(vtime_now, tctx->deadline))
-		vtime_now = tctx->deadline;
+	if (vtime_before(vtime_now, p->scx.dsq_vtime))
+		vtime_now = p->scx.dsq_vtime;
 }
 
 /*
@@ -177,12 +133,7 @@ void BPF_STRUCT_OPS(vder_running, struct task_struct *p)
  */
 void BPF_STRUCT_OPS(vder_stopping, struct task_struct *p, bool runnable)
 {
-	struct task_ctx *tctx;
 	u64 slice;
-
-	tctx = try_lookup_task_ctx(p);
-	if (!tctx)
-		return;
 
 	/*
 	 * Evaluate the time slice used by the task.
@@ -194,7 +145,7 @@ void BPF_STRUCT_OPS(vder_stopping, struct task_struct *p, bool runnable)
 	/*
 	 * Update task's vruntime.
 	 */
-	tctx->deadline += scale_inverse_fair(p, slice);
+	p->scx.dsq_vtime += scale_inverse_fair(p, slice);
 }
 
 /*
@@ -202,32 +153,10 @@ void BPF_STRUCT_OPS(vder_stopping, struct task_struct *p, bool runnable)
  */
 void BPF_STRUCT_OPS(vder_enable, struct task_struct *p)
 {
-	struct task_ctx *tctx;
-
-	tctx = try_lookup_task_ctx(p);
-	if (!tctx)
-		return;
-
 	/*
 	 * Initialize the task vruntime to the current global vruntime.
 	 */
-	tctx->deadline = vtime_now;
-}
-
-/*
- * Task @p is created.
- */
-s32 BPF_STRUCT_OPS(vder_init_task, struct task_struct *p,
-		   struct scx_init_task_args *args)
-{
-	struct task_ctx *tctx;
-
-	tctx = bpf_task_storage_get(&task_ctx_stor, p, 0,
-				    BPF_LOCAL_STORAGE_GET_F_CREATE);
-	if (!tctx)
-		return -ENOMEM;
-
-	return 0;
+	p->scx.dsq_vtime = vtime_now;
 }
 
 s32 BPF_STRUCT_OPS_SLEEPABLE(vder_init)
@@ -247,7 +176,6 @@ SCX_OPS_DEFINE(vder_ops,
 	       .running			= (void *)vder_running,
 	       .stopping		= (void *)vder_stopping,
 	       .enable			= (void *)vder_enable,
-	       .init_task		= (void *)vder_init_task,
 	       .init			= (void *)vder_init,
 	       .exit			= (void *)vder_exit,
 	       .flags			= SCX_OPS_ENQ_EXITING,
