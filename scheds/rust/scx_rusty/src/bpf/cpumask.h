4268dad413bba2c1a4290b6025ebaab5110b959c
Emil Tsalapatis <etsal@meta.com>
2025-02-05 13:24:11 -0800

    CO-RE relocation bug

--- scheds/rust/scx_rusty/src/bpf/cpumask.h
+++ scheds/rust/scx_rusty/src/bpf/cpumask.h
@@ -2,12 +2,14 @@ int bpf_cpumask_from_mem(struct bpf_cpumask *dst, void *src, size_t src__sz) __k
 int bpf_cpumask_to_mem(void *dst, size_t dst__sz, struct bpf_cpumask *src) __ksym __weak;
 
 extern const volatile u32 nr_cpu_ids;
-size_t mask_size;
 
-/* XXX HACK hardcoded MAX_CPUS / 8 from src/bpf/intf.h, right now this header is a mess */
+/* Mask size in 64-bit words. */
+static size_t mask_size;
+
+/* XXX HACK hardcoded MAX_CPUS / 64 from src/bpf/intf.h, right now this header is a mess */
 struct scx_cpumask {
 	union sdt_id tid;
-	u8 mask[64];
+	u64 bits[8];
 };
 
 typedef struct scx_cpumask __arena * __arg_arena scx_cpumask_t;
@@ -17,9 +19,9 @@ struct sdt_allocator scx_mask_allocator;
 __weak
 int scx_mask_init(__u64 total_mask_size)
 {
-	mask_size = total_mask_size;
+	mask_size = (total_mask_size + 7 / 8);
 
-	return sdt_alloc_init(&scx_mask_allocator, mask_size + sizeof(union sdt_id));
+	return sdt_alloc_init(&scx_mask_allocator, mask_size * 8 + sizeof(union sdt_id));
 }
 
 static
@@ -35,7 +37,7 @@ scx_cpumask_t scx_mask_alloc(void)
 	mask = (scx_cpumask_t)data->payload;
 	mask->tid = data->tid;
 	bpf_for(i, 0, mask_size) {
-		mask->mask[i] = 0ULL;
+		mask->bits[i] = 0ULL;
 	}
 
 	return mask;
@@ -66,9 +68,9 @@ int scxmask_copy_to_stack(struct scx_cpumask *dst, scx_cpumask_t __arg_arena src
 	}
 
 	bpf_for(i, 0, mask_size) {
-		if (i >= 64 || i < 0)
+		if (i >= 8 || i < 0)
 			return 0;
-		dst->mask[i] = src->mask[i];
+		dst->bits[i] = src->bits[i];
 	}
 
 	return 0;
@@ -77,21 +79,21 @@ int scxmask_copy_to_stack(struct scx_cpumask *dst, scx_cpumask_t __arg_arena src
 __weak
 int scxmask_set_cpu(u32 cpu, scx_cpumask_t __arg_arena mask)
 {
-	mask->mask[cpu / 8] |= 1 << (cpu % 8);
+	mask->bits[cpu / 64] |= 1 << (cpu % 64);
 	return 0;
 }
 
 __weak
 int scxmask_clear_cpu(u32 cpu, scx_cpumask_t __arg_arena mask)
 {
-	mask->mask[cpu / 8] &= 1 << ~(cpu % 8);
+	mask->bits[cpu / 64] &= 1 << ~(cpu % 64);
 	return 0;
 }
 
 __weak
 bool scxmask_test_cpu(u32 cpu, scx_cpumask_t __arg_arena mask)
 {
-	return mask->mask[cpu / 8] & (1 << (cpu % 8));
+	return mask->bits[cpu / 64] & (1 << (cpu % 64));
 }
 
 __weak
@@ -100,7 +102,7 @@ int scxmask_clear(scx_cpumask_t __arg_arena mask)
 	int i;
 
 	bpf_for(i, 0, mask_size) {
-		mask->mask[i] = 0;
+		mask->bits[i] = 0;
 	}
 
 	return 0;
@@ -112,7 +114,7 @@ int scxmask_and(scx_cpumask_t __arg_arena dst, scx_cpumask_t __arg_arena src1, s
 	int i;
 
 	bpf_for(i, 0, mask_size) {
-		dst->mask[i] = src1->mask[i] & src2->mask[i];
+		dst->bits[i] = src1->bits[i] & src2->bits[i];
 	}
 
 	return 0;
@@ -124,7 +126,7 @@ bool scxmask_empty(scx_cpumask_t __arg_arena mask)
 	int i;
 
 	bpf_for(i, 0, mask_size) {
-		if (mask->mask[i])
+		if (mask->bits[i])
 			return true;
 	}
 
@@ -155,23 +157,33 @@ scxmask_to_bpf(struct bpf_cpumask __kptr *bpfmask __arg_trusted,
 	}
 }
 
+cpumask_t tmpmask;
 
 /*
  * XXX Terrible implementations. We require a kfunc pair to do this properly.
  */
 static void
-scxmask_from_bpf(scx_cpumask_t __arg_arena scxmask,
-		     const struct cpumask *bpfmask __arg_trusted)
+scxmask_from_bpf(scx_cpumask_t __arg_arena scxmask, const struct cpumask __kptr *bpfmask)
 {
+	size_t len = mask_size;
 	int i;
 
-	bpf_for(i, 0, nr_cpu_ids) {
-		if (bpf_cpumask_test_cpu(i, bpfmask))
-			scxmask_set_cpu(i, scxmask);
-		else
-			scxmask_clear_cpu(i, scxmask);
-	}
+	if (len > sizeof(tmpmask))
+		len = sizeof(tmpmask);
 
+	/*
+	 * We cannot access the BPF mask in a loop. Since we know its exact
+	 * size, but not that of our cpumask type, copy over the bpfmask into a
+	 * temporary buffer, then copy over the valid data from that buffer into
+	 * our cpumask.
+	 */
+	#pragma unroll
+	for (i = 0; i < (sizeof(tmpmask) / sizeof(tmpmask.bits[0])); i++)
+		tmpmask.bits[i] = bpfmask->bits[i];
+
+	bpf_for (i, 0, mask_size) {
+		scxmask->bits[i] = tmpmask.bits[i];
+	}
 }
 
 __weak
@@ -180,7 +192,7 @@ int scxmask_copy(scx_cpumask_t __arg_arena dst, scx_cpumask_t __arg_arena src)
 	int i;
 
 	bpf_for(i, 0, mask_size) {
-		dst->mask[i] = src->mask[i];
+		dst->bits[i] = src->bits[i];
 	}
 
 	return 0;
