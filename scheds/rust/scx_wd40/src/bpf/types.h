ebb3b516e5e4b7fa9d09d0a5e076752136c52220
Emil Tsalapatis <etsal@meta.com>
2025-02-07 11:38:49 -0800

    scx_wd40: remove Clang18 compatibility typedefs

--- scheds/rust/scx_wd40/src/bpf/types.h
+++ scheds/rust/scx_wd40/src/bpf/types.h
@@ -1,28 +1,8 @@
 #ifndef __TYPES_H
 #define __TYPES_H
 
-/*
- * XXXETSAL This is convoluted for a reason. We have a three way conflict here:
- *
- * 1) intf.h gets parsed by bindgen, and I guess bindgen does not fully
- * preprocess the file because it does not resolve __arena to
- * __attribute__((address_space(1))) and errors out. So we cannot use the
- * __arena macro from bpf_arena_common.h that involves all this #ifdef'ing.
- * 2) Clang 18 full-on crashes if we pass address_space(1) as an attribute as it
- * is unable to generate the address space conversion instruction.
- * 3) Clang 19 requires the address_space attribute because address space
- * conversions can only be implicit.
- *
- * The below snippet replicates as tersely as possible the logic in
- * bpf_arena_common.h to get the code passing with both Clang 18 and 19.
- */
-
+typedef struct dom_ctx __arena *dom_ptr;
 struct dom_ctx;
-#if defined(__BPF_FEATURE_ADDR_SPACE_CAST) && !defined(BPF_ARENA_FORCE_ASM)
-typedef struct dom_ctx __attribute__((address_space(1))) *dom_ptr;
-#else
-typedef struct dom_ctx *dom_ptr;
-#endif
 
 struct task_ctx {
 	/* The domains this task can run on */
@@ -65,13 +45,7 @@ struct task_ctx {
 	struct ravg_data dcyc_rd;
 };
 
-/* XXXETSAL Same rationale as for dom_ptr. Remove once we dump Clang 18.*/
-
-#if defined(__BPF_FEATURE_ADDR_SPACE_CAST) && !defined(BPF_ARENA_FORCE_ASM)
-typedef struct task_ctx __attribute__((address_space(1))) *task_ptr;
-#else
-typedef struct task_ctx *task_ptr;
-#endif
+typedef struct task_ctx __arena *task_ptr;
 
 struct bucket_ctx {
 	u64 dcycle;
