sched-ext/scx

commit 43c9759f8c8258db66b593879c08c27ad47d216e

scx_bpfland: fix non-SMT CPU busy state detection
Rename is_full_smt() to is_fully_idle() and return true when SMT is
disabled, to represent the fact that the CPU doesn't have any busy
shared resources.

This solves an issue in non-SMT cases where CPUs were incorrectly
considered always busy.

Fixes: a480d880 ("scx_bpfland: reduce task migrations")
Signed-off-by: Andrea Righi <arighi@nvidia.com>

scheds/rust/scx_bpfland/src/bpf/main.bpf.c
@@ -228,15 +228,16 @@ static bool is_queued(const struct task_struct *p)
 }
 
 /*
- * Return true if @cpu is a full-idle SMT core, false otherwise.
+ * Return true if @cpu is a full-idle SMT core (or if SMT is disabled),
+ * false otherwise.
  */
-static bool is_idle_smt(s32 cpu)
+static bool is_fully_idle(s32 cpu)
 {
 	const struct cpumask *idle_smtmask;
 	bool is_idle;
 
 	if (!smt_enabled)
-		return false;
+		return true;
 
 	idle_smtmask = scx_bpf_get_idle_smtmask();
 	is_idle = bpf_cpumask_test_cpu(cpu, idle_smtmask);
@@ -703,7 +704,7 @@ static bool kick_idle_cpu(const struct task_struct *p, const struct task_ctx *tc
 	/*
 	 * Try to reuse the same CPU if idle.
 	 */
-	if (!idle_smt || (idle_smt && is_idle_smt(prev_cpu))) {
+	if (!idle_smt || (idle_smt && is_fully_idle(prev_cpu))) {
 		if (scx_bpf_test_and_clear_cpu_idle(prev_cpu)) {
 			scx_bpf_kick_cpu(prev_cpu, SCX_KICK_IDLE);
 			return true;
@@ -809,7 +810,7 @@ static bool try_direct_dispatch(struct task_struct *p, struct task_ctx *tctx,
 		 */
 		if (!scx_bpf_dsq_nr_queued(SCX_DSQ_LOCAL_ON | prev_cpu) &&
 		    (local_pcpu || !scx_bpf_dsq_nr_queued(SHARED_DSQ)) &&
-		    is_idle_smt(prev_cpu)) {
+		    is_fully_idle(prev_cpu)) {
 			scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL_ON | prev_cpu,
 					   slice_max, enq_flags);
 			__sync_fetch_and_add(&nr_direct_dispatches, 1);

commit 7a18ed782c3e5c0701f66b16556d7cd693185e4e

scx_bpfland: Fix idle detection when SMT is disabled
is_fully_idle() now checks whether @cpu is in a full-idle physical core
dispite SMT is enabled or not.
This fixes a regression introduced by 43c9759f8c8258db66b593879c08c27ad47d216e.

scheds/rust/scx_bpfland/src/bpf/main.bpf.c
@@ -215,7 +215,7 @@ static bool is_queued(const struct task_struct *p)
 }
 
 /*
- * Return true if @cpu is a full-idle SMT core (or if SMT is disabled),
+ * Return true if @cpu is in a full-idle physical core,
  * false otherwise.
  */
 static bool is_fully_idle(s32 cpu)
@@ -223,9 +223,6 @@ static bool is_fully_idle(s32 cpu)
 	const struct cpumask *idle_smtmask;
 	int node = __COMPAT_scx_bpf_cpu_node(cpu);
 	bool is_idle;
-
-	if (!smt_enabled)
-		return true;
 
 	idle_smtmask = __COMPAT_scx_bpf_get_idle_smtmask_node(node);
 	is_idle = bpf_cpumask_test_cpu(cpu, idle_smtmask);