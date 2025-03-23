43844ec290beb9bec64205b6a5061dacdadd2445
Changwoo Min <changwoo@igalia.com>
2025-03-23 19:30:55 +0900

    scx_lavd: Enable SCX_OPS_ALLOW_QUEUED_WAKEUP when supported.
    Enable the ttwu_queue optimization (SCX_OPS_ALLOW_QUEUED_WAKEUP)
    if the kernel supports it. Since LAVD does not assume that ops.select_cpu()
    is called in all wake-ups, it is okay to enable the option.
    
    Signed-off-by: Changwoo Min <changwoo@igalia.com>

--- scheds/rust/scx_lavd/src/bpf/main.bpf.c
+++ scheds/rust/scx_lavd/src/bpf/main.bpf.c
@@ -1722,9 +1722,9 @@ SCX_OPS_DEFINE(lavd_ops,
 	       .init_task		= (void *)lavd_init_task,
 	       .init			= (void *)lavd_init,
 	       .exit			= (void *)lavd_exit,
-	       .flags			= SCX_OPS_ENQ_LAST |
-					  SCX_OPS_KEEP_BUILTIN_IDLE |
-					  SCX_OPS_ENQ_EXITING,
+	       .flags			= SCX_OPS_ENQ_EXITING |
+					  SCX_OPS_ENQ_LAST |
+					  SCX_OPS_KEEP_BUILTIN_IDLE,
 	       .timeout_ms		= 30000U,
 	       .name			= "lavd");
 
