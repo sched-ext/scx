a254940688c511650f4f52cf6a5a42214c55e4c5
Pat Somaru <patso@likewhatevs.io>
2025-04-01 14:56:16 -0400

    scx_layered: update logic around idle selection

--- scheds/rust/scx_layered/src/bpf/main.bpf.c
+++ scheds/rust/scx_layered/src/bpf/main.bpf.c
@@ -766,31 +766,33 @@ static s32 pick_idle_cpu_from(const struct cpumask *cand_cpumask, s32 prev_cpu,
 	 * partially idle @prev_cpu.
 	 */
 	if (smt_enabled) {
+
+		// try prev if prev_over_idle_core
 		if (prev_in_cand &&
 			layer->prev_over_idle_core) {
-			// try prev if prev_over_idle_core
 			if (scx_bpf_test_and_clear_cpu_idle(prev_cpu))
 				return prev_cpu;
 			prev_in_cand = false;
 		}
 
-		// try prev if smt sibling is empty
+		// try prev if smt sibling empty
 		if (prev_in_cand &&
 			bpf_cpumask_test_cpu(prev_cpu, idle_smtmask) &&
 			scx_bpf_test_and_clear_cpu_idle(prev_cpu))
 			return prev_cpu;
+		prev_in_cand = false;
 
-		// try a wholly idle cpu
+		// try any idle core
 		cpu = scx_bpf_pick_idle_cpu(cand_cpumask, SCX_PICK_IDLE_CORE);
 		if (cpu >= 0)
 			return cpu;
-
-	} else {
-		// if not SMT, try prev
-		if (prev_in_cand &&
-			scx_bpf_test_and_clear_cpu_idle(prev_cpu))
-			return prev_cpu;
 	}
+
+	// try prev if not previously tried and failed
+	if (prev_in_cand &&
+		scx_bpf_test_and_clear_cpu_idle(prev_cpu))
+		return prev_cpu;
+
 	// return any idle cpu
 	return scx_bpf_pick_idle_cpu(cand_cpumask, 0);
 }
