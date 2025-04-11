d487414433646d979a06ec1c249c6cc3c748b5f0
Pat Somaru <patso@likewhatevs.io>
2025-04-11 16:15:38 -0400

    scx_layered: enable tiered toggling of gpu kprobes

--- scheds/rust/scx_layered/src/main.rs
+++ scheds/rust/scx_layered/src/main.rs
@@ -586,9 +586,15 @@ struct Opts {
     #[clap(long, default_value = "false")]
     enable_gpu_support: bool,
 
-    /// Enable nvidia poll kprobe
-    #[clap(long, default_value = "false")]
-    enable_nvidia_poll: bool,
+    /// Gpu Kprobe Level
+    /// The value set here determines how agressive
+    /// the kprobes enabled on gpu driver functions are.
+    /// Higher values are more aggressive, incurring more system overhead
+    /// and more accurately identifying PIDs using GPUs in a more timely manner.
+    /// Lower values incur less system overhead, at the cost of less accurately
+    /// identifying GPU pids and taking longer to do so.
+    #[clap(long, default_value = "1")]
+    gpu_kprobe_level: u64,
 
     /// Enable netdev IRQ balancing. This is experimental and should be used with caution.
     #[clap(long, default_value = "false")]
@@ -1771,11 +1777,17 @@ impl<'a> Scheduler<'a> {
         // enable autoloads for conditionally loaded things
         // immediately after creating skel (because this is always before loading)
         if opts.enable_gpu_support {
-            compat::cond_kprobe_enable("nvidia_open", &skel.progs.kprobe_nvidia_open)?;
-            compat::cond_kprobe_enable("nvidia_mmap", &skel.progs.kprobe_nvidia_mmap)?;
-            // this has it's own flag because it is called so often
-            // that it is suspicious wrt/ perf overhead.
-            if opts.enable_nvidia_poll {
+            // by default, enable open if gpu support is enabled.
+            // open has been observed to be relatively cheap to kprobe.
+            if opts.gpu_kprobe_level >= 1 {
+                compat::cond_kprobe_enable("nvidia_open", &skel.progs.kprobe_nvidia_open)?;
+            }
+            // enable the rest progressively based upon how often they are called
+            // for observed workloads
+            if opts.gpu_kprobe_level >= 2 {
+                compat::cond_kprobe_enable("nvidia_mmap", &skel.progs.kprobe_nvidia_mmap)?;
+            }
+            if opts.gpu_kprobe_level >= 3 {
                 compat::cond_kprobe_enable("nvidia_poll", &skel.progs.kprobe_nvidia_poll)?;
             }
         }
