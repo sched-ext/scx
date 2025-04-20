sched-ext/scx

commit f61405e413465adbfbbda3d0c8212b7276877917

scx_bpfland: Do not scale slice lag with the task weight
Scaling the slice lag with task weight can amplify task prioritization
and may even cause stalls when there is a significant difference in nice
values among tasks.

To prevent this, always use a constant slice lag to determine the
maximum vruntime budget a task can accumulate.

Signed-off-by: Andrea Righi <arighi@nvidia.com>

scheds/rust/scx_bpfland/src/bpf/main.bpf.c
@@ -258,38 +258,11 @@ static u64 nr_tasks_waiting(void)
 }
 
 /*
- * Return task's weight.
- */
-static u64 task_weight(const struct task_struct *p, const struct task_ctx *tctx)
-{
-	return p->scx.weight;
-}
-
-/*
- * Return a value proportionally scaled to the task's priority.
- */
-static u64 scale_up_fair(const struct task_struct *p,
-			 const struct task_ctx *tctx, u64 value)
-{
-	return value * task_weight(p, tctx) / 100;
-}
-
-/*
  * Return a value inversely proportional to the task's priority.
  */
-static u64 scale_inverse_fair(const struct task_struct *p,
-			      const struct task_ctx *tctx, u64 value)
+static u64 scale_inverse_fair(const struct task_struct *p, u64 value)
 {
-	return value * 100 / task_weight(p, tctx);
-}
-
-/*
- * Return the task's allowed lag: used to determine how early its vruntime can
- * be.
- */
-static u64 task_lag(const struct task_struct *p, const struct task_ctx *tctx)
-{
-	return scale_up_fair(p, tctx, slice_lag);
+	return value * 100 / p->scx.weight;
 }
 
 /*
@@ -297,7 +270,7 @@ static u64 task_lag(const struct task_struct *p, const struct task_ctx *tctx)
  */
 static u64 task_deadline(struct task_struct *p, struct task_ctx *tctx)
 {
-	u64 min_vruntime = vtime_now - task_lag(p, tctx);
+	u64 min_vruntime = vtime_now - slice_lag;
 
 	/*
 	 * Per-CPU kthreads are critical for the entire system
@@ -313,7 +286,7 @@ static u64 task_deadline(struct task_struct *p, struct task_ctx *tctx)
 	if (time_before(p->scx.dsq_vtime, min_vruntime))
 		p->scx.dsq_vtime = min_vruntime;
 
-	return p->scx.dsq_vtime + scale_inverse_fair(p, tctx, tctx->sum_runtime);
+	return p->scx.dsq_vtime + scale_inverse_fair(p, tctx->sum_runtime);
 }
 
 static void task_set_domain(struct task_struct *p, s32 cpu,
@@ -951,7 +924,7 @@ void BPF_STRUCT_OPS(bpfland_stopping, struct task_struct *p, bool runnable)
 	/*
 	 * Update task vruntime charging the weighted used time slice.
 	 */
-	p->scx.dsq_vtime += scale_inverse_fair(p, tctx, slice);
+	p->scx.dsq_vtime += scale_inverse_fair(p, slice);
 
 	if (!nvcsw_max_thresh)
 		return;

commit 2437d01d05f0ece531683706cf9916b331150c7d

scx_bpfland: Restore slice_lag tunable
The --slice-lag option was introduced to give users control over the
maximum vruntime budget that an idle task can accumulate, effectively
capping the advantage sleeping tasks receive in scheduling decisions.

A larger slice_lag favors tasks that sleep longer by allowing them to
accumulate more credit, leading to shorter deadlines and earlier
execution. A smaller slice_lag reduces the advantage of long sleeps,
treating short and long sleeps equally once they exceed the threshold.

If slice_lag is negative, it can be used to de-emphasize the
deadline-based scheduling altogether by charging all tasks a fixed
vruntime penalty (equal to the absolute value of slice_lag), effectively
approximating FIFO behavior as the penalty increases.

Commit f61405e4 ("scx_bpfland: Do not scale slice lag with the task
weight") implicitly changed slice_lag to be always equal to slice_max,
which is usually the optimal decision, ignoring the user-specified
slice_lag.

However, there are users that still want to try different settings of
slice_lag under different workloads.

Therefore, ensure the code correctly honor the --slice-lag parameter,
restoring the old behavior and update the comment in the code to better
clarify this concept.

Fixes: f61405e4 ("scx_bpfland: Do not scale slice lag with the task weight")
Signed-off-by: Andrea Righi <arighi@nvidia.com>

scheds/rust/scx_bpfland/src/bpf/main.bpf.c
@@ -363,14 +363,23 @@ static u64 task_deadline(const struct task_struct *p, struct task_ctx *tctx)
 	u64 vtime_min;
 
 	/*
-	 * Limit the amount of vtime budget that an idling task can
-	 * accumulate to prevent excessive prioritization of sleeping
-	 * tasks.
+	 * Cap the vruntime budget that an idle task can accumulate to
+	 * slice_lag, preventing sleeping tasks from gaining excessive
+	 * priority.
 	 *
-	 * Tasks with a higher weight get a bigger "bucket" for their
-	 * allowed accumulated time budget.
+	 * A larger slice_lag favors tasks that sleep longer by allowing
+	 * them to accumulate more credit, leading to shorter deadlines and
+	 * earlier execution. A smaller slice_lag reduces the advantage of
+	 * long sleeps, treating short and long sleeps equally once they
+	 * exceed the threshold.
+	 *
+	 * If slice_lag is negative, it can be used to de-emphasize the
+	 * deadline-based scheduling altogether by charging all tasks a
+	 * fixed vruntime penalty (equal to the absolute value of
+	 * slice_lag), effectively approximating FIFO behavior as the
+	 * penalty increases.
 	 */
-	vtime_min = vtime_now - slice_max;
+	vtime_min = vtime_now - slice_lag;
 	if (time_before(tctx->deadline, vtime_min))
 		tctx->deadline = vtime_min;
 