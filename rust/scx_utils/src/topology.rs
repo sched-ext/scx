98f24eaa45166b7ac01f0c7ef01c471f0e2195b9
Changwoo Min <changwoo@igalia.com>
2025-04-22 10:37:01 +0900

    scx_utils: Create Cpus in a numerical order.
    Previously, the CPUs are created in CPU id's lexicographical order (e.g., 1 ->
    10 -> 11 -> 2), so core ids are assigned following the lexicographical ordering.
    There is nothing wrong with it, but it is unintuitive. Thus, let's iterate and
    create CPUs in numerical order, so assign core id in an intuitive order.
    
    Signed-off-by: Changwoo Min <changwoo@igalia.com>

--- rust/scx_utils/src/topology.rs
+++ rust/scx_utils/src/topology.rs
@@ -785,22 +785,13 @@ fn create_numa_nodes(
             }
         }
 
-        let cpu_pattern = numa_path.join("cpu[0-9]*");
-        let cpu_paths = glob(cpu_pattern.to_string_lossy().as_ref())?;
         let big_little = has_big_little().unwrap_or(false);
         let capacity_src = cpu_capacity_source();
         let avg_cpu_freq = avg_cpu_freq();
-        for cpu_path in cpu_paths.filter_map(Result::ok) {
-            let cpu_str = cpu_path.to_str().unwrap().trim();
-            let cpu_id = match sscanf!(cpu_str, "/sys/devices/system/node/node{usize}/cpu{usize}") {
-                Ok((_, val)) => val,
-                Err(_) => {
-                    bail!("Failed to parse cpu ID {}", cpu_str);
-                }
-            };
-
+        let cpu_ids = read_cpu_ids()?;
+        for cpu_id in cpu_ids.iter() {
             create_insert_cpu(
-                cpu_id,
+                *cpu_id,
                 &mut node,
                 online_mask,
                 topo_ctx,
