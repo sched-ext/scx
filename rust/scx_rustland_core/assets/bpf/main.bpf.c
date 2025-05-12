sched-ext/scx

commit e4fb2c0ffd56c5443cc6cf1cda5244f8d85242af

scx_rustland_core: Improve sync wakeups
Certain pipe-intensive workloads are not capable of maxing out all
available CPUs.

Improve this by routing the wakee to the waker's CPU during sync
wakeups. This helps maintain locality and balance load more effectively,
improving overall CPU utilization.

Example (using
https://github.com/marioroy/mce-sandbox/blob/main/bin/algorithm3.pl):

 $ ./algorithm3.pl 1e12 --threads=8

 - before:

    0[||||||||||||||||||      58.3%]   4[|||||||||||||||||||||||100.0%]
    1[||||||||||||||||||||||||85.9%]   5[||||||||||||||||||||||||95.6%]
    2[||||||||||||||||||||||||87.8%]   6[||||||||||||||||||||||||91.2%]
    3[|||||||||||||||||||||||100.0%]   7[||||||||||||||||||||||||94.4%]

 - after:

    0[|||||||||||||||||||||||100.0%]   4[|||||||||||||||||||||||100.0%]
    1[|||||||||||||||||||||||100.0%]   5[|||||||||||||||||||||||100.0%]
    2[|||||||||||||||||||||||100.0%]   6[|||||||||||||||||||||||100.0%]
    3[|||||||||||||||||||||||100.0%]   7[|||||||||||||||||||||||100.0%]

Signed-off-by: Andrea Righi <arighi@nvidia.com>

rust/scx_rustland_core/assets/bpf/main.bpf.c
@@ -620,11 +620,30 @@ out_release:
 	bpf_task_release(p);
 }
 
+/*
+ * Return true if the waker commits to release the CPU after waking up @p,
+ * false otherwise.
+ */
+static bool is_wake_sync(u64 wake_flags)
+{
+	const struct task_struct *current = (void *)bpf_get_current_task_btf();
+
+	return (wake_flags & SCX_WAKE_SYNC) && !(current->flags & PF_EXITING);
+}
+
 s32 BPF_STRUCT_OPS(rustland_select_cpu, struct task_struct *p, s32 prev_cpu,
 		   u64 wake_flags)
 {
 	bool is_idle = false;
 	s32 cpu;
+
+	/*
+	 * In case of a sync wakeup always try to use the waker's CPU, but
+	 * never perform a direct dispatch otherwise we may have starvation
+	 * with rapid producer/consumer workloads.
+	 */
+	if (is_wake_sync(wake_flags))
+		return bpf_get_smp_processor_id();
 
 	/*
 	 * If built-in idle CPU policy is not enabled completely delegate

commit 40a93212a94c261ad1f2d79a7468c182c5e14e68

scx_rustland_core: Pick waker's CPU as a last resort on sync wakeup
Consider the waker's CPU as idle on a sync wakeup only if we couldn't
find any idle CPU in the system.

This fixes a performance regression (in terms of fps) with certain games
such as Rocket League.

Fixes: e4fb2c0f ("scx_rustland_core: Improve sync wakeups")
Signed-off-by: Andrea Righi <arighi@nvidia.com>

rust/scx_rustland_core/assets/bpf/main.bpf.c
@@ -638,14 +638,6 @@ s32 BPF_STRUCT_OPS(rustland_select_cpu, struct task_struct *p, s32 prev_cpu,
 	s32 cpu;
 
 	/*
-	 * In case of a sync wakeup always try to use the waker's CPU, but
-	 * never perform a direct dispatch otherwise we may have starvation
-	 * with rapid producer/consumer workloads.
-	 */
-	if (is_wake_sync(wake_flags))
-		return bpf_get_smp_processor_id();
-
-	/*
 	 * If built-in idle CPU policy is not enabled completely delegate
 	 * the idle selection policy to user-space and keep re-using the
 	 * same CPU here.
@@ -653,11 +645,22 @@ s32 BPF_STRUCT_OPS(rustland_select_cpu, struct task_struct *p, s32 prev_cpu,
 	if (!builtin_idle || is_usersched_task(p))
 		return prev_cpu;
 
-	cpu = scx_bpf_select_cpu_dfl(p, prev_cpu, wake_flags, &is_idle);
+	/*
+	 * Exclude sync wakeup, since we are handling this special case
+	 * below.
+	 */
+	cpu = scx_bpf_select_cpu_dfl(p, prev_cpu, wake_flags & !SCX_WAKE_SYNC, &is_idle);
 	if (is_idle && !scx_bpf_dsq_nr_queued(SHARED_DSQ)) {
 		scx_bpf_dsq_insert_vtime(p, cpu_to_dsq(cpu), SCX_SLICE_DFL, p->scx.dsq_vtime, 0);
 		__sync_fetch_and_add(&nr_kernel_dispatches, 1);
 	}
+
+	/*
+	 * If we couldn't find an idle CPU, in case of a sync wakeup
+	 * prioritize the waker's CPU.
+	 */
+	if (!is_idle && is_wake_sync(wake_flags))
+		return bpf_get_smp_processor_id();
 
 	return cpu;
 }