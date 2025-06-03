892fa7b45622072541b338d6ea8035592c7870e5
Emil Tsalapatis <emil@etsalapatis.com>
2025-06-02 21:49:32 -0400

    scx_chaos: update calls to arena initialization

--- scheds/rust/scx_chaos/src/lib.rs
+++ scheds/rust/scx_chaos/src/lib.rs
@@ -212,8 +212,17 @@ impl Builder<'_> {
     }
 
     fn setup_topology_node(&self, skel: &mut BpfSkel, mask: &[u64]) -> Result<()> {
-        // Copy the address of ptr to the kernel to populate it from BPF with the arena pointer.
+        let mut args = types::arena_alloc_mask_args {
+            bitmap: 0 as c_ulong,
+        };
+
         let input = ProgramInput {
+            context_in: Some(unsafe {
+                std::slice::from_raw_parts_mut(
+                    &mut args as *mut _ as *mut u8,
+                    std::mem::size_of_val(&args),
+                )
+            }),
             ..Default::default()
         };
 
@@ -225,16 +234,26 @@ impl Builder<'_> {
             );
         }
 
-        let ptr = unsafe {
-            std::mem::transmute::<u64, &mut [u64; 10]>(skel.maps.bss_data.arena_topo_setup_ptr)
-        };
+        let ptr = unsafe { std::mem::transmute::<u64, &mut [u64; 10]>(args.bitmap) };
 
         let (valid_mask, _) = ptr.split_at_mut(mask.len());
         valid_mask.clone_from_slice(mask);
 
+        let mut args = types::arena_topology_node_init_args {
+            bitmap: args.bitmap as c_ulong,
+            data_size: 0 as c_ulong,
+        };
+
         let input = ProgramInput {
+            context_in: Some(unsafe {
+                std::slice::from_raw_parts_mut(
+                    &mut args as *mut _ as *mut u8,
+                    std::mem::size_of_val(&args),
+                )
+            }),
             ..Default::default()
         };
+
         let output = skel.progs.arena_topology_node_init.test_run(input)?;
         if output.return_value != 0 {
             bail!(
