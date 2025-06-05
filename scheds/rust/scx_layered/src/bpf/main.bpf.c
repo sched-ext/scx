sched-ext/scx

commit e2b6cbe7bee7954d58694ed3673e339ee78a05d1

layered: improve compat of excl fixes on older kernels

scheds/rust/scx_layered/src/bpf/main.bpf.c
@@ -1856,27 +1856,20 @@ bool try_consume_layers(u32 *layer_order, u32 nr, u32 exclude_layer_id,
 	return false;
 }
 
-void BPF_STRUCT_OPS(layered_dispatch, s32 cpu, struct task_struct *prev)
+int sib_keep_idle(s32 cpu, struct task_struct *prev __arg_trusted)
 {
 	struct task_ctx *prev_taskc = NULL;
-	struct layer *prev_layer;
+	struct layer *prev_layer = NULL;
 	struct cpu_ctx *cpuc;
-	struct llc_ctx *llcc;
-	bool tried_preempting = false, tried_lo_fb = false;
-	u32 nr_ogp_layers = nr_op_layers + nr_gp_layers;
-	u32 nr_ogn_layers = nr_on_layers + nr_gn_layers;
 
 	if (!(cpuc = lookup_cpu_ctx(-1)))
-		return;
-
-	if (antistall_consume(cpuc))
-		return;
+		return 1;
 
 	/* !NULL prev_taskc indicates runnable prev */
 	if (prev && (prev->scx.flags & SCX_TASK_QUEUED)) {
 		if (!(prev_taskc = lookup_task_ctx(prev)) ||
 		    !(prev_layer = lookup_layer(prev_taskc->layer_id)))
-			return;
+			return 1;
 	}
 
 	/*
@@ -1890,9 +1883,31 @@ void BPF_STRUCT_OPS(layered_dispatch, s32 cpu, struct task_struct *prev)
 		if ((sib = sibling_cpu(cpu)) >= 0 && (sib_cpuc = lookup_cpu_ctx(sib)) &&
 		    (sib_cpuc->current_excl || sib_cpuc->next_excl)) {
 			gstat_inc(GSTAT_EXCL_IDLE, cpuc);
-			return;
+			return 1;
 		}
 	}
+	
+	return 0;
+}
+
+void BPF_STRUCT_OPS(layered_dispatch, s32 cpu, struct task_struct *prev)
+{
+	struct task_ctx *prev_taskc = NULL;
+	struct layer *prev_layer = NULL;
+	struct cpu_ctx *cpuc;
+	struct llc_ctx *llcc;
+	bool tried_preempting = false, tried_lo_fb = false;
+	u32 nr_ogp_layers = nr_op_layers + nr_gp_layers;
+	u32 nr_ogn_layers = nr_on_layers + nr_gn_layers;
+
+	if (!(cpuc = lookup_cpu_ctx(-1)))
+		return;
+
+	if (antistall_consume(cpuc))
+		return;
+	
+	if (sib_keep_idle(cpu, prev))
+		return;
 
 	/*
 	 * if @prev was on SCX and is still runnable, we are here because @prev
@@ -2044,7 +2059,7 @@ void BPF_STRUCT_OPS(layered_dispatch, s32 cpu, struct task_struct *prev)
 	if (!tried_lo_fb && scx_bpf_dsq_move_to_local(cpuc->lo_fb_dsq_id))
 		return;
 
-	if (prev_taskc)
+	if (prev_taskc && prev_layer)
 		prev->scx.slice = prev_layer->slice_ns;
 }
 

commit 38ac7cde18c4733cd19ef22f1e7b78d7b507456a

scx_layered: Unbreak layered_dispatch
Since the fixed commit, prev_taskc and prev_layer are always NULL in
layered_dispatch and all checks conditional on them (keep_running()
heuristics and slice assignment to the previous task) do not occur.

Move the hunk initializing them back to layered_dispatch. Before this,
all keep/max/busy counters for layers are 0, after this they are
populated again.

Restore the comment before slice refresh again for clarity.

Fixes: e2b6cbe7bee7 ("layered: improve compat of excl fixes on older kernels")
Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>

scheds/rust/scx_layered/src/bpf/main.bpf.c
@@ -1967,17 +1967,9 @@ bool try_consume_layers(u32 *layer_order, u32 nr, u32 exclude_layer_id,
 	return false;
 }
 
-bool __always_inline sib_keep_idle(s32 cpu, struct task_struct *prev __arg_trusted, struct cpu_ctx *cpuc)
+bool __always_inline sib_keep_idle(s32 cpu, struct task_struct *prev __arg_trusted, struct layer *prev_layer,
+				   struct task_ctx *prev_taskc, struct cpu_ctx *cpuc)
 {
-	struct task_ctx *prev_taskc = NULL;
-	struct layer *prev_layer = NULL;
-
-	/* !NULL prev_taskc indicates runnable prev */
-	if (prev && (prev->scx.flags & SCX_TASK_QUEUED)) {
-		if (!(prev_taskc = lookup_task_ctx(prev)) ||
-		    !(prev_layer = lookup_layer(prev_taskc->layer_id)))
-			return false;
-	}
 
 	/*
 	 * If the sibling CPU is running an exclusive task, keep this CPU idle
@@ -2013,7 +2005,14 @@ void BPF_STRUCT_OPS(layered_dispatch, s32 cpu, struct task_struct *prev)
 	if (antistall_consume(cpuc))
 		return;
 
-	if (prev && sib_keep_idle(cpu, prev, cpuc))
+	/* !NULL prev_taskc indicates runnable prev */
+	if (prev && (prev->scx.flags & SCX_TASK_QUEUED)) {
+		if (!(prev_taskc = lookup_task_ctx(prev)) ||
+		    !(prev_layer = lookup_layer(prev_taskc->layer_id)))
+			return;
+	}
+
+	if (prev && sib_keep_idle(cpu, prev, prev_layer, prev_taskc, cpuc))
 		return;
 
 	/*
@@ -2166,6 +2165,7 @@ void BPF_STRUCT_OPS(layered_dispatch, s32 cpu, struct task_struct *prev)
 	if (!tried_lo_fb && scx_bpf_dsq_move_to_local(cpuc->lo_fb_dsq_id))
 		return;
 
+	/* !NULL prev_taskc indicates runnable prev */
 	if (prev_taskc && prev_layer)
 		prev->scx.slice = prev_layer->slice_ns;
 }