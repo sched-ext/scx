070c93a8e9bada225caa06ac4612b5f77ba45524
Jake Hillion <jake@hillion.co.uk>
2025-06-11 22:43:45 +0100

    scx_chaos: Add stats interface for chaos injection metrics
    Throw some stats into scx_chaos This adds the unobjectionable stats. I
    think excluded is a subset of skipped, but that's okay. The stats also
    look pretty broken, but that's because chaos is broken :)
    
    Test plan:
    - CI
    
    ```
    jake@rooster:/data/users/jake/repos/scx/ > sudo target/release/scx_chaos --stats 1 --random-delay-frequency 0.5 --random-delay-min-us 1000 --random-delay-max-us 5000
    17:34:48 [INFO] Builder { traits: [RandomDelays { frequency: 0.5, min_us: 1000, max_us: 5000 }], verbose: 0, p2dq_opts: SchedulerOpts { disable_kthreads_local: false, autoslice: false, interactive_ratio: 10, eager_load_balance: false, freq_control: false, greedy_idle_disable: true, interactive_sticky: false, interactive_fifo: false, dispatch_pick2_disable: false, dispatch_lb_busy: 75, dispatch_lb_interactive: false, keep_running: false, wakeup_lb_busy: 0, wakeup_llc_migrations: false, select_idle_in_enqueue: false, idle_resume_us: None, max_dsq_pick2: false, min_slice_us: 100, lb_slack_factor: 5, min_llc_runs_pick2: 3, dsq_time_slices: [], dsq_shift: 4, min_nr_queued_pick2: 0, dumb_queues: 3, init_dsq_index: 0 }, requires_ppid: None }
    17:34:48 [INFO] DSQ[0] slice_ns 100000
    17:34:48 [INFO] DSQ[1] slice_ns 3200000
    17:34:48 [INFO] DSQ[2] slice_ns 6400000
    17:34:48 [INFO] libbpf: struct_ops chaos: member priv not found in kernel, skipping it as it's set to zero
    
    17:34:48 [WARN] libbpf: prog 'scx_bitmap_from_bpf': relo #20: insn #8 (LDX/ST/STX) accesses field incorrectly. Make sure you are accessing pointers, unsigned integers, or fields of matching type and size.
    
    chaos traits: random_delays/cpu_freq/degradation 0/0/0
            chaos excluded/skipped 0/0
            timer kicks: 0
    chaos traits: random_delays/cpu_freq/degradation 1/0/0
            chaos excluded/skipped 0/0
            timer kicks: 1
    chaos traits: random_delays/cpu_freq/degradation 0/0/0
            chaos excluded/skipped 0/0
            timer kicks: 0
    chaos traits: random_delays/cpu_freq/degradation 0/0/0
            chaos excluded/skipped 0/0
            timer kicks: 0
    ```

---
+++ scheds/rust/scx_chaos/src/stats.rs
@@ -0,0 +1,90 @@
+use std::io::Write;
+use std::sync::Arc;
+use std::sync::Condvar;
+use std::sync::Mutex;
+use std::time::Duration;
+
+use anyhow::Result;
+use scx_stats::prelude::*;
+use scx_stats_derive::stat_doc;
+use scx_stats_derive::Stats;
+use serde::Deserialize;
+use serde::Serialize;
+
+#[stat_doc]
+#[derive(Clone, Debug, Default, Serialize, Deserialize, Stats)]
+#[stat(top)]
+pub struct Metrics {
+    #[stat(desc = "Number of times random delay chaos trait was applied")]
+    pub trait_random_delays: u64,
+    #[stat(desc = "Number of times CPU frequency chaos trait was applied")]
+    pub trait_cpu_freq: u64,
+    #[stat(desc = "Number of times performance degradation chaos trait was applied")]
+    pub trait_degradation: u64,
+    #[stat(desc = "Number of times chaos was excluded due to task matching")]
+    pub chaos_excluded: u64,
+    #[stat(desc = "Number of times chaos was skipped (TRAIT_NONE selected)")]
+    pub chaos_skipped: u64,
+    #[stat(desc = "Number of timer-based CPU kicks for delayed tasks")]
+    pub timer_kicks: u64,
+}
+
+impl Metrics {
+    fn format<W: Write>(&self, w: &mut W) -> Result<()> {
+        writeln!(
+            w,
+            "chaos traits: random_delays/cpu_freq/degradation {}/{}/{}\n\tchaos excluded/skipped {}/{}\n\ttimer kicks: {}",
+            self.trait_random_delays,
+            self.trait_cpu_freq,
+            self.trait_degradation,
+            self.chaos_excluded,
+            self.chaos_skipped,
+            self.timer_kicks,
+        )?;
+        Ok(())
+    }
+
+    fn delta(&self, rhs: &Self) -> Self {
+        Self {
+            trait_random_delays: self.trait_random_delays - rhs.trait_random_delays,
+            trait_cpu_freq: self.trait_cpu_freq - rhs.trait_cpu_freq,
+            trait_degradation: self.trait_degradation - rhs.trait_degradation,
+            chaos_excluded: self.chaos_excluded - rhs.chaos_excluded,
+            chaos_skipped: self.chaos_skipped - rhs.chaos_skipped,
+            timer_kicks: self.timer_kicks - rhs.timer_kicks,
+        }
+    }
+}
+
+pub fn server_data() -> StatsServerData<(), Metrics> {
+    let open: Box<dyn StatsOpener<(), Metrics>> = Box::new(move |(req_ch, res_ch)| {
+        req_ch.send(())?;
+        let mut prev = res_ch.recv()?;
+
+        let read: Box<dyn StatsReader<(), Metrics>> = Box::new(move |_args, (req_ch, res_ch)| {
+            req_ch.send(())?;
+            let cur = res_ch.recv()?;
+            let delta = cur.delta(&prev);
+            prev = cur;
+            delta.to_json()
+        });
+
+        Ok(read)
+    });
+
+    StatsServerData::new()
+        .add_meta(Metrics::meta())
+        .add_ops("top", StatsOps { open, close: None })
+}
+
+pub fn monitor(intv: Duration, shutdown: Arc<(Mutex<bool>, Condvar)>) -> Result<()> {
+    scx_utils::monitor_stats::<Metrics>(
+        &[],
+        intv,
+        || {
+            let (lock, _) = &*shutdown;
+            *lock.lock().unwrap()
+        },
+        |metrics| metrics.format(&mut std::io::stdout()),
+    )
+}
