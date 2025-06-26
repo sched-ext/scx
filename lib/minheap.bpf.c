ca3599715fa9e8c99e194ca0c71101f119b37768
Emil Tsalapatis <emil@etsalapatis.com>
2025-06-26 17:09:21 -0400

    lib/minheap: un-inline scx_minheap_pop()

--- lib/minheap.bpf.c
+++ lib/minheap.bpf.c
@@ -123,6 +123,28 @@ int scx_minheap_insert(void __arena *heap_ptr __arg_arena, u64 elem, u64 weight)
 	return 0;
 }
 
+/* Inlined because we are passing a non-arena pointer argument. */
+__hidden
+int scx_minheap_pop(void __arena *heap_ptr __arg_arena, struct scx_minheap_elem *helem __arg_trusted)
+{
+	scx_minheap_t *heap = (scx_minheap_t *)heap_ptr;
+
+	if (heap->size == 0)
+		return -EINVAL;
+
+	helem->elem = heap->helems[0].elem;
+	helem->weight = heap->helems[0].elem;
+
+	heap->helems[0].elem = heap->helems[heap->size - 1].elem;
+	heap->helems[0].weight = heap->helems[heap->size - 1].weight;
+
+	heap->size -= 1;
+
+	scx_minheap_balance_top_down(heap);
+
+	return 0;
+}
+
 __hidden
 int scx_minheap_dump(scx_minheap_t *heap __arg_arena)
 {
--- scheds/include/lib/minheap.h
+++ scheds/include/lib/minheap.h
@@ -19,26 +19,4 @@ u64 scx_minheap_alloc_internal(size_t capacity);
 int scx_minheap_balance_top_down(void __arena *heap_ptr __arg_arena);
 int scx_minheap_insert(void __arena *heap_ptr __arg_arena, u64 elem, u64 weight);
 int scx_minheap_dump(scx_minheap_t *heap __arg_arena);
-
-/* Inlined because we are passing a non-arena pointer argument. */
-static inline
-int scx_minheap_pop(void __arena *heap_ptr __arg_arena, struct scx_minheap_elem *helem __arg_trusted)
-{
-	scx_minheap_t *heap = (scx_minheap_t *)heap_ptr;
-
-	if (heap->size == 0)
-		return -EINVAL;
-
-	helem->elem = heap->helems[0].elem;
-	helem->weight = heap->helems[0].elem;
-
-	heap->helems[0].elem = heap->helems[heap->size - 1].elem;
-	heap->helems[0].weight = heap->helems[heap->size - 1].weight;
-
-	heap->size -= 1;
-
-	scx_minheap_balance_top_down(heap);
-
-	return 0;
-}
-
+int scx_minheap_pop(void __arena *heap_ptr __arg_arena, struct scx_minheap_elem *helem __arg_trusted);
