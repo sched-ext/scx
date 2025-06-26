30ccc0e259a87f5cb176f7fe4a6edb8d80b2edae
ystein <ymstein14@gmail.com>
2025-06-25 17:50:50 -0700

    scx_chaos: revert 09ce862

--- scheds/rust/scx_chaos/src/bpf/main.bpf.c
+++ scheds/rust/scx_chaos/src/bpf/main.bpf.c
@@ -599,7 +599,6 @@ int generic(struct pt_regs *ctx)
 	u32 roll = bpf_get_prandom_u32();
 	if (roll < kprobe_delays_freq_frac32) {
 		taskc->next_trait = CHAOS_TRAIT_RANDOM_DELAYS;
-		p->scx.slice = 0;
 		dbg("GENERIC: setting next_trait to RANDOM_DELAYS - task[%d]", p->pid);
 	}
 
