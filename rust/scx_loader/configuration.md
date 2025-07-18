42b336296cd35601f07f806cdc29d59a8a2a92ae
Piotr Gorski <lucjan.lucjanov@gmail.com>
2025-07-18 09:30:46 +0200

    scx_loader: Add chaos scheduler
    Signed-off-by: Piotr Gorski <lucjan.lucjanov@gmail.com>

--- rust/scx_loader/configuration.md
+++ rust/scx_loader/configuration.md
@@ -76,6 +76,13 @@ gaming_mode = []
 lowlatency_mode = []
 powersave_mode = []
 server_mode = []
+
+[scheds.scx_chaos]
+auto_mode = []
+gaming_mode = []
+lowlatency_mode = ["-y"]
+powersave_mode = []
+server_mode = ["--keep-running"]
 ```
 
 **`default_sched`:**
@@ -132,6 +139,9 @@ The example configuration above shows how to set custom flags for different sche
     * No custom flags are defined, so the default flags for each mode will be used.
 * For `scx_cosmos`:
     * No custom flags are defined, so the default flags for each mode will be used.
+* For `scx_chaos`:
+    * Low Latency mode: `-y`
+    * Server mode: `--keep-running`
 
 ## Fallback Behavior
 
--- rust/scx_loader/src/config.rs
+++ rust/scx_loader/src/config.rs
@@ -87,6 +87,7 @@ pub fn get_default_config() -> Config {
         SupportedSched::Tickless,
         SupportedSched::Rustland,
         SupportedSched::Cosmos,
+        SupportedSched::Chaos,
     ];
     let scheds_map = HashMap::from(supported_scheds.map(|x| init_default_config_entry(x)));
     Config {
@@ -229,6 +230,13 @@ fn get_default_scx_flags_for_mode(scx_sched: &SupportedSched, sched_mode: SchedM
         SupportedSched::Rustland => vec![],
         // scx_cosmos doesn't support any of these modes
         SupportedSched::Cosmos => vec![],
+        SupportedSched::Chaos => match sched_mode {
+            SchedMode::Gaming => vec![],
+            SchedMode::LowLatency => vec!["-y"],
+            SchedMode::PowerSave => vec![],
+            SchedMode::Server => vec!["--keep-running"],
+            SchedMode::Auto => vec![],
+        },
     }
 }
 
@@ -305,6 +313,13 @@ gaming_mode = []
 lowlatency_mode = []
 powersave_mode = []
 server_mode = []
+
+[scheds.scx_chaos]
+auto_mode = []
+gaming_mode = []
+lowlatency_mode = ["-y"]
+powersave_mode = []
+server_mode = ["--keep-running"]
 "#;
 
         let parsed_config = parse_config_content(config_str).expect("Failed to parse config");
