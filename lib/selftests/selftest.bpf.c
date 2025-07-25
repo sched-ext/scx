9046509bb0bad8b84af8a2c6afdeadaedbcab349
Emil Tsalapatis <etsal@meta.com>
2025-07-25 14:05:35 -0700

    lib: add Lev-Chase work stealing queues

--- lib/selftests/selftest.bpf.c
+++ lib/selftests/selftest.bpf.c
@@ -23,6 +23,12 @@ int arena_selftest(void)
 		return ret;
 	}
 
+	ret = scx_selftest_lvqueue();
+	if (ret) {
+		bpf_printk("scx_selftest_lvqueue failed with %d", ret);
+		return ret;
+	}
+
 	ret = scx_selftest_minheap();
 	if (ret) {
 		bpf_printk("scx_selftest_minheap failed with %d", ret);
