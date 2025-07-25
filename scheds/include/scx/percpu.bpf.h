b91d8bf5e818410dc47b155825d88c82dfc4d9fb
Daniel Hodges <hodges.daniel.scott@gmail.com>
2025-07-25 19:54:32 -0400

    percpu: Add helper to get cpufreq_policy
    Add helper to return the percpu cpufreq_policy used by the cpufreq
    governor. Refactor the formatting of helper macros.
    
    Signed-off-by: Daniel Hodges <hodges.daniel.scott@gmail.com>

--- scheds/include/scx/percpu.bpf.h
+++ scheds/include/scx/percpu.bpf.h
@@ -24,51 +24,74 @@ extern struct sugov_cpu sugov_cpu __ksym __weak;
 extern struct psi_group_cpu psi_group_cpu __ksym __weak;
 extern struct kernel_stat kernel_stat __ksym __weak;
 extern struct kernel_cpustat kernel_cpustat __ksym __weak;
+extern struct cpufreq_policy* cpufreq_cpu_data __ksym __weak;
 
-#define DEFINE_PER_CPU_PTR_FUNC(func_name, type, var_name)        \
-type *func_name(s32 cpu) {                                        \
-    type *ptr;                                                    \
-    if (!&var_name)                                               \
-        return NULL;                                              \
-    ptr = bpf_per_cpu_ptr(&var_name, cpu);                        \
-    if (!ptr)                                                     \
-        return NULL;                                              \
-    return ptr;                                                   \
+
+#define DEFINE_PER_CPU_PTR_FUNC(func_name, type, var_name)	\
+type *func_name(s32 cpu)					\
+{								\
+	type *ptr;						\
+								\
+	if (!&var_name)						\
+		return NULL;					\
+								\
+	ptr = bpf_per_cpu_ptr(&var_name, cpu);			\
+	if (!ptr)						\
+		return NULL;					\
+	return ptr;						\
 }
 
-#define DEFINE_PER_CPU_VAL_FUNC(func_name, type, var_name)        \
-type func_name(s32 cpu) {                                         \
-    type *ptr;                                                    \
-    ptr = bpf_per_cpu_ptr(&var_name, cpu);                        \
-    if (!ptr)                                                     \
-        return -EINVAL;                                           \
-    return *ptr;                                                  \
+
+#define DEFINE_PER_CPU_PTR_PTR_FUNC(func_name, type, per_cpu_var_name)		\
+static __always_inline type func_name(s32 cpu)					\
+{										\
+	type *ptr_to_per_cpu_var = bpf_per_cpu_ptr(&per_cpu_var_name, cpu);	\
+										\
+	if (!ptr_to_per_cpu_var)                                                \
+		return NULL;							\
+	return *ptr_to_per_cpu_var;						\
+}
+
+
+#define DEFINE_PER_CPU_VAL_FUNC(func_name, type, var_name)	\
+type func_name(s32 cpu) {					\
+	type *ptr;						\
+								\
+	ptr = bpf_per_cpu_ptr(&var_name, cpu);			\
+	if (!ptr)						\
+		return -EINVAL;					\
+	return *ptr;						\
 }
 
 #define DEFINE_THIS_CPU_VAL_FUNC(orig_func_name)			\
 static inline typeof(orig_func_name(0)) this_##orig_func_name(void) {	\
-    return orig_func_name(bpf_get_smp_processor_id());			\
+	return orig_func_name(bpf_get_smp_processor_id());		\
 }
 
 #define DEFINE_THIS_CPU_PTR_FUNC(orig_func_name)			\
 static inline typeof(orig_func_name(0)) this_##orig_func_name(void) {	\
-    return orig_func_name(bpf_get_smp_processor_id());			\
+	return orig_func_name(bpf_get_smp_processor_id());		\
 }
 
 DEFINE_PER_CPU_VAL_FUNC(cpu_llc_size, int, sd_llc_size)
 DEFINE_PER_CPU_VAL_FUNC(cpu_llc_id, int, sd_llc_id)
 DEFINE_PER_CPU_VAL_FUNC(cpu_priority, int, sched_core_priority)
-DEFINE_PER_CPU_PTR_FUNC(cpu_sugov, struct sugov_cpu, sugov_cpu)
-DEFINE_PER_CPU_PTR_FUNC(cpu_psi_group, struct psi_group_cpu, psi_group_cpu)
-DEFINE_PER_CPU_PTR_FUNC(cpu_kernel_stat, struct kernel_stat, kernel_stat)
-DEFINE_PER_CPU_PTR_FUNC(cpu_kernel_cpustat, struct kernel_cpustat, kernel_cpustat)
 
-DEFINE_THIS_CPU_VAL_FUNC(cpu_llc_size)
+DEFINE_PER_CPU_PTR_PTR_FUNC(cpu_cpufreq_policy, struct cpufreq_policy*, cpufreq_cpu_data)
+
+DEFINE_PER_CPU_PTR_FUNC(cpu_kernel_cpustat, struct kernel_cpustat, kernel_cpustat)
+DEFINE_PER_CPU_PTR_FUNC(cpu_kernel_stat, struct kernel_stat, kernel_stat)
+DEFINE_PER_CPU_PTR_FUNC(cpu_psi_group, struct psi_group_cpu, psi_group_cpu)
+DEFINE_PER_CPU_PTR_FUNC(cpu_sugov, struct sugov_cpu, sugov_cpu)
+
 DEFINE_THIS_CPU_VAL_FUNC(cpu_llc_id)
+DEFINE_THIS_CPU_VAL_FUNC(cpu_llc_size)
 DEFINE_THIS_CPU_VAL_FUNC(cpu_priority)
-DEFINE_THIS_CPU_PTR_FUNC(cpu_sugov)
-DEFINE_THIS_CPU_PTR_FUNC(cpu_psi_group)
-DEFINE_THIS_CPU_PTR_FUNC(cpu_kernel_stat)
+
+DEFINE_THIS_CPU_PTR_FUNC(cpu_cpufreq_policy)
 DEFINE_THIS_CPU_PTR_FUNC(cpu_kernel_cpustat)
+DEFINE_THIS_CPU_PTR_FUNC(cpu_kernel_stat)
+DEFINE_THIS_CPU_PTR_FUNC(cpu_psi_group)
+DEFINE_THIS_CPU_PTR_FUNC(cpu_sugov)
 
 #endif /* BPF_PERCPU_H */
