42758283038d5fabfd8c5a4f1cc91d82f6dede7d
Emil Tsalapatis <emil@etsalapatis.com>
2025-08-25 11:07:12 -0400

    include/lib: refactor sdt_task.h to provide definitions to C userspace

--- scheds/include/lib/sdt_task.h
+++ scheds/include/lib/sdt_task.h
@@ -6,40 +6,20 @@
  */
 #pragma once
 
+#ifdef __BPF__
+
 #include <scx/bpf_arena_common.bpf.h>
 #include <scx/bpf_arena_spin_lock.h>
 #include "sdt_task_defs.h"
+#endif
 
 struct scx_stk_seg;
 typedef struct scx_stk_seg __arena scx_stk_seg_t;
+struct scx_stk;
 
 #define SCX_STK_SEG_MAX (SDT_TASK_ENTS_PER_CHUNK - 2)
 
-struct scx_stk_seg {
-	void __arena	*elems[SCX_STK_SEG_MAX];
-	scx_stk_seg_t	*prev;
-	scx_stk_seg_t	*next;
-};
-
-/*
- * Extensible stack struct.
- */
-struct scx_stk {
-	arena_spinlock_t __arena *lock;
-
-	scx_stk_seg_t *first;	/* First stack segment. */
-	scx_stk_seg_t *last;
-
-	scx_stk_seg_t *current;	/* Current stack segment. */
-	__u64 cind;
-
-	__u64 capacity;		/* Free slots in the stack. */
-	__u64 available;	/* Available items in the stack. */
-	__u64 data_size;
-	__u64 nr_pages_per_alloc;
-
-	scx_stk_seg_t *reserve;
-};
+#ifdef __BPF__
 
 void __arena *scx_task_data(struct task_struct *p);
 int scx_task_init(__u64 data_size);
@@ -148,3 +128,32 @@ int scx_ffs(__u64 word)
 
 	return num;
 }
+
+#endif /* __BPF__ */
+
+struct scx_stk_seg {
+	void __arena	*elems[SCX_STK_SEG_MAX];
+	scx_stk_seg_t	*prev;
+	scx_stk_seg_t	*next;
+};
+
+/*
+ * Extensible stack struct.
+ */
+struct scx_stk {
+	arena_spinlock_t __arena *lock;
+
+	scx_stk_seg_t *first;	/* First stack segment. */
+	scx_stk_seg_t *last;
+
+	scx_stk_seg_t *current;	/* Current stack segment. */
+	__u64 cind;
+
+	__u64 capacity;		/* Free slots in the stack. */
+	__u64 available;	/* Available items in the stack. */
+	__u64 data_size;
+	__u64 nr_pages_per_alloc;
+
+	scx_stk_seg_t *reserve;
+};
+
