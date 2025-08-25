d0c8cfd02a135908d962c80072e4581460dee035
Emil Tsalapatis <emil@etsalapatis.com>
2025-08-25 11:21:40 -0400

    include/lib: fix scx_atq definitions

--- scheds/include/lib/atq.h
+++ scheds/include/lib/atq.h
@@ -13,14 +13,14 @@ struct scx_atq {
 	arena_spinlock_t lock;
 	u64 seq;
 	u64 fifo;
-}
+};
 
-typedef __arena scx_atq, scx_atq_t;
+typedef struct scx_atq __arena scx_atq_t;
 
 u64 scx_atq_create_internal(bool fifo, size_t capacity);
 #define scx_atq_create(fifo) scx_atq_create_internal((fifo), SCX_ATQ_MAX_CAPACITY)
 #define scx_atq_create_size(fifo, capacity) scx_atq_create_internal((fifo), (capacity))
-int scx_atq_insert(scx_atq *atq_ptr, u64 taskc_ptr);
+int scx_atq_insert(scx_atq_t *atq_ptr, u64 taskc_ptr);
 int scx_atq_insert_vtime(scx_atq_t *atq, u64 taskc_ptr, u64 vtime);
 int scx_atq_nr_queued(scx_atq_t *atq);
 u64 scx_atq_pop(scx_atq_t *atq);
