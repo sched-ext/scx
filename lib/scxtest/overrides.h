51cac6effa54bd34451619e20bf6000994ae3f1c
Jake Hillion <jake@hillion.co.uk>
2025-09-01 15:44:38 +0100

    scx_bpf_unittests: fix build and run tests again
    These tests were disabled recently because of missing arena stubs. Add
    them as stubs (for now) and re-enable the tests.
    
    I want to start using these tests in scx_chaos to test for correct
    behaviour without the verifier interfering, which will likely require
    not stubbing the arena code. But this is good enough for now.
    
    Test plan:
    - `cargo test -p scx_bpf_unittests`
    - CI

--- lib/scxtest/overrides.h
+++ lib/scxtest/overrides.h
@@ -25,3 +25,34 @@
 
 /* This is a static helper for some reason, so we have to define it here. */
 #define bpf_get_prandom_u32() 0
+
+/* Arena spinlock stubs for unittest environment */
+#define arena_spin_lock(lock) 0
+#define arena_spin_unlock(lock) do { (void)(lock); } while(0)
+
+/* Define arena_spinlock_t as simple int for unittest environment */
+#define arena_spinlock_t int
+
+/* Stub out __arena for unittest environment */
+#define __arena
+
+
+
+/* Function declarations for BPF functions overridden in overrides.c */
+struct task_struct;
+void *scx_task_data(struct task_struct *p);
+void *scx_minheap_alloc(unsigned int nr_elems);
+int scx_minheap_insert(void *heap_ptr, unsigned long long key, unsigned long long value);
+struct scx_minheap_elem;
+int scx_minheap_pop(void *heap_ptr, struct scx_minheap_elem *helem);
+unsigned long long scx_atq_create_internal(int fifo, unsigned long capacity);
+int scx_atq_insert(void *atq_ptr, unsigned long long taskc_ptr);
+int scx_atq_insert_vtime(void *atq, unsigned long long taskc_ptr, unsigned long long vtime);
+int scx_atq_nr_queued(void *atq);
+unsigned long long scx_atq_pop(void *atq);
+unsigned long long scx_atq_peek(void *atq);
+void *scx_task_alloc(struct task_struct *p);
+void scx_task_free(struct task_struct *p);
+#define scx_atq_create_size(fifo, capacity) scx_atq_create_internal((fifo), (capacity))
+
+
