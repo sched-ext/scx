7f0121008a324b42ca5770dca4437b37359f764a
Emil Tsalapatis <emil@etsalapatis.com>
2025-09-07 21:21:09 -0400

    rust: add arena library initialization code

---
+++ rust/arenalib/arenalib/build.rs
@@ -0,0 +1,21 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+//
+// This software may be used and distributed according to the terms of the
+// GNU General Public License version 2.
+
+fn main() {
+    scx_utils::BpfBuilder::new()
+        .unwrap()
+        .enable_skel("src/bpf/main.bpf.c", "bpf")
+        .add_source("../lib/arena.bpf.c")
+        .add_source("../lib/atq.bpf.c")
+        .add_source("../lib/bitmap.bpf.c")
+        .add_source("../lib/cpumask.bpf.c")
+        .add_source("../lib/minheap.bpf.c")
+        .add_source("../lib/rbtree.bpf.c")
+        .add_source("../lib/sdt_alloc.bpf.c")
+        .add_source("../lib/sdt_task.bpf.c")
+        .add_source("../lib/topology.bpf.c")
+        .compile_link_gen()
+        .unwrap();
+}
