f71a174bfe3b4d17e4c52b3e17052164628504d1
Cheng-Yang Chou <yphbchou0911@gmail.com>
2025-09-16 18:50:48 +0800

    scx_mitosis: Rename for consistency
    Unify BPF source file naming with other schedulers by renaming
    `mitosis.bpf.c` to `main.bpf.c` and updating build.rs accordingly.
    
    Test plan:
    - `cargo build -j`
    
    Signed-off-by: Cheng-Yang Chou <yphbchou0911@gmail.com>

--- scheds/rust/scx_mitosis/build.rs
+++ scheds/rust/scx_mitosis/build.rs
@@ -7,7 +7,7 @@ fn main() {
     scx_cargo::BpfBuilder::new()
         .unwrap()
         .enable_intf("src/bpf/intf.h", "bpf_intf.rs")
-        .enable_skel("src/bpf/mitosis.bpf.c", "bpf")
+        .enable_skel("src/bpf/main.bpf.c", "bpf")
         .build()
         .unwrap();
 }
