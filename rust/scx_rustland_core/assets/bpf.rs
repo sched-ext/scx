sched-ext/scx

commit beab2f381a4bc6539cb0edaaa6bc84c1002df573

scx_rustland_core: Introduce QueuedTask:comm_str()
Add a helper to QueuedTask to retrieve the task's command name.

Having direct access to the executable name from user space can be
useful for classifying workloads based on the application or task name.t

Signed-off-by: Andrea Righi <arighi@nvidia.com>

rust/scx_rustland_core/assets/bpf.rs
@@ -48,6 +48,7 @@ use scx_rustland_core::ALLOCATOR;
 
 // Defined in UAPI
 const SCHED_EXT: i32 = 7;
+const TASK_COMM_LEN: usize = 16;
 
 // Allow to dispatch the task on any CPU.
 //
@@ -77,15 +78,31 @@ pub const RL_CPU_ANY: i32 = bpf_intf::RL_CPU_ANY as i32;
 // Task queued for scheduling from the BPF component (see bpf_intf::queued_task_ctx).
 #[derive(Debug, PartialEq, Eq, PartialOrd, Clone)]
 pub struct QueuedTask {
-    pub pid: i32,             // pid that uniquely identifies a task
-    pub cpu: i32,             // CPU previously used by the task
-    pub nr_cpus_allowed: u64, // Number of CPUs that the task can use
-    pub flags: u64,           // task's enqueue flags
-    pub start_ts: u64,        // Timestamp since last time the task ran on a CPU (in ns)
-    pub stop_ts: u64,         // Timestamp since last time the task released a CPU (in ns)
-    pub exec_runtime: u64,    // Total cpu time since last sleep (in ns)
-    pub weight: u64,          // Task priority in the range [1..10000] (default is 100)
-    pub vtime: u64,           // Current task vruntime / deadline (set by the scheduler)
+    pub pid: i32,                  // pid that uniquely identifies a task
+    pub cpu: i32,                  // CPU previously used by the task
+    pub nr_cpus_allowed: u64,      // Number of CPUs that the task can use
+    pub flags: u64,                // task's enqueue flags
+    pub start_ts: u64,             // Timestamp since last time the task ran on a CPU (in ns)
+    pub stop_ts: u64,              // Timestamp since last time the task released a CPU (in ns)
+    pub exec_runtime: u64,         // Total cpu time since last sleep (in ns)
+    pub weight: u64,               // Task priority in the range [1..10000] (default is 100)
+    pub vtime: u64,                // Current task vruntime / deadline (set by the scheduler)
+    pub comm: [i8; TASK_COMM_LEN], // Task's executable name
+}
+
+impl QueuedTask {
+    /// Convert the task's comm field (C char array) into a Rust String.
+    #[allow(dead_code)]
+    pub fn comm_str(&self) -> String {
+        let bytes: &[u8] =
+            unsafe { std::slice::from_raw_parts(self.comm.as_ptr() as *const u8, self.comm.len()) };
+
+        // Find the first NUL byte, or take the whole array.
+        let nul_pos = bytes.iter().position(|&c| c == 0).unwrap_or(bytes.len());
+
+        // Convert to String (handle invalid UTF-8 gracefully).
+        String::from_utf8_lossy(&bytes[..nul_pos]).into_owned()
+    }
 }
 
 // Task queued for dispatching to the BPF component (see bpf_intf::dispatched_task_ctx).
@@ -149,6 +166,7 @@ impl EnqueuedMessage {
             exec_runtime: self.inner.exec_runtime,
             weight: self.inner.weight,
             vtime: self.inner.vtime,
+            comm: self.inner.comm,
         }
     }
 }

scheds/rust/scx_rlfifo/src/main.rs
@@ -55,6 +55,7 @@
 //!     pub exec_runtime: u64,     // Total cpu time since last sleep (in ns)
 //!     pub weight: u64,           // Task priority in the range [1..10000] (default is 100)
 //!     pub vtime: u64,            // Current task vruntime / deadline (set by the scheduler)
+//!     pub comm: [i8; TASK_COMM_LEN], // Task's executable name
 //! }
 //!
 //! Each task dispatched using dispatch_task() contains the following:

rust/scx_rustland_core/assets/bpf/intf.h
@@ -45,6 +45,10 @@ typedef int pid_t;
  */
 #define MAX_CPUS 1024
 
+#ifndef TASK_COMM_LEN
+#define TASK_COMM_LEN	16
+#endif
+
 /* Special dispatch flags */
 enum {
 	/*
@@ -89,6 +93,7 @@ struct queued_task_ctx {
 	u64 exec_runtime; /* Total cpu time since last sleep */
 	u64 weight; /* Task static priority */
 	u64 vtime; /* Current task's vruntime */
+	char comm[TASK_COMM_LEN]; /* Task's executable name */
 };
 
 /*

rust/scx_rustland_core/assets/bpf/main.bpf.c
@@ -782,6 +782,8 @@ static void get_task_info(struct queued_task_ctx *task,
 	task->exec_runtime = tctx ? tctx->exec_runtime : 0;
 	task->weight = p->scx.weight;
 	task->vtime = p->scx.dsq_vtime;
+
+	bpf_core_read(&task->comm, sizeof(task->comm), &p->comm);
 }
 
 /*

commit 9b3fce70a0a1b13837315f3022e50231e3b355c7

scx_rustland_core: Fix char type on arm64
Commit beab2f38 ("scx_rustland_core: Introduce QueuedTask:comm_str()")
introduced the following build error on arm64:

error[E0308]: mismatched types
   --> scheds/rust/scx_rustland/src/bpf.rs:167:19
    |
167 |             comm: self.inner.comm,
    |                   ^^^^^^^^^^^^^^^ expected `[i8; 16]`, found `[u8; 16]`
    |
    = note: expected array `[i8; 16]`
               found array `[u8; 16]`

Fix by declaring comm as an array of c_char elements, instead of i8,
ensuring portability across different architectures.

Fixes: beab2f38 ("scx_rustland_core: Introduce QueuedTask:comm_str()")
Signed-off-by: Andrea Righi <arighi@nvidia.com>

rust/scx_rustland_core/assets/bpf.rs
@@ -30,7 +30,7 @@ use libbpf_rs::libbpf_sys::bpf_object_open_opts;
 use libbpf_rs::OpenObject;
 use libbpf_rs::ProgramInput;
 
-use libc::{pthread_self, pthread_setschedparam, sched_param};
+use libc::{pthread_self, pthread_setschedparam, sched_param, c_char};
 
 #[cfg(target_env = "musl")]
 use libc::timespec;
@@ -87,7 +87,7 @@ pub struct QueuedTask {
     pub exec_runtime: u64,         // Total cpu time since last sleep (in ns)
     pub weight: u64,               // Task priority in the range [1..10000] (default is 100)
     pub vtime: u64,                // Current task vruntime / deadline (set by the scheduler)
-    pub comm: [i8; TASK_COMM_LEN], // Task's executable name
+    pub comm: [c_char; TASK_COMM_LEN], // Task's executable name
 }
 
 impl QueuedTask {

scheds/rust/scx_rlfifo/src/main.rs
@@ -55,7 +55,7 @@
 //!     pub exec_runtime: u64,     // Total cpu time since last sleep (in ns)
 //!     pub weight: u64,           // Task priority in the range [1..10000] (default is 100)
 //!     pub vtime: u64,            // Current task vruntime / deadline (set by the scheduler)
-//!     pub comm: [i8; TASK_COMM_LEN], // Task's executable name
+//!     pub comm: [c_char; TASK_COMM_LEN], // Task's executable name
 //! }
 //!
 //! Each task dispatched using dispatch_task() contains the following: