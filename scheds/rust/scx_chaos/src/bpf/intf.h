0547166b25b970d889826d48a113f4528470fc8e
Jake Hillion <jake@hillion.co.uk>
2025-10-07 21:48:44 +0100

    ci: clang-format scx_chaos
    In #2718 we added CI checked clang-format for scxtop. The formatting is
    okay, and it's improved consistency a lot, especially when it comes to
    random contributors or vibe coding.
    
    Move the scxtop .clang-format, which is the kernel's, to the root of the
    repository. Mitosis has a custom .clang-format and that will continue
    applying.
    
    Test plan:
    - CI

--- scheds/rust/scx_chaos/src/bpf/intf.h
+++ scheds/rust/scx_chaos/src/bpf/intf.h
@@ -10,19 +10,19 @@ typedef unsigned long long u64;
 #endif
 
 enum chaos_consts {
-	CHAOS_DSQ_BASE_SHIFT	= 16,
-	CHAOS_DSQ_BASE		= 1 << CHAOS_DSQ_BASE_SHIFT,
+	CHAOS_DSQ_BASE_SHIFT  = 16,
+	CHAOS_DSQ_BASE	      = 1 << CHAOS_DSQ_BASE_SHIFT,
 
-	CHAOS_NUM_PPIDS_CHECK	= 1 << 20,
+	CHAOS_NUM_PPIDS_CHECK = 1 << 20,
 };
 
 enum chaos_match {
-	CHAOS_MATCH_UNKNOWN		= 0,
-	CHAOS_MATCH_COMPLETE		= 1 << 0,
-	CHAOS_MATCH_EXCLUDED		= 1 << 1,
-	CHAOS_MATCH_HAS_PARENT		= 1 << 2,
+	CHAOS_MATCH_UNKNOWN    = 0,
+	CHAOS_MATCH_COMPLETE   = 1 << 0,
+	CHAOS_MATCH_EXCLUDED   = 1 << 1,
+	CHAOS_MATCH_HAS_PARENT = 1 << 2,
 
-	CHAOS_MATCH_MAX			= 1 << 3,
+	CHAOS_MATCH_MAX	       = 1 << 3,
 };
 
 enum chaos_trait_kind {
@@ -36,12 +36,12 @@ enum chaos_trait_kind {
 
 struct chaos_task_ctx {
 	// chaos_task_ctx is initialised zero'd
-	enum chaos_match	match;
+	enum chaos_match      match;
 
-	enum chaos_trait_kind	next_trait;
-	enum chaos_trait_kind	pending_trait;
-	u64			enq_flags;
-	u64			p2dq_vtime;
+	enum chaos_trait_kind next_trait;
+	enum chaos_trait_kind pending_trait;
+	u64		      enq_flags;
+	u64		      p2dq_vtime;
 };
 
 enum chaos_stat_idx {
