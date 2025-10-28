sched-ext/scx

commit 8f20db19336bba22df6761b52bac46a85f2571a2

scx_utils, scx_lavd: Check if ftrace is enabled before tracing futex.
When the kernel is built without ftrace support, setting the autoload of
futex functions using ftrace causes the verifier errors. So, let's check
if the kernel supports ftrace before tracing the futex functions.

Signed-off-by: Changwoo Min <changwoo@igalia.com>

scheds/rust/scx_lavd/src/main.rs
@@ -455,6 +455,11 @@ impl<'a> Scheduler<'a> {
             ("futex_unlock_pi", &skel.progs.fexit_futex_unlock_pi),
         ];
 
+        if compat::tracer_available("function_graph")? == false {
+            info!("Ftrace is not enabled in the kernel.");
+            return Ok(false);
+        }
+
         compat::cond_kprobes_enable(ftraces)
     }
 

rust/scx_utils/src/compat.rs
@@ -214,6 +214,25 @@ pub fn debugfs_mount() -> Result<std::path::PathBuf> {
     mounts.into_iter().next().context("No debugfs mount found")
 }
 
+pub fn tracer_available(tracer: &str) -> Result<bool> {
+    let base_path = tracefs_mount().unwrap_or_else(|_| debugfs_mount().unwrap().join("tracing"));
+    let file = match std::fs::File::open(base_path.join("available_tracers")) {
+        Ok(f) => f,
+        Err(_) => return Ok(false),
+    };
+    let reader = std::io::BufReader::new(file);
+
+    for line in reader.lines() {
+        for tc in line.unwrap().split_whitespace() {
+            if tracer == tc {
+                return Ok(true);
+            }
+        }
+    }
+
+    Ok(false)
+}
+
 pub fn tracepoint_exists(tracepoint: &str) -> Result<bool> {
     let base_path = tracefs_mount().unwrap_or_else(|_| debugfs_mount().unwrap().join("tracing"));
     let file = match std::fs::File::open(base_path.join("available_events")) {

commit 7904017a3fa0946c0b5711d080a291ddee1fb28c

scx_lavd: Fix the required tracer name for futex tracing.
'fexit', which is used for futex tracing, requires a "function" tracer,
not a "function_graph" tracer. So, let's fix it.

Fixes: 8f20db19336b ("scx_utils, scx_lavd: Check if ftrace is enabled before tracing futex.")
Signed-off-by: Changwoo Min <changwoo@igalia.com>

scheds/rust/scx_lavd/src/main.rs
@@ -455,7 +455,7 @@ impl<'a> Scheduler<'a> {
             ("futex_unlock_pi", &skel.progs.fexit_futex_unlock_pi),
         ];
 
-        if compat::tracer_available("function_graph")? == false {
+        if compat::tracer_available("function")? == false {
             info!("Ftrace is not enabled in the kernel.");
             return Ok(false);
         }