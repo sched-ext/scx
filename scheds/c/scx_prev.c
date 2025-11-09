f65d65e5566919e834154e63b39c308e3120307b
David Carlier <devnexen@gmail.com>
2025-11-09 08:51:06 +0000

    simplify fix

--- scheds/c/scx_prev.c
+++ scheds/c/scx_prev.c
@@ -49,11 +49,10 @@ static void read_stats(struct scx_prev *skel, __u64 *stats)
 	__u32 idx;
 
 	memset(stats, 0, sizeof(stats[0]) * 4);
+	memset(cnts, 0, sizeof(cnts));
 
 	for (idx = 0; idx < 4; idx++) {
 		int ret, cpu;
-		for (cpu = 0; cpu < nr_cpus; cpu++)
-			cnts[idx][cpu] = 0;
 
 		ret = bpf_map_lookup_elem(bpf_map__fd(skel->maps.stats),
 					  &idx, cnts[idx]);
