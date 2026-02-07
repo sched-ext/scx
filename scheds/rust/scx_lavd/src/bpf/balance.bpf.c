b40917e3f7ee4308d52a74446f35b1bafd43b220
Gavin Guo <gavinguo@igalia.com>
2026-02-07 23:01:19 +0800

    scx_lavd: Always use avg_util_wall_sum for stable load balancing
    In current implementation, plan_x_cpdom_migration() uses cur_util_wall_sum,
    which fluctuates significantly between sampling, when mig_delta_pct is
    not set. Since periodic load balancing aims for stable and long-term
    balance and shouldn't be adjusted to spontaneous spikes, let's use
    avg_util_wall_sum for the scaled load calculation.
    
    Signed-off-by: Gavin Guo <gavinguo@igalia.com>

--- scheds/rust/scx_lavd/src/bpf/balance.bpf.c
+++ scheds/rust/scx_lavd/src/bpf/balance.bpf.c
@@ -78,12 +78,9 @@ int plan_x_cpdom_migration(void)
 		}
 
 		/*
-		 * Use avg_util_sum when mig_delta_pct is set, otherwise use cur_util_sum.
+		 * Use avg_util_wall_sum for stable load balancing decisions.
 		 */
-		if (mig_delta_pct > 0)
-			util = (cpdomc->avg_util_wall_sum << LAVD_SHIFT) / cpdomc->nr_active_cpus;
-		else
-			util = (cpdomc->cur_util_wall_sum << LAVD_SHIFT) / cpdomc->nr_active_cpus;
+		util = (cpdomc->avg_util_wall_sum << LAVD_SHIFT) / cpdomc->nr_active_cpus;
 		qlen = cpdomc->nr_queued_task;
 		qlen_invr = (qlen << (LAVD_SHIFT * 3)) / cpdomc->cap_sum_active_cpus;
 		cpdomc->load_invr = util + qlen_invr;
