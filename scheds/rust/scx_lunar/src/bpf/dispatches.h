0d17223fd96ce2e2d3bcac8068083600d85742bf
Timon <timon2201@gmail.com>
2026-07-04 18:30:39 +0200

    update

--- scheds/rust/scx_lunar/src/bpf/dispatches.h
+++ scheds/rust/scx_lunar/src/bpf/dispatches.h
@@ -29,25 +29,60 @@ static __always_inline u64 try_acquire_task_from_other_cpu(u64 dsqType, u32 cpu,
 
 static __always_inline u64 dispatch_dsq_per_cpu(u32 cpu, struct dispatch_ctx* ctx)
 {
-  // if (scx_bpf_dsq_move_to_local(DSQ_CPU_QUEUE_BASE_HARD + cpu))
-  // {
-  //   return DSQ_TYPE_HARD;
-  // }
+  bool lc_ok = ctx->runtime_per_queue[DSQ_TYPE_LC] < MAX_CONTINUOUS_LC_TIME;
+  bool int_ok = ctx->runtime_per_queue[DSQ_TYPE_INTERACTIVE] < MAX_CONTINUOUS_INTERACTIVE_TIME;
+  bool norm_ok = ctx->runtime_per_queue[DSQ_TYPE_NORMAL] < MAX_CONTINUOUS_NORMAL_TIME;
+  bool batch_ok = ctx->runtime_per_queue[DSQ_TYPE_BATCH] < MAX_CONTINUOUS_BATCH_TIME;
 
-  // if (try_acquire_task_from_other_cpu(DSQ_TYPE_HARD, cpu, true) != DSQ_TYPE_EMPTY)
-  // {
-  //   return DSQ_TYPE_HARD;
-  // }
-
+  /* gated pass: priority order, over-budget queues skipped */
   if (scx_bpf_dsq_move_to_local(DSQ_CPU_QUEUE_BASE_SOFT + cpu, 0))
-  {
     return DSQ_TYPE_SOFT;
-  }
+  if (lc_ok && scx_bpf_dsq_move_to_local(DSQ_CPU_QUEUE_BASE_LC + cpu, 0))
+    return DSQ_TYPE_LC;
+  if (int_ok && scx_bpf_dsq_move_to_local(DSQ_CPU_QUEUE_BASE_INTERACTIVE + cpu, 0))
+    return DSQ_TYPE_INTERACTIVE;
+  if (norm_ok && scx_bpf_dsq_move_to_local(DSQ_CPU_QUEUE_BASE_NORMAL + cpu, 0))
+    return DSQ_TYPE_NORMAL;
+  if (batch_ok && scx_bpf_dsq_move_to_local(DSQ_CPU_QUEUE_BASE_BATCH + cpu, 0))
+    return DSQ_TYPE_BATCH;
+  if (scx_bpf_dsq_move_to_local(DSQ_CPU_QUEUE_BASE_GREEDY + cpu, 0))
+    return DSQ_TYPE_GREEDY;
+
+  /* [cross-LLC steal pass with the same gates] */
 
   if (try_acquire_task_from_other_cpu(DSQ_TYPE_SOFT, cpu, true) != DSQ_TYPE_EMPTY)
-  {
     return DSQ_TYPE_SOFT;
+  if (lc_ok && try_acquire_task_from_other_cpu(DSQ_TYPE_LC, cpu, true) != DSQ_TYPE_EMPTY)
+    return DSQ_TYPE_LC;
+  if (int_ok && try_acquire_task_from_other_cpu(DSQ_TYPE_INTERACTIVE, cpu, true) != DSQ_TYPE_EMPTY)
+    return DSQ_TYPE_INTERACTIVE;
+  if (norm_ok && try_acquire_task_from_other_cpu(DSQ_TYPE_NORMAL, cpu, true) != DSQ_TYPE_EMPTY)
+    return DSQ_TYPE_NORMAL;
+  if (batch_ok && try_acquire_task_from_other_cpu(DSQ_TYPE_BATCH, cpu, true) != DSQ_TYPE_EMPTY)
+    return DSQ_TYPE_BATCH;
+  if (try_acquire_task_from_other_cpu(DSQ_TYPE_GREEDY, cpu, true) != DSQ_TYPE_EMPTY)
+    return DSQ_TYPE_GREEDY;
+
+  if (nr_llcs > 1)
+  {
+    if (try_acquire_task_from_other_cpu(DSQ_TYPE_SOFT, cpu, false) != DSQ_TYPE_EMPTY)
+      return DSQ_TYPE_SOFT;
+    if (lc_ok && try_acquire_task_from_other_cpu(DSQ_TYPE_LC, cpu, false) != DSQ_TYPE_EMPTY)
+      return DSQ_TYPE_LC;
+    if (int_ok && try_acquire_task_from_other_cpu(DSQ_TYPE_INTERACTIVE, cpu, false) != DSQ_TYPE_EMPTY)
+      return DSQ_TYPE_INTERACTIVE;
+    if (norm_ok && try_acquire_task_from_other_cpu(DSQ_TYPE_NORMAL, cpu, false) != DSQ_TYPE_EMPTY)
+      return DSQ_TYPE_NORMAL;
+    if (batch_ok && try_acquire_task_from_other_cpu(DSQ_TYPE_BATCH, cpu, false) != DSQ_TYPE_EMPTY)
+      return DSQ_TYPE_BATCH;
+    if (try_acquire_task_from_other_cpu(DSQ_TYPE_GREEDY, cpu, false) != DSQ_TYPE_EMPTY)
+      return DSQ_TYPE_GREEDY;
   }
+
+  ctx->runtime_per_queue[DSQ_TYPE_LC] = 0;
+  ctx->runtime_per_queue[DSQ_TYPE_INTERACTIVE] = 0;
+  ctx->runtime_per_queue[DSQ_TYPE_NORMAL] = 0;
+  ctx->runtime_per_queue[DSQ_TYPE_BATCH] = 0;
 
   if (scx_bpf_dsq_move_to_local(DSQ_CPU_QUEUE_BASE_LC + cpu, 0))
   {
@@ -86,28 +121,8 @@ static __always_inline u64 dispatch_dsq_per_cpu(u32 cpu, struct dispatch_ctx* ct
     return DSQ_TYPE_BATCH;
   }
 
-  if (scx_bpf_dsq_move_to_local(DSQ_CPU_QUEUE_BASE_GREEDY + cpu, 0))
-  {
-    return DSQ_TYPE_GREEDY;
-  }
-
-  if (try_acquire_task_from_other_cpu(DSQ_TYPE_GREEDY, cpu, true) != DSQ_TYPE_EMPTY)
-  {
-    return DSQ_TYPE_GREEDY;
-  }
-
   if (nr_llcs > 1)
   {
-    // if (try_acquire_task_from_other_cpu(DSQ_TYPE_HARD, cpu, false) != DSQ_TYPE_EMPTY)
-    // {
-    //   return DSQ_TYPE_HARD;
-    // }
-
-    if (try_acquire_task_from_other_cpu(DSQ_TYPE_SOFT, cpu, false) != DSQ_TYPE_EMPTY)
-    {
-      return DSQ_TYPE_SOFT;
-    }
-
     if (try_acquire_task_from_other_cpu(DSQ_TYPE_LC, cpu, false) != DSQ_TYPE_EMPTY)
     {
       return DSQ_TYPE_LC;
@@ -126,11 +141,6 @@ static __always_inline u64 dispatch_dsq_per_cpu(u32 cpu, struct dispatch_ctx* ct
     if (try_acquire_task_from_other_cpu(DSQ_TYPE_BATCH, cpu, false) != DSQ_TYPE_EMPTY)
     {
       return DSQ_TYPE_BATCH;
-    }
-
-    if (try_acquire_task_from_other_cpu(DSQ_TYPE_GREEDY, cpu, false) != DSQ_TYPE_EMPTY)
-    {
-      return DSQ_TYPE_GREEDY;
     }
   }
   return DSQ_TYPE_EMPTY;
