efef4db14bcd1db7bebe965417e46b20a84bc424
Changwoo Min <changwoo@igalia.com>
2026-08-31 17:34:43 +0900

    scx_lavd: wrap overflow cpumask mutations in helpers
    Every mutation of the global overflow cpumask open-codes a
    bpf_cpumask_*_cpu() call with the cpu and mask arguments in the bpf
    order. Wrap the set and clear paths in ovrflw_test_and_set() and
    ovrflw_test_and_clear() and convert all per-CPU mutation sites.
    
    This gives later work a single choke point for overflow-set bookkeeping.
    
    No functional change.
    
    Signed-off-by: Changwoo Min <changwoo@igalia.com>

--- scheds/rust/scx_lavd/src/bpf/power.bpf.c
+++ scheds/rust/scx_lavd/src/bpf/power.bpf.c
@@ -371,7 +371,7 @@ int do_core_compaction(void)
 		cpuc = get_cpu_ctx_id(cpu);
 		if (!cpuc || !cpuc->is_online) {
 			bpf_cpumask_clear_cpu(cpu, active);
-			bpf_cpumask_clear_cpu(cpu, ovrflw);
+			ovrflw_test_and_clear(ovrflw, cpu);
 			continue;
 		}
 
@@ -380,7 +380,7 @@ int do_core_compaction(void)
 		 */
 		if (i < nr_active) {
 			bpf_cpumask_set_cpu(cpu, active);
-			bpf_cpumask_clear_cpu(cpu, ovrflw);
+			ovrflw_test_and_clear(ovrflw, cpu);
 
 			/*
 			 * Accumulate the capacity of active CPUs and
@@ -403,7 +403,7 @@ int do_core_compaction(void)
 				 * If there is something to run on this CPU,
 				 * add this CPU to the overflow set.
 				 */
-				bpf_cpumask_set_cpu(cpu, ovrflw);
+				ovrflw_test_and_set(ovrflw, cpu);
 			} else {
 				if (!bpf_cpumask_test_cpu(cpu, cast_mask(ovrflw)))
 					continue;
@@ -425,7 +425,7 @@ int do_core_compaction(void)
 					 * overflow cpumask here for a while,
 					 * approximately for LAVD_CC_CPU_PIN_INTERVAL.
 					 */
-					bpf_cpumask_clear_cpu(cpu, ovrflw);
+					ovrflw_test_and_clear(ovrflw, cpu);
 					continue;
 				}
 			}
@@ -735,15 +735,15 @@ int reinit_active_cpumask_for_performance(void)
 				continue;
 			if (!cpuc->is_online) {
 				bpf_cpumask_clear_cpu(cpu, active);
-				bpf_cpumask_clear_cpu(cpu, ovrflw);
+				ovrflw_test_and_clear(ovrflw, cpu);
 				continue;
 			}
 
 			if (cpuc->big_core) {
 				bpf_cpumask_set_cpu(cpu, active);
-				bpf_cpumask_clear_cpu(cpu, ovrflw);
+				ovrflw_test_and_clear(ovrflw, cpu);
 			} else {
-				bpf_cpumask_set_cpu(cpu, ovrflw);
+				ovrflw_test_and_set(ovrflw, cpu);
 				bpf_cpumask_clear_cpu(cpu, active);
 			}
 			scx_bpf_kick_cpu(cpu, SCX_KICK_IDLE);
