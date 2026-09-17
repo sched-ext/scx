sched-ext/scx

commit 8964cb0356d271ddc0905e24d3b42507c8d31578

[RFC] scx_lavd: initial refactor for separate unit compilation

scheds/rust/scx_lavd/src/bpf/sys_stat.bpf.c
@@ -4,9 +4,30 @@
  * Author: Changwoo Min <changwoo@igalia.com>
  */
 
-/*
- * To be included to the main.bpf.c
- */
+#include <scx/common.bpf.h>
+#include "intf.h"
+#include "lavd.bpf.h"
+#include <errno.h>
+#include <stdbool.h>
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+struct sys_stat		__weak	sys_stat;
+const volatile u8	__weak preempt_shift;
+volatile u64		__weak performance_mode_ns;
+volatile u64		__weak balanced_mode_ns;
+volatile u64		__weak powersave_mode_ns;
+extern const volatile u64	slice_min_ns;
+extern const volatile u64	slice_max_ns;
+extern volatile bool		__weak no_core_compaction;
+extern volatile bool		__weak reinit_cpumask_for_performance;
+const volatile bool	__weak is_autopilot_on;
+
+int do_autopilot(void);
+u32 calc_avg32(u32 old_val, u32 new_val);
+u64 calc_avg(u64 old_val, u64 new_val);
+int update_power_mode_time(void);
 
 /*
  * Timer for updating system-wide status periorically
@@ -20,7 +41,7 @@ struct {
 	__uint(max_entries, 1);
 	__type(key, u32);
 	__type(value, struct update_timer);
-} update_timer SEC(".maps");
+} update_timer SEC(".maps") __weak;
 
 struct sys_stat_ctx {
 	u64		now;
@@ -361,16 +382,19 @@ static void calc_sys_time_slice(void)
 	sys_stat.slice = calc_avg(sys_stat.slice, slice);
 }
 
-static void do_update_sys_stat(void)
+static int do_update_sys_stat(void)
 {
 	struct sys_stat_ctx c;
 
 	init_sys_stat_ctx(&c);
 	collect_sys_stat(&c);
 	calc_sys_stat(&c);
+
+	return 0;
 }
 
-static void update_sys_stat(void)
+__weak
+int update_sys_stat(void)
 {
 	/*
 	 * Update system statistics.
@@ -406,6 +430,8 @@ static void update_sys_stat(void)
 	 */
 	if (nr_cpdoms > 1)
 		plan_x_cpdom_migration();
+
+	return 0;
 }
 
 static int update_timer_cb(void *map, int *key, struct bpf_timer *timer)
@@ -421,7 +447,8 @@ static int update_timer_cb(void *map, int *key, struct bpf_timer *timer)
 	return 0;
 }
 
-static s32 init_sys_stat(u64 now)
+__weak
+s32 init_sys_stat(u64 now)
 {
 	struct cpdom_ctx *cpdomc;
 	struct bpf_timer *timer;

scheds/rust/scx_lavd/build.rs
@@ -8,6 +8,7 @@ fn main() {
         .unwrap()
         .enable_intf("src/bpf/intf.h", "bpf_intf.rs")
         .enable_skel("src/bpf/main.bpf.c", "bpf")
-        .build()
+        .add_source("src/bpf/sys_stat.bpf.c")
+        .compile_link_gen()
         .unwrap();
 }

scheds/rust/scx_lavd/src/bpf/balance.bpf.c
@@ -20,7 +20,8 @@ u64 __attribute__ ((noinline)) calc_mig_delta(u64 avg_sc_load, int nz_qlen)
 	return avg_sc_load >> LAVD_CPDOM_MIG_SHIFT;
 }
 
-static void plan_x_cpdom_migration(void)
+__weak
+int plan_x_cpdom_migration(void)
 {
 	struct cpdom_ctx *cpdomc;
 	u64 dsq_id;
@@ -96,7 +97,7 @@ static void plan_x_cpdom_migration(void)
 		 * [stealer_threshold ... avg_sc_load ... max_sc_load ... stealee_threshold]
 		 *            -------------------------------------->
 		 */
-		return;
+		return 0;
 	}
 	if ((stealee_threshold <= max_sc_load || overflow_running) &&
 	    (stealer_threshold < min_sc_load)) {
@@ -147,6 +148,8 @@ static void plan_x_cpdom_migration(void)
 	}
 
 	sys_stat.nr_stealee = nr_stealee;
+
+	return 0;
 }
 
 static bool consume_dsq(struct cpdom_ctx *cpdomc)

scheds/rust/scx_lavd/src/bpf/lavd.bpf.h
@@ -112,6 +112,14 @@ struct cpdom_ctx {
 	u64	__cpumask[LAVD_CPU_ID_MAX/64];	    /* cpumasks belongs to this compute domain */
 } __attribute__((aligned(CACHELINE_SIZE)));
 
+extern struct cpdom_ctx	cpdom_ctxs[LAVD_CPDOM_MAX_NR];
+extern int		nr_cpdoms;
+
+struct task_ctx *get_task_ctx(struct task_struct *p);
+struct cpu_ctx *get_cpu_ctx(void);
+struct cpu_ctx *get_cpu_ctx_id(s32 cpu_id);
+struct cpu_ctx *get_cpu_ctx_task(const struct task_struct *p);
+
 /*
  * CPU context
  */
@@ -201,5 +209,75 @@ struct cpu_ctx {
 	struct bpf_cpumask __kptr *tmp_t3_mask;
 } __attribute__((aligned(CACHELINE_SIZE)));
 
+extern const volatile u64	nr_cpu_ids;	/* maximum CPU IDs */
+extern volatile u64		nr_cpus_onln;	/* current number of online CPUs */
+
+/* Logging helpers. */
+
+extern const volatile u8	verbose;
+
+#define debugln(fmt, ...)						\
+({									\
+	if (verbose > 0)						\
+		bpf_printk("[%s:%d] " fmt, __func__, __LINE__,		\
+					##__VA_ARGS__);			\
+})
+
+#define traceln(fmt, ...)						\
+({									\
+	if (verbose > 1)						\
+		bpf_printk("[%s:%d] " fmt, __func__, __LINE__,		\
+					##__VA_ARGS__);			\
+})
+
+/* Arithmetic helpers. */
+
+#ifndef min
+#define min(X, Y) (((X) < (Y)) ? (X) : (Y))
+#endif
+
+#ifndef max
+#define max(X, Y) (((X) < (Y)) ? (Y) : (X))
+#endif
+
+#ifndef clamp
+#define clamp(val, lo, hi) min(max(val, lo), hi)
+#endif
+
+u64 calc_avg(u64 old_val, u64 new_val);
+u64 calc_asym_avg(u64 old_val, u64 new_val);
+
+/* System statistics module .*/
+extern struct sys_stat		sys_stat;
+
+s32 init_sys_stat(u64 now);
+int update_sys_stat(void);
+
+extern volatile u64		performance_mode_ns;
+extern volatile u64		balanced_mode_ns;
+extern volatile u64		powersave_mode_ns;
+
+/* Helpers from util.bpf.c for querying CPU/task state. */
+
+bool is_lock_holder_running(struct cpu_ctx *cpuc);
+bool have_scheduled(struct task_ctx *taskc);
+bool have_pending_tasks(struct cpu_ctx *cpuc);
+bool can_boost_slice(void);
+bool is_lat_cri(struct task_ctx *taskc);
+
+/* Power management helpers. */
+int do_core_compaction(void);
+int update_thr_perf_cri(void);
+int reinit_active_cpumask_for_performance(void);
+
+extern bool			have_little_core;
+
+/* Load balancer helpers. */
+
+int plan_x_cpdom_migration(void);
+
+/* Preemption management helpers. */
+
+int shrink_boosted_slice_remote(struct cpu_ctx *cpuc, u64 now);
 
 #endif /* __LAVD_H */

scheds/rust/scx_lavd/src/bpf/main.bpf.c
@@ -208,6 +208,8 @@ static u64		cur_svc_time;
 const volatile u64	slice_min_ns = LAVD_SLICE_MIN_NS_DFL;
 const volatile u64	slice_max_ns = LAVD_SLICE_MAX_NS_DFL;
 
+static volatile u64	nr_cpus_big;
+
 /*
  * Include sub-modules
  */
@@ -218,7 +220,6 @@ const volatile u64	slice_max_ns = LAVD_SLICE_MAX_NS_DFL;
 #include "lock.bpf.c"
 #include "idle.bpf.c"
 #include "balance.bpf.c"
-#include "sys_stat.bpf.c"
 #include "lat_cri.bpf.c"
 
 static void advance_cur_logical_clk(struct task_struct *p)

scheds/rust/scx_lavd/src/bpf/power.bpf.c
@@ -12,7 +12,7 @@
  * System-wide properties of CPUs
  */
 static bool		have_turbo_core;
-static bool		have_little_core;
+bool			have_little_core;
 const volatile bool	is_smt_active;
 
 
@@ -33,7 +33,7 @@ const volatile u8	cpu_turbo[LAVD_CPU_ID_MAX];
  * Compute domain properties
  */
 /* number of compute domains */
-static int		nr_cpdoms;
+int			nr_cpdoms;
 
 /* contexts for compute domains */
 struct cpdom_ctx	cpdom_ctxs[LAVD_CPDOM_MAX_NR];
@@ -86,9 +86,7 @@ static u64		LAVD_AP_LOW_CAP;
 static u64		LAVD_AP_HIGH_CAP;
 volatile int		power_mode;
 volatile u64		last_power_mode_clk;
-volatile u64		performance_mode_ns;
-volatile u64		balanced_mode_ns;
-volatile u64		powersave_mode_ns;
+volatile bool		is_powersave_mode;
 
 static void reset_suspended_duration(struct cpu_ctx *cpuc)
 {
@@ -248,7 +246,8 @@ static int calc_nr_active_cpus(void)
 	return nr_cpu_ids;
 }
 
-static void do_core_compaction(void)
+__weak
+int do_core_compaction(void)
 {
 	const volatile u16 *cpu_order;
 	struct cpu_ctx *cpuc;
@@ -388,9 +387,11 @@ static void do_core_compaction(void)
 
 unlock_out:
 	bpf_rcu_read_unlock();
+
+	return 0;
 }
 
-static void update_power_mode_time(void)
+int update_power_mode_time(void)
 {
 	u64 now = scx_bpf_now();
 	u64 delta;
@@ -412,6 +413,8 @@ static void update_power_mode_time(void)
 		__sync_fetch_and_add(&powersave_mode_ns, delta);
 		break;
 	}
+	
+	return 0;
 }
 
 static int do_set_power_profile(s32 pm)
@@ -473,7 +476,8 @@ static int do_set_power_profile(s32 pm)
 	return 0;
 }
 
-static int do_autopilot(void)
+__weak
+int do_autopilot(void)
 {
 	/*
 	 * Calculate the required compute capacity from the scaled utilization.
@@ -506,7 +510,8 @@ static int do_autopilot(void)
 	return do_set_power_profile(LAVD_PM_PERFORMANCE);
 }
 
-static void update_thr_perf_cri(void)
+__weak
+int update_thr_perf_cri(void)
 {
 	u32 little_core_scale, delta, diff, thr;
 
@@ -518,7 +523,7 @@ static void update_thr_perf_cri(void)
 	 */
 	if (cur_big_core_scale == LAVD_SCALE) {
 		sys_stat.thr_perf_cri = 0;
-		return;
+		return 0;
 	}
 
 	/*
@@ -580,9 +585,12 @@ static void update_thr_perf_cri(void)
 	}
 
 	sys_stat.thr_perf_cri = thr;
+
+	return 0;
 }
 
-static int reinit_active_cpumask_for_performance(void)
+__weak
+int reinit_active_cpumask_for_performance(void)
 {
 	struct cpu_ctx *cpuc;
 	struct bpf_cpumask *active, *ovrflw;

scheds/rust/scx_lavd/src/bpf/preempt.bpf.c
@@ -235,7 +235,8 @@ static void ask_cpu_yield_after(struct cpu_ctx *victim_cpuc, u64 new_slice)
 	}
 }
 
-static void shrink_boosted_slice_remote(struct cpu_ctx *cpuc, u64 now)
+__hidden
+int shrink_boosted_slice_remote(struct cpu_ctx *cpuc, u64 now)
 {
 	u64 dur, new_slice = 0;
 
@@ -257,6 +258,8 @@ static void shrink_boosted_slice_remote(struct cpu_ctx *cpuc, u64 now)
 		ask_cpu_yield_after(cpuc, new_slice);
 
 	cpuc->nr_preempt++;
+
+	return 0;
 }
 
 static void shrink_boosted_slice_at_tick(struct task_struct *p,

scheds/rust/scx_lavd/src/bpf/util.bpf.c
@@ -18,15 +18,11 @@ private(LAVD) struct bpf_cpumask __kptr *active_cpumask; /* CPU mask for active 
 private(LAVD) struct bpf_cpumask __kptr *ovrflw_cpumask; /* CPU mask for overflow CPUs */
 
 const volatile u64	nr_cpu_ids;	/* maximum CPU IDs */
-static volatile u64	nr_cpus_onln;	/* current number of online CPUs */
-static volatile u64	nr_cpus_big;
-
-struct sys_stat	sys_stat;
+volatile u64		nr_cpus_onln;	/* current number of online CPUs */
 
 /*
  * Options
  */
-volatile bool		is_powersave_mode;
 volatile bool		reinit_cpumask_for_performance;
 volatile bool		no_preemption;
 volatile bool		no_core_compaction;
@@ -34,9 +30,7 @@ volatile bool		no_freq_scaling;
 
 const volatile bool	no_wake_sync;
 const volatile bool	no_slice_boost;
-const volatile bool	is_autopilot_on;
 const volatile u8	verbose;
-const volatile u8	preempt_shift;
 
 /*
  * Exit information
@@ -64,50 +58,28 @@ struct {
 } task_ctx_stor SEC(".maps");
 
 
-#define debugln(fmt, ...)						\
-({									\
-	if (verbose > 0)						\
-		bpf_printk("[%s:%d] " fmt, __func__, __LINE__,		\
-					##__VA_ARGS__);			\
-})
-
-#define traceln(fmt, ...)						\
-({									\
-	if (verbose > 1)						\
-		bpf_printk("[%s:%d] " fmt, __func__, __LINE__,		\
-					##__VA_ARGS__);			\
-})
-
-#ifndef min
-#define min(X, Y) (((X) < (Y)) ? (X) : (Y))
-#endif
-
-#ifndef max
-#define max(X, Y) (((X) < (Y)) ? (Y) : (X))
-#endif
-
-#ifndef clamp
-#define clamp(val, lo, hi) min(max(val, lo), hi)
-#endif
-
-static struct task_ctx *get_task_ctx(struct task_struct *p)
+__hidden
+struct task_ctx *get_task_ctx(struct task_struct *p)
 {
 	return bpf_task_storage_get(&task_ctx_stor, p, 0, 0);
 }
 
-static struct cpu_ctx *get_cpu_ctx(void)
+__hidden
+struct cpu_ctx *get_cpu_ctx(void)
 {
 	const u32 idx = 0;
 	return bpf_map_lookup_elem(&cpu_ctx_stor, &idx);
 }
 
-static struct cpu_ctx *get_cpu_ctx_id(s32 cpu_id)
+__hidden
+struct cpu_ctx *get_cpu_ctx_id(s32 cpu_id)
 {
 	const u32 idx = 0;
 	return bpf_map_lookup_percpu_elem(&cpu_ctx_stor, &idx, cpu_id);
 }
 
-static struct cpu_ctx *get_cpu_ctx_task(const struct task_struct *p)
+__hidden
+struct cpu_ctx *get_cpu_ctx_task(const struct task_struct *p)
 {
 	return get_cpu_ctx_id(scx_bpf_task_cpu(p));
 }
@@ -199,22 +171,22 @@ static inline void reset_cpu_flag(struct cpu_ctx *cpuc, u64 flag)
 	cpuc->flags &= ~flag;
 }
 
-static bool is_lat_cri(struct task_ctx *taskc)
+bool is_lat_cri(struct task_ctx *taskc)
 {
 	return taskc->lat_cri >= sys_stat.avg_lat_cri;
 }
 
-static bool is_lock_holder(struct task_ctx *taskc)
+bool is_lock_holder(struct task_ctx *taskc)
 {
 	return test_task_flag(taskc, LAVD_FLAG_FUTEX_BOOST);
 }
 
-static bool is_lock_holder_running(struct cpu_ctx *cpuc)
+bool is_lock_holder_running(struct cpu_ctx *cpuc)
 {
 	return test_cpu_flag(cpuc, LAVD_FLAG_FUTEX_BOOST);
 }
 
-static bool have_scheduled(struct task_ctx *taskc)
+bool have_scheduled(struct task_ctx *taskc)
 {
 	/*
 	 * If task's time slice hasn't been updated, that means the task has
@@ -223,12 +195,13 @@ static bool have_scheduled(struct task_ctx *taskc)
 	return taskc->slice != 0;
 }
 
-static bool can_boost_slice(void)
+bool can_boost_slice(void)
 {
 	return slice_max_ns <= sys_stat.slice;
 }
 
-static bool have_pending_tasks(struct cpu_ctx *cpuc)
+__hidden
+bool have_pending_tasks(struct cpu_ctx *cpuc)
 {
 	return scx_bpf_dsq_nr_queued(cpuc->cpdom_id) ||
 	       scx_bpf_dsq_nr_queued(SCX_DSQ_LOCAL_ON | cpuc->cpu_id);

commit 02ff6a5649df331263b2fcad60404fc78f1a0405

scx_lavd: Fix the autopilot configuration binding
sys_stat.bpf.c defines is_autopilot_on weak next to the strong definition in
util.bpf.c. Both survive the link: BTF keeps the weak variable and the ELF
symbol resolves to the strong one, at different offsets in .rodata. The Rust
skeleton writes the BTF offset and the update_sys_stat() relocation reads
the ELF symbol, a byte nothing ever writes.

So the autopilot flag the BPF side tests is never set: in the default
autopilot mode do_autopilot() never runs and lavd stays in its initial power
mode under any load. Remove the weak definition so both sides refer to the
one in util.bpf.c.

Fixes: 8964cb0356d2 ("[RFC] scx_lavd: initial refactor for separate unit compilation")

scheds/rust/scx_lavd/src/bpf/sys_stat.bpf.c
@@ -27,7 +27,6 @@ extern const volatile u64	slice_min_ns;
 extern const volatile u64	slice_max_ns;
 extern volatile bool		__weak no_core_compaction;
 extern volatile bool		__weak reinit_cpumask_for_performance;
-const volatile bool	__weak is_autopilot_on;
 
 int do_autopilot(void);
 u32 calc_avg32(u32 old_val, u32 new_val);