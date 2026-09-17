sched-ext/scx

commit 9bc7900b71ab6b14cc4b5eadfdb0fe8c43235c3c

scx_lavd: Move task_ctx to the BPF arena.
In order to use the cpu.max, task_ctx should be in the BPF arena
because the cpu.max library internally uses ATQ to manage throttled tasks.
To this end, let's move the task_ctx to the BPF arena.

More specifically, it includes
  - rename struct task_ctx to task_ctx
  - move pid to main task_ctx
  - move task_ctx to arenas
  - mark functions taking a task context with __arg_arena

Signed-off-by: Emil Tsalapatis <etsal@meta.com>
Signed-off-by: Changwoo Min <changwoo@igalia.com>

scheds/rust/scx_lavd/src/bpf/lock.bpf.c
@@ -16,7 +16,7 @@
 static void __inc_futex_boost(struct cpu_ctx *cpuc)
 {
 	struct task_struct *p = bpf_get_current_task_btf();
-	struct task_ctx *taskc = get_task_ctx(p);
+	task_ctx *taskc = get_task_ctx(p);
 
 	if (taskc) {
 		if (!cpuc)
@@ -35,7 +35,7 @@ static void __inc_futex_boost(struct cpu_ctx *cpuc)
 static void __dec_futex_boost(struct cpu_ctx *cpuc)
 {
 	struct task_struct *p = bpf_get_current_task_btf();
-	struct task_ctx *taskc = get_task_ctx(p);
+	task_ctx *taskc = get_task_ctx(p);
 
 	if (taskc && test_task_flag(taskc, LAVD_FLAG_FUTEX_BOOST)) {
 		if (!cpuc)
@@ -62,7 +62,7 @@ static void dec_futex_boost(void)
 }
 
 __hidden
-void reset_lock_futex_boost(struct task_ctx *taskc, struct cpu_ctx *cpuc)
+void reset_lock_futex_boost(task_ctx *taskc, struct cpu_ctx *cpuc)
 {
 	if (is_lock_holder(taskc))
 		set_task_flag(taskc, LAVD_FLAG_NEED_LOCK_BOOST);

scheds/rust/scx_lavd/src/bpf/main.bpf.c
@@ -189,6 +189,8 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <lib/cgroup.h>
+#include <lib/sdt_task.h>
+#include <lib/atq.h>
 
 char _license[] SEC("license") = "GPL";
 
@@ -266,7 +268,7 @@ static void advance_cur_logical_clk(struct task_struct *p)
 	}
 }
 
-static u64 calc_time_slice(struct task_ctx *taskc, struct cpu_ctx *cpuc)
+static u64 calc_time_slice(task_ctx *taskc, struct cpu_ctx *cpuc)
 {
 	/*
 	 * Calculate the time slice of @taskc to run on @cpuc.
@@ -353,7 +355,7 @@ static u64 calc_time_slice(struct task_ctx *taskc, struct cpu_ctx *cpuc)
 }
 
 static void update_stat_for_running(struct task_struct *p,
-				    struct task_ctx *taskc,
+				    task_ctx *taskc,
 				    struct cpu_ctx *cpuc, u64 now)
 {
 	u64 wait_period, interval;
@@ -444,7 +446,7 @@ static void update_stat_for_running(struct task_struct *p,
 }
 
 static void account_task_runtime(struct task_struct *p,
-				 struct task_ctx *taskc,
+				 task_ctx *taskc,
 				 struct cpu_ctx *cpuc,
 				 u64 now)
 {
@@ -471,7 +473,7 @@ static void account_task_runtime(struct task_struct *p,
 }
 
 static void update_stat_for_stopping(struct task_struct *p,
-				     struct task_ctx *taskc,
+				     task_ctx *taskc,
 				     struct cpu_ctx *cpuc)
 {
 	u64 now = scx_bpf_now();
@@ -511,7 +513,7 @@ static void update_stat_for_stopping(struct task_struct *p,
 }
 
 static void update_stat_for_refill(struct task_struct *p,
-				   struct task_ctx *taskc,
+				   task_ctx *taskc,
 				   struct cpu_ctx *cpuc)
 {
 	u64 now = scx_bpf_now();
@@ -602,7 +604,7 @@ static bool can_direct_dispatch(u64 dsq_id, s32 cpu, bool is_idle)
 
 void BPF_STRUCT_OPS(lavd_enqueue, struct task_struct *p, u64 enq_flags)
 {
-	struct task_ctx *taskc;
+	task_ctx *taskc;
 	struct cpu_ctx *cpuc, *cpuc_cur;
 	s32 task_cpu, cpu = -ENOENT;
 	u64 cpdom_id, dsq_id;
@@ -712,7 +714,7 @@ void BPF_STRUCT_OPS(lavd_enqueue, struct task_struct *p, u64 enq_flags)
 void BPF_STRUCT_OPS(lavd_dispatch, s32 cpu, struct task_struct *prev)
 {
 	struct cpu_ctx *cpuc;
-	struct task_ctx *taskc_prev = NULL;
+	task_ctx *taskc_prev = NULL;
 	struct bpf_cpumask *active, *ovrflw;
 	struct task_struct *p;
 	u32 dsq_type;
@@ -828,7 +830,7 @@ void BPF_STRUCT_OPS(lavd_dispatch, s32 cpu, struct task_struct *prev)
 	bpf_for(dsq_type, dsq_start, LAVD_DSQ_NR_TYPES) {
 		u64 dsq_id = dsq_ids[dsq_type];
 		bpf_for_each(scx_dsq, p, dsq_id, 0) {
-			struct task_ctx *taskc;
+			task_ctx *taskc;
 
 			/*
 			 * note that this is a hack to bypass the restriction of the
@@ -955,8 +957,9 @@ consume_prev:
 void BPF_STRUCT_OPS(lavd_runnable, struct task_struct *p, u64 enq_flags)
 {
 	struct task_struct *waker;
-	struct task_ctx *p_taskc, *waker_taskc;
+	task_ctx *p_taskc, *waker_taskc;
 	u64 now, interval;
+	int i;
 
 	/*
 	 * Clear the accumulated runtime.
@@ -1025,15 +1028,15 @@ void BPF_STRUCT_OPS(lavd_runnable, struct task_struct *p, u64 enq_flags)
 	 */
 	if (is_monitored) {
 		p_taskc->waker_pid = waker->pid;
-		__builtin_memcpy_inline(p_taskc->waker_comm, waker->comm,
-					TASK_COMM_LEN);
+		for (i = 0; i < TASK_COMM_LEN && can_loop; i++)
+			p_taskc->waker_comm[i] = waker->comm[i];
 	}
 }
 
 void BPF_STRUCT_OPS(lavd_running, struct task_struct *p)
 {
 	struct cpu_ctx *cpuc;
-	struct task_ctx *taskc;
+	task_ctx *taskc;
 	u64 now = scx_bpf_now();
 
 	cpuc = get_cpu_ctx_task(p);
@@ -1088,7 +1091,7 @@ void BPF_STRUCT_OPS(lavd_running, struct task_struct *p)
 void BPF_STRUCT_OPS(lavd_tick, struct task_struct *p)
 {
 	struct cpu_ctx *cpuc;
-	struct task_ctx *taskc;
+	task_ctx *taskc;
 	u64 now;
 
 	/*
@@ -1131,7 +1134,7 @@ void BPF_STRUCT_OPS(lavd_tick, struct task_struct *p)
 void BPF_STRUCT_OPS(lavd_stopping, struct task_struct *p, bool runnable)
 {
 	struct cpu_ctx *cpuc;
-	struct task_ctx *taskc;
+	task_ctx *taskc;
 
 	/*
 	 * Update task statistics
@@ -1149,7 +1152,7 @@ void BPF_STRUCT_OPS(lavd_stopping, struct task_struct *p, bool runnable)
 void BPF_STRUCT_OPS(lavd_quiescent, struct task_struct *p, u64 deq_flags)
 {
 	struct cpu_ctx *cpuc;
-	struct task_ctx *taskc;
+	task_ctx *taskc;
 	u64 now, interval;
 
 	cpuc = get_cpu_ctx_task(p);
@@ -1338,7 +1341,7 @@ void BPF_STRUCT_OPS(lavd_update_idle, s32 cpu, bool idle)
 void BPF_STRUCT_OPS(lavd_set_cpumask, struct task_struct *p,
 		    const struct cpumask *cpumask)
 {
-	struct task_ctx *taskc;
+	task_ctx *taskc;
 
 	taskc = get_task_ctx(p);
 	if (!taskc) {
@@ -1423,7 +1426,7 @@ void BPF_STRUCT_OPS(lavd_cpu_release, s32 cpu,
 
 void BPF_STRUCT_OPS(lavd_enable, struct task_struct *p)
 {
-	struct task_ctx *taskc;
+	task_ctx *taskc;
 
 	/*
 	 * Set task's service time to the current, minimum service time.
@@ -1437,11 +1440,13 @@ void BPF_STRUCT_OPS(lavd_enable, struct task_struct *p)
 	taskc->svc_time = READ_ONCE(cur_svc_time);
 }
 
-s32 BPF_STRUCT_OPS(lavd_init_task, struct task_struct *p,
-		   struct scx_init_task_args *args)
+
+s32 BPF_STRUCT_OPS_SLEEPABLE(lavd_init_task, struct task_struct *p,
+			     struct scx_init_task_args *args)
 {
-	struct task_ctx *taskc;
-	u64 now = scx_bpf_now();
+	task_ctx *taskc;
+	u64 now;
+	int i;
 
 	/*
 	 * When @p becomes under the SCX control (e.g., being forked), @p's
@@ -1457,8 +1462,7 @@ s32 BPF_STRUCT_OPS(lavd_init_task, struct task_struct *p,
 		return -ESRCH;
 	}
 	
-	taskc = bpf_task_storage_get(&task_ctx_stor, p, 0,
-				     BPF_LOCAL_STORAGE_GET_F_CREATE);
+	taskc = scx_task_alloc(p);
 	if (!taskc) {
 		scx_bpf_error("task_ctx_stor first lookup failed");
 		return -ENOMEM;
@@ -1468,20 +1472,34 @@ s32 BPF_STRUCT_OPS(lavd_init_task, struct task_struct *p,
 	/*
 	 * Initialize @p's context.
 	 */
-	__builtin_memset(taskc, 0, sizeof(*taskc));
+	for (i = 0; i < sizeof(*taskc) && can_loop; i++)
+		((char __arena *)taskc)[i] = 0;
+
+	bpf_rcu_read_lock();
 	if (bpf_cpumask_weight(p->cpus_ptr) != nr_cpu_ids)
 		set_task_flag(taskc, LAVD_FLAG_IS_AFFINITIZED);
 	else
 		reset_task_flag(taskc, LAVD_FLAG_IS_AFFINITIZED);
+	bpf_rcu_read_unlock();
+
+	now = scx_bpf_now();
 	taskc->last_runnable_clk = now;
 	taskc->last_running_clk = now; /* for avg_runtime */
 	taskc->last_stopping_clk = now; /* for avg_runtime */
 	taskc->last_quiescent_clk = now;
 	taskc->avg_runtime = sys_stat.slice;
 	taskc->svc_time = sys_stat.avg_svc_time;
+	taskc->pid = p->pid;
 	taskc->cgrp_id = args->cgroup->kn->id;
 
 	set_on_core_type(taskc, p->cpus_ptr);
+	return 0;
+}
+
+s32 BPF_STRUCT_OPS(lavd_exit_task, struct task_struct *p,
+		   struct scx_exit_task_args *args)
+{
+	scx_task_free(p);
 	return 0;
 }
 
@@ -1819,7 +1837,7 @@ void BPF_STRUCT_OPS(lavd_cgroup_exit, struct cgroup *cgrp)
 void BPF_STRUCT_OPS(lavd_cgroup_move, struct task_struct *p,
 		    struct cgroup *from, struct cgroup *to)
 {
-	struct task_ctx *taskc;
+	task_ctx *taskc;
 
 	taskc = get_task_ctx(p);
 	if (!taskc)
@@ -1851,6 +1869,10 @@ s32 BPF_STRUCT_OPS_SLEEPABLE(lavd_init)
 {
 	u64 now = scx_bpf_now();
 	int err;
+
+	_Static_assert(
+		sizeof(struct atq_ctx) >= sizeof(struct scx_task_common),
+		"atq_ctx should be equal or larger than scx_task_common");
 
 	/*
 	 * Create compute domains.
@@ -1941,6 +1963,7 @@ SCX_OPS_DEFINE(lavd_ops,
 	       .cpu_release		= (void *)lavd_cpu_release,
 	       .enable			= (void *)lavd_enable,
 	       .init_task		= (void *)lavd_init_task,
+	       .exit_task		= (void *)lavd_exit_task,
 	       .cgroup_init		= (void *)lavd_cgroup_init,
 	       .cgroup_exit		= (void *)lavd_cgroup_exit,
 	       .cgroup_move		= (void *)lavd_cgroup_move,

scheds/rust/scx_lavd/src/bpf/util.bpf.c
@@ -53,21 +53,10 @@ struct {
 	__uint(max_entries, 1);
 } cpu_ctx_stor SEC(".maps");
 
-/*
- * Per-task scheduling context
- */
-struct {
-	__uint(type, BPF_MAP_TYPE_TASK_STORAGE);
-	__uint(map_flags, BPF_F_NO_PREALLOC);
-	__type(key, int);
-	__type(value, struct task_ctx);
-} task_ctx_stor SEC(".maps");
-
-
 __hidden
-struct task_ctx *get_task_ctx(struct task_struct *p)
+u64 get_task_ctx_internal(struct task_struct __arg_trusted *p)
 {
-	return bpf_task_storage_get(&task_ctx_stor, p, 0, 0);
+	return (u64)scx_task_data(p);
 }
 
 __hidden
@@ -148,19 +137,19 @@ static bool is_pinned(const struct task_struct *p)
 }
 
 __hidden
-bool test_task_flag(struct task_ctx *taskc, u64 flag)
+bool test_task_flag(task_ctx __arg_arena *taskc, u64 flag)
 {
 	return (taskc->flags & flag) == flag;
 }
 
 __hidden
-void set_task_flag(struct task_ctx *taskc, u64 flag)
+void set_task_flag(task_ctx __arg_arena *taskc, u64 flag)
 {
 	taskc->flags |= flag;
 }
 
 __hidden
-void reset_task_flag(struct task_ctx *taskc, u64 flag)
+void reset_task_flag(task_ctx __arg_arena *taskc, u64 flag)
 {
 	taskc->flags &= ~flag;
 }
@@ -184,13 +173,13 @@ inline void reset_cpu_flag(struct cpu_ctx *cpuc, u64 flag)
 }
 
 __hidden
-bool is_lat_cri(struct task_ctx *taskc)
+bool is_lat_cri(task_ctx __arg_arena *taskc)
 {
 	return taskc->lat_cri >= sys_stat.avg_lat_cri;
 }
 
 __hidden
-bool is_lock_holder(struct task_ctx *taskc)
+bool is_lock_holder(task_ctx __arg_arena *taskc)
 {
 	return test_task_flag(taskc, LAVD_FLAG_FUTEX_BOOST);
 }
@@ -201,7 +190,7 @@ bool is_lock_holder_running(struct cpu_ctx *cpuc)
 	return test_cpu_flag(cpuc, LAVD_FLAG_FUTEX_BOOST);
 }
 
-bool have_scheduled(struct task_ctx *taskc)
+bool have_scheduled(task_ctx __arg_arena *taskc)
 {
 	/*
 	 * If task's time slice hasn't been updated, that means the task has
@@ -247,7 +236,7 @@ s64 __attribute__ ((noinline)) pick_any_bit(u64 bitmap, u64 nuance)
 	return (tz + shift) & 63;
 }
 
-static void set_on_core_type(struct task_ctx *taskc,
+static void set_on_core_type(task_ctx __arg_arena *taskc,
 			     const struct cpumask *cpumask)
 {
 	bool on_big = false, on_little = false;

scheds/rust/scx_lavd/src/bpf/idle.bpf.c
@@ -12,7 +12,7 @@ struct pick_ctx {
 	 * Input arguments for pick_idle_cpu().
 	 */
 	const struct task_struct *p;
-	struct task_ctx *taskc;
+	task_ctx *taskc;
 	u64 wake_flags;
 	s32 prev_cpu;
 	/*
@@ -866,7 +866,7 @@ unlock_out:
 }
 
 static
-s64 pick_proper_dsq(const struct task_struct *p, struct task_ctx *taskc,
+s64 pick_proper_dsq(const struct task_struct *p, task_ctx *taskc,
 		    s32 task_cpu, s32 *cpu, bool *is_idle,
 		    struct cpu_ctx *cpuc_cur)
 {

scheds/rust/scx_lavd/src/bpf/intf.h
@@ -122,7 +122,22 @@ struct sys_stat {
 /*
  * Task context
  */
+struct atq_ctx {
+	u64	dummy[8];
+};
+
 struct task_ctx {
+	/*
+	 * Do NOT change the position of atq. It should be at the beginning
+	 * of the task_ctx. 
+	 *
+	 * TODO: The type of atq should be scx_task_common. However, to
+	 * workaround the complex header dependencies, a large enough space
+	 * that can hold scx_task_common is allocated for now. This will be
+	 * fixed later after some more refactoring.
+	 */
+	struct atq_ctx atq;
+
 	/*
 	 * Clocks when a task state transition happens for task statistics calculation
 	 */
@@ -152,8 +167,9 @@ struct task_ctx {
 	u64	slice;			/* time slice */
 
 	/*
-	 * Task cgroup
+	 * Task cgroup and id
 	 */
+	pid_t	pid;			/* pid for this task */
 	u64	cgrp_id;		/* cgroup id of this task */
 
 	/*
@@ -177,7 +193,6 @@ struct task_ctx {
  * Task's extra context for report
  */
 struct task_ctx_x {
-	pid_t	pid;
 	char	comm[TASK_COMM_LEN + 1];
 	char	stat[LAVD_STATUS_STR_LEN + 1];
 	u16	static_prio;	/* nice priority */

scheds/rust/scx_lavd/src/bpf/introspec.bpf.c
@@ -30,11 +30,12 @@ struct {
 } introspec_msg SEC(".maps");
 
 static __always_inline
-int submit_task_ctx(struct task_struct *p, struct task_ctx *taskc, u32 cpu_id)
+int submit_task_ctx(struct task_struct *p, task_ctx __arg_arena *taskc, u32 cpu_id)
 {
 	struct cpu_ctx *cpuc;
 	struct cpdom_ctx *cpdomc;
 	struct msg_task_ctx *m;
+	int i;
 
 	cpuc = get_cpu_ctx_id(cpu_id);
 	if (!cpuc)
@@ -49,7 +50,6 @@ int submit_task_ctx(struct task_struct *p, struct task_ctx *taskc, u32 cpu_id)
 		return -ENOMEM;
 
 	m->hdr.kind = LAVD_MSG_TASKC;
-	m->taskc_x.pid = p->pid;
 	__builtin_memcpy_inline(m->taskc_x.comm, p->comm, TASK_COMM_LEN);
 	m->taskc_x.static_prio = get_nice_prio(p);
 	m->taskc_x.cpu_util = s2p(cpuc->avg_util);
@@ -69,7 +69,8 @@ int submit_task_ctx(struct task_struct *p, struct task_ctx *taskc, u32 cpu_id)
 	m->taskc_x.stat[3] = test_task_flag(taskc, LAVD_FLAG_IS_GREEDY)? 'G' : 'E';
 	m->taskc_x.stat[4] = '\0';
 
-	__builtin_memcpy_inline(&m->taskc, taskc, sizeof(m->taskc));
+	for (i = 0; i < sizeof(m->taskc) && can_loop; i++)
+		((char *) &m->taskc)[i] = ((char __arena *) taskc)[i];
 
 	bpf_ringbuf_submit(m, 0);
 
@@ -77,7 +78,7 @@ int submit_task_ctx(struct task_struct *p, struct task_ctx *taskc, u32 cpu_id)
 }
 
 static void proc_introspec_sched_n(struct task_struct *p,
-				   struct task_ctx *taskc)
+				   task_ctx __arg_arena *taskc)
 {
 	u64 cur_nr, prev_nr;
 	u32 cpu_id;
@@ -112,7 +113,7 @@ static void proc_introspec_sched_n(struct task_struct *p,
 }
 
 __hidden
-void try_proc_introspec_cmd(struct task_struct *p, struct task_ctx *taskc)
+void try_proc_introspec_cmd(struct task_struct *p, task_ctx __arg_arena *taskc)
 {
 	if (!is_monitored)
 		return;

scheds/rust/scx_lavd/src/bpf/lat_cri.bpf.c
@@ -8,7 +8,7 @@
  * To be included to the main.bpf.c
  */
 
-static u64 calc_weight_factor(struct task_struct *p, struct task_ctx *taskc)
+static u64 calc_weight_factor(struct task_struct *p, task_ctx *taskc)
 {
 	u64 weight_boost = 1;
 
@@ -63,19 +63,19 @@ static u64 calc_weight_factor(struct task_struct *p, struct task_ctx *taskc)
 	return p->scx.weight * weight_boost + 1;
 }
 
-static u64 calc_wait_factor(struct task_ctx *taskc)
+static u64 calc_wait_factor(task_ctx *taskc)
 {
 	u64 freq = min(taskc->wait_freq, LAVD_LC_FREQ_MAX);
 	return freq + 1;
 }
 
-static u64 calc_wake_factor(struct task_ctx *taskc)
+static u64 calc_wake_factor(task_ctx *taskc)
 {
 	u64 freq = min(taskc->wake_freq, LAVD_LC_FREQ_MAX);
 	return freq + 1;
 }
 
-static inline u64 calc_runtime_factor(struct task_ctx *taskc)
+static inline u64 calc_runtime_factor(task_ctx *taskc)
 {
 	u64 ft = 1, delta;
 
@@ -86,7 +86,7 @@ static inline u64 calc_runtime_factor(struct task_ctx *taskc)
 	return ft;
 }
 
-static u64 calc_sum_runtime_factor(struct task_struct *p, struct task_ctx *taskc)
+static u64 calc_sum_runtime_factor(struct task_struct *p, task_ctx *taskc)
 {
 	u64 sum = max(taskc->run_freq, 1) * max(taskc->avg_runtime, 1);
 	return (sum >> LAVD_SHIFT) * p->scx.weight;
@@ -97,7 +97,7 @@ u32 __attribute__ ((noinline)) log2x(u64 v)
 	return log2_u64(v);
 }
 
-static void calc_lat_cri(struct task_struct *p, struct task_ctx *taskc)
+static void calc_lat_cri(struct task_struct *p, task_ctx *taskc)
 {
 	u64 weight_ft, wait_ft, wake_ft, runtime_ft, sum_runtime_ft;
 	u64 log_wwf, lat_cri, perf_cri = LAVD_SCALE;
@@ -171,7 +171,7 @@ static void calc_lat_cri(struct task_struct *p, struct task_ctx *taskc)
 	taskc->perf_cri = perf_cri;
 }
 
-static u64 calc_greedy_penalty(struct task_ctx *taskc)
+static u64 calc_greedy_penalty(task_ctx *taskc)
 {
 	u64 ratio, penalty;
 
@@ -198,7 +198,7 @@ static u64 calc_greedy_penalty(struct task_ctx *taskc)
 	return penalty;
 }
 
-static u64 calc_adjusted_runtime(struct task_ctx *taskc)
+static u64 calc_adjusted_runtime(task_ctx *taskc)
 {
 	u64 runtime;
 
@@ -214,7 +214,7 @@ static u64 calc_adjusted_runtime(struct task_ctx *taskc)
 }
 
 static u64 calc_virtual_deadline_delta(struct task_struct *p,
-				       struct task_ctx *taskc)
+				       task_ctx *taskc)
 {
 	u64 deadline, adjusted_runtime;
 	u32 greedy_penalty;
@@ -231,7 +231,7 @@ static u64 calc_virtual_deadline_delta(struct task_struct *p,
 	return deadline >> LAVD_SHIFT;
 }
 
-static u64 calc_when_to_run(struct task_struct *p, struct task_ctx *taskc)
+static u64 calc_when_to_run(struct task_struct *p, task_ctx *taskc)
 {
 	u64 dl_delta, clc;
 

scheds/rust/scx_lavd/src/bpf/lavd.bpf.h
@@ -7,6 +7,7 @@
 #define __LAVD_H
 
 #include <scx/common.bpf.h>
+#include <scx/bpf_arena_common.bpf.h>
 
 /*
  * common macros
@@ -125,7 +126,11 @@ extern struct cpdom_ctx		cpdom_ctxs[LAVD_CPDOM_MAX_NR];
 extern struct bpf_cpumask	cpdom_cpumask[LAVD_CPDOM_MAX_NR];
 extern int			nr_cpdoms;
 
-struct task_ctx *get_task_ctx(struct task_struct *p);
+typedef struct task_ctx __arena task_ctx;
+
+u64 get_task_ctx_internal(struct task_struct *p);
+#define get_task_ctx(p) ((task_ctx *)get_task_ctx_internal((p)))
+
 struct cpu_ctx *get_cpu_ctx(void);
 struct cpu_ctx *get_cpu_ctx_id(s32 cpu_id);
 struct cpu_ctx *get_cpu_ctx_task(const struct task_struct *p);
@@ -313,19 +318,19 @@ bool test_cpu_flag(struct cpu_ctx *cpuc, u64 flag);
 void set_cpu_flag(struct cpu_ctx *cpuc, u64 flag);
 void reset_cpu_flag(struct cpu_ctx *cpuc, u64 flag);
 
-bool is_lock_holder(struct task_ctx *taskc);
+bool is_lock_holder(task_ctx *taskc);
 bool is_lock_holder_running(struct cpu_ctx *cpuc);
-bool have_scheduled(struct task_ctx *taskc);
+bool have_scheduled(task_ctx *taskc);
 bool have_pending_tasks(struct cpu_ctx *cpuc);
 bool can_boost_slice(void);
-bool is_lat_cri(struct task_ctx *taskc);
+bool is_lat_cri(task_ctx *taskc);
 u16 get_nice_prio(struct task_struct *p);
 u32 cpu_to_dsq(u32 cpu);
 
-void set_task_flag(struct task_ctx *taskc, u64 flag);
-void reset_task_flag(struct task_ctx *taskc, u64 flag);
-bool test_task_flag(struct task_ctx *taskc, u64 flag);
-void reset_task_flag(struct task_ctx *taskc, u64 flag);
+void set_task_flag(task_ctx *taskc, u64 flag);
+void reset_task_flag(task_ctx *taskc, u64 flag);
+bool test_task_flag(task_ctx *taskc, u64 flag);
+void reset_task_flag(task_ctx *taskc, u64 flag);
 
 extern struct bpf_cpumask __kptr *turbo_cpumask; /* CPU mask for turbo CPUs */
 extern struct bpf_cpumask __kptr *big_cpumask; /* CPU mask for big CPUs */
@@ -337,7 +342,7 @@ extern struct bpf_cpumask __kptr *ovrflw_cpumask; /* CPU mask for overflow CPUs 
 int do_core_compaction(void);
 int update_thr_perf_cri(void);
 int reinit_active_cpumask_for_performance(void);
-bool is_perf_cri(struct task_ctx *taskc);
+bool is_perf_cri(task_ctx *taskc);
 
 extern bool			have_little_core;
 extern bool			have_turbo_core;
@@ -371,18 +376,18 @@ int shrink_boosted_slice_remote(struct cpu_ctx *cpuc, u64 now);
 
 /* Futex lock-related helpers. */
 
-void reset_lock_futex_boost(struct task_ctx *taskc, struct cpu_ctx *cpuc);
+void reset_lock_futex_boost(task_ctx *taskc, struct cpu_ctx *cpuc);
 
 /* Scheduler introspection-related helpers. */
 
-u64 get_est_stopping_clk(struct task_ctx *taskc, u64 now);
-void try_proc_introspec_cmd(struct task_struct *p, struct task_ctx *taskc);
+u64 get_est_stopping_clk(task_ctx *taskc, u64 now);
+void try_proc_introspec_cmd(struct task_struct *p, task_ctx *taskc);
 void reset_cpu_preemption_info(struct cpu_ctx *cpuc, bool released);
 int shrink_boosted_slice_remote(struct cpu_ctx *cpuc, u64 now);
 void shrink_boosted_slice_at_tick(struct task_struct *p,
 					 struct cpu_ctx *cpuc, u64 now);
 void try_find_and_kick_victim_cpu(struct task_struct *p,
-					 struct task_ctx *taskc,
+					 task_ctx *taskc,
 					 s32 preferred_cpu,
 					 u64 dsq_id);
 

commit 43841691c5289e14c1ff658f10c0f63df6bd7714

scx_lavd: Fix torn task_ctx cache fills and the racy parent lookup
lavd caches the last task_ctx lookup per CPU as a task, pid and context
triple. Scheduler callbacks fill it with preemption disabled, but the futex
and execve hooks and the sleepable init_task() filled it too, and a
preemptible or migrating writer can interleave with a callback's fill and
leave a torn entry that returns another task's context. The parent lookup in
init_task() also races with the parent's exit: the program is sleepable, so
it has no implicit RCU section, and the reference it holds on the parent
does not keep the data alive because ops.exit_task() runs when the task
dies.

Keep the cached get_task_ctx() for the callback's own task and add
find_task_ctx() for every other task and the hooks, uncached and quiet since
the hooks fire for tasks the scheduler has no data for. Wrap the parent copy
in an RCU read-side section and free through scx_task_free_rcu().

Fixes: 9bc7900b71ab ("scx_lavd: Move task_ctx to the BPF arena.")
Fixes: 66da81a3004f ("scx_lavd: cache last task_ctx lookup per CPU")

scheds/rust/scx_lavd/src/bpf/balance.bpf.c
@@ -271,7 +271,7 @@ u64 __attribute__((noinline)) dsq_peek_task_load(u64 dsq_id)
 	struct task_struct *peek_p = __COMPAT_scx_bpf_dsq_peek(dsq_id);
 
 	if (peek_p) {
-		task_ctx *peek_taskc = get_task_ctx(peek_p);
+		task_ctx *peek_taskc = find_task_ctx(peek_p);
 		if (peek_taskc)
 			return task_load_metric(peek_taskc);
 	}

scheds/rust/scx_lavd/src/bpf/lock.bpf.c
@@ -21,13 +21,12 @@ static void __inc_futex_boost(struct cpu_ctx *cpuc)
 
 	/*
 	 * These hooks are system-wide, so they also fire for RT and DL
-	 * tasks. sched_ext does not manage those, so they have no task_ctx
-	 * and get_task_ctx() would report the miss as an error.
+	 * tasks, which sched_ext never runs and so never boosts.
 	 */
 	if (rt_or_dl_task(p))
 		return;
 
-	taskc = get_task_ctx(p);
+	taskc = find_task_ctx(p);
 
 	if (taskc) {
 		if (!cpuc)
@@ -50,13 +49,12 @@ static void __dec_futex_boost(struct cpu_ctx *cpuc)
 
 	/*
 	 * These hooks are system-wide, so they also fire for RT and DL
-	 * tasks. sched_ext does not manage those, so they have no task_ctx
-	 * and get_task_ctx() would report the miss as an error.
+	 * tasks, which sched_ext never runs and so never boosts.
 	 */
 	if (rt_or_dl_task(p))
 		return;
 
-	taskc = get_task_ctx(p);
+	taskc = find_task_ctx(p);
 
 	if (taskc && test_task_flag(taskc, LAVD_FLAG_FUTEX_BOOST)) {
 		if (!cpuc)

scheds/rust/scx_lavd/src/bpf/main.bpf.c
@@ -1502,7 +1502,7 @@ void BPF_STRUCT_OPS(lavd_dispatch, s32 cpu, struct task_struct *prev)
 		 * if the task can run on either active or overflow set,
 		 * try another task.
 		 */
-		taskc = get_task_ctx(p);
+		taskc = find_task_ctx(p);
 		if(taskc &&
 		(!test_task_flag(taskc, LAVD_FLAG_IS_AFFINITIZED) ||
 		bpf_cpumask_intersects(cast_mask(active), p->cpus_ptr) ||
@@ -1618,7 +1618,7 @@ void BPF_STRUCT_OPS(lavd_runnable, struct task_struct *p, u64 enq_flags)
 	else
 		reset_task_flag(p_taskc, LAVD_FLAG_WOKEN_BY_RT_DL);
 
-	waker_taskc = get_task_ctx(waker);
+	waker_taskc = find_task_ctx(waker);
 	if (!waker_taskc) {
 		/*
 		 * In this case, the waker could be an idle task
@@ -2174,7 +2174,8 @@ s32 BPF_STRUCT_OPS_SLEEPABLE(lavd_init_task, struct task_struct *p,
 	 *   https://man7.org/linux/man-pages/man2/sched_setscheduler.2.html
 	 */
 	parent = bpf_task_from_pid(p->real_parent->pid);
-	if (parent && (taskc_parent = get_task_ctx(parent))) {
+	bpf_rcu_read_lock();
+	if (parent && (taskc_parent = find_task_ctx(parent))) {
 		/* Do not inherit cgroup status. */
 		for (i = 0; i < sizeof(taskc->atq) && can_loop; i++)
 			((char __arena *)taskc)[i] = 0;
@@ -2193,6 +2194,8 @@ s32 BPF_STRUCT_OPS_SLEEPABLE(lavd_init_task, struct task_struct *p,
 		taskc->avg_runtime_invr = sys_stat.slice_wall;
 		taskc->svc_time_iwgt = sys_stat.avg_svc_time_iwgt;
 	}
+
+	bpf_rcu_read_unlock();
 
 	taskc->suggested_cpu_id = scx_bpf_task_cpu(p);
 	taskc->pinned_cpu_id = -ENOENT;
@@ -2248,12 +2251,12 @@ s32 BPF_STRUCT_OPS(lavd_exit_task, struct task_struct *p,
 	/*
 	 * Mark the task dead in the bandwidth-throttle queues before freeing
 	 * taskc. Concurrent drains or cgroup moves that already hold the task
-	 * must finish before scx_task_free() releases the arena storage.
+	 * must finish before the arena storage can be reclaimed.
 	 */
 	if (enable_cpu_bw && taskc)
 		scx_cgroup_bw_cancel((u64)taskc, SCX_CGROUP_BW_CANCEL_DROP);
 
-	scx_task_free(p);
+	scx_task_free_rcu(p);
 	return 0;
 }
 
@@ -2832,7 +2835,7 @@ int set_aggressive_migration(void)
 	cpuc = get_cpu_ctx();
 	if (cpuc &&
 	    (curr = bpf_get_current_task_btf()) &&
-	    (taskc = get_task_ctx_curcpu(curr, cpuc)) &&
+	    (taskc = find_task_ctx(curr)) &&
 	    (cpdc = MEMBER_VPTR(cpdom_ctxs, [cpuc->cpdom_id])) &&
 	    READ_ONCE(cpdc->is_stealee)) {
 		set_task_flag(taskc, LAVD_FLAG_MIGRATION_AGGRESSIVE);

scheds/rust/scx_lavd/src/bpf/util.bpf.c
@@ -63,10 +63,9 @@ struct {
 } cpu_ctx_stor SEC(".maps");
 
 __hidden
-u64 __get_task_ctx_slowpath(struct task_struct __arg_trusted *p,
-			    struct cpu_ctx *cpuc)
+u64 __find_task_ctx(struct task_struct __arg_trusted *p, struct cpu_ctx *cpuc, bool quiet)
 {
-	u64 raw = (u64)scx_task_data(p);
+	u64 raw = (u64)(quiet ? __scx_task_data(p) : scx_task_data(p));
 
 	if (cpuc && raw) {
 		cpuc->cached_task = (u64)p;

scheds/rust/scx_lavd/src/bpf/util.bpf.h
@@ -55,18 +55,24 @@ static __always_inline bool is_rt_or_dl_task_running(s32 cpu)
 }
 
 /*
- * task_ctx lookup with per-CPU cache.
+ * Two lookups, chosen by which task a program is asking about.
  *
- * get_task_ctx_curcpu(p, cpuc) -- @cpuc MUST be the current CPU's cpu_ctx
- * (i.e. obtained via get_cpu_ctx(), not get_cpu_ctx_id(...) or
- * get_cpu_ctx_task(...) for an arbitrary CPU). Misuse silently corrupts
- * the cache of a remote CPU and racing reads can return torn results.
+ * get_task_ctx() is for the task a scheduler callback was invoked for. The
+ * kernel keeps that task alive across the callback, its context must exist, and
+ * the callback runs with preemption disabled, so the lookup may cache the
+ * result in this CPU's cpu_ctx and treats a miss as an error.
+ * get_task_ctx_curcpu() is the same with the current CPU's cpu_ctx the caller
+ * already holds.
  *
- * get_task_ctx(p) is a foot-gun-free wrapper that always uses
- * get_cpu_ctx() internally.
+ * find_task_ctx() is for every other task: wakers, DSQ candidates, the parent
+ * in init_task() and the current task in tracing hooks. Those may have no
+ * context or may exit concurrently, so it returns NULL instead of reporting the
+ * miss on the error stream, and it never touches the cache, because a
+ * preemptible or sleepable caller can tear a cache entry or write another
+ * CPU's. Keep the returned pointer inside one RCU read-side critical section.
  */
 struct cpu_ctx;
-u64 __get_task_ctx_slowpath(struct task_struct *p, struct cpu_ctx *cpuc);
+u64 __find_task_ctx(struct task_struct *p, struct cpu_ctx *cpuc, bool quiet);
 
 static __always_inline u64
 __get_task_ctx_curcpu(struct task_struct *p, struct cpu_ctx *cpuc)
@@ -83,12 +89,16 @@ __get_task_ctx_curcpu(struct task_struct *p, struct cpu_ctx *cpuc)
 		    cpuc->cached_pid == p->pid)
 			return cpuc->cached_taskc_raw;
 	}
-	return __get_task_ctx_slowpath(p, cpuc);
+	return __find_task_ctx(p, cpuc, false);
 }
 
 #define get_task_ctx_curcpu(p, cpuc) \
 	((task_ctx *)__get_task_ctx_curcpu((p), (cpuc)))
 #define get_task_ctx(p)	get_task_ctx_curcpu((p), get_cpu_ctx())
 
+static __always_inline task_ctx *find_task_ctx(struct task_struct *p)
+{
+	return (task_ctx *)__find_task_ctx(p, NULL, true);
+}
 
 #endif /* __UTIL_H */